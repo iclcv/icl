9edeab090892eeddfa9df66834a19bb0b54c5bd8
Michael Götting <mgoettin@techfak.uni-bielefeld.de>
2006-07-28 12:15:24 +0000

    - Removing boost library - Modify DEBUG_LOG messages

--- ICLCore/src/ICLMacros.h
+++ ICLCore/src/ICLMacros.h
@@ -16,7 +16,7 @@ using namespace std;
 /* {{{ Error log */
 
 #define ERROR_LOG(x) \
-{ cerr << "ERROR: [" __FILE__ ":" << __FUNCTION__ << ",line:" << __LINE__ << "] " << x << "\n" << ends; exit(-1);} 
+{ cerr << "ERROR: [" __FILE__ ":" << __FUNCTION__ << ",line: " << __LINE__ << "] " << x << "\n" << ends; exit(-1);} 
 
 /* }}} */
 
@@ -26,7 +26,7 @@ using namespace std;
 //---- Debug Level 1 ----
 #if (defined(DEBUGLEVEL_1) || defined(DEBUGLEVEL_2) || defined (DEBUGLEVEL_3) || defined(DEBUGLEVEL_4) || defined(DEBUGLEVEL_5)) 
 #define DEBUG_LOG1(x) \
-{ cerr << "[" __FILE__ ":" << __LINE__ << "] " << x << "\n" << ends; } 
+{ cerr << "[" __FILE__ ":" << __FUNCTION__ << ",line: " << __LINE__ << "] " << x << "\n" << ends; } 
 #else
 #define DEBUG_LOG1(x)
 #endif // DEBUGLEVEL 1
@@ -34,7 +34,7 @@ using namespace std;
 //---- Debug Level 2 ----
 #if (defined(DEBUGLEVEL_2) || defined(DEBUGLEVEL_3) || defined(DEBUGLEVEL_4) || defined(DEBUGLEVEL_5))
 #define DEBUG_LOG2(x) \
-{ cerr << "[" __FILE__ ":" << __LINE__ << "] " << x << "\n" << ends; } 
+{ cerr << "[" __FILE__ ":" << __FUNCTION__ << ",line: " << __LINE__ << "] " << x << "\n" << ends; } 
 #else
 #define DEBUG_LOG2(x)
 #endif // DEBUGLEVEL 2
@@ -42,7 +42,7 @@ using namespace std;
 //---- Debug Level 3 ----
 #if (defined(DEBUGLEVEL_3) || defined(DEBUGLEVEL_4) || defined(DEBUGLEVEL_5))
 #define DEBUG_LOG3(x) \
-{ cerr << "[" __FILE__ ":" << __LINE__ << "] " << x << "\n" << ends; }
+{ cerr << "[" __FILE__ ":" << __FUNCTION__ << ",line: " << __LINE__ << "] " << x << "\n" << ends; }
 #else
 #define DEBUG_LOG3(x)
 #endif // DEBUGLEVEL 3
@@ -50,7 +50,7 @@ using namespace std;
 //---- Debug Level 4 ----
 #if (defined(DEBUGLEVEL_4) || defined(DEBUGLEVEL_5))
 #define DEBUG_LOG4(x) \
-{ cerr << "[" __FILE__ ":" << __LINE__ << "] " << x << "\n" << ends; }
+{ cerr << "[" __FILE__ ":" << __FUNCTION__ << ",line: " << __LINE__ << "] " << x << "\n" << ends; }
 #else
 #define DEBUG_LOG4(x)
 #endif // DEBUGLEVEL 4
@@ -58,7 +58,7 @@ using namespace std;
 //---- Debug Level 5 ----
 #if (defined(DEBUGLEVEL_5))
 #define DEBUG_LOG5(x) \
-{ cerr << "[" __FILE__ ":" << __LINE__ << "] " << x << "\n" << ends; }
+{ cerr << "[" __FILE__ ":" << __FUNCTION__ << ",line: " << __LINE__ << "] " << x << "\n" << ends; }
 #else
 #define DEBUG_LOG5(x)
 #endif // DEBUGLEVEL 5
