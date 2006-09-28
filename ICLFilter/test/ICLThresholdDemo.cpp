36ea3cdbf28d9e9e37250ec66b281116b9582c50
Christof Elbrechter <c.elbrechter@gmx.de>
2006-09-28 16:25:41 +0000

    removed old ICLFilter/test/Test* applications

--- ICLFilter/test/ICLThresholdDemo.cpp
+++ ICLFilter/test/ICLThresholdDemo.cpp
@@ -1,88 +1,34 @@
 #include "Threshold.h"
-#include "Img.h"
 #include "FileWrite.h"
+#include "FileRead.h"
 
 using namespace icl;
 
-static char *apc[38]={ // 40 x 38
-  ".................................###....",
-  ".................................#####..",
-  "........##..#####........##......######.",
-  "...#######.............######.....######",
-  "...#####.............#########.....###..",
-  "...####........#.....##########.........",
-  "............#####...############........",
-  "...........#######.######aaa#####.......",
-  "..........########.###aaaaaaa#####......",
-  "..####....#########aaa.bbaaaab####......",
-  ".######..#######aaaaaa.bbaaaaab###......",
-  "..#####..####aaaaaaaaa.bbaaaaaab##......",
-  "#.##########a.aaaaaaaa.baaaaaaaab.......",
-  "#.#########aa.aaaaaaaaaaaaaaaaaaab..####",
-  "#.########a.a.aaaaaaaaaaaaaaaaaaaab.####",
-  "...######aa.a.a.aaaaaaaaaaaaaaaaabbb####",
-  ".#######a.a.a.a.a.aaaaaaaaaaaabbb#######",
-  "#######a..a.a.a.a.aaaaaaaaabbbbbb#######",
-  "######aaaaaaaaa.a.a.aaaabbbbbbbbb#######",
-  "#####a#a....aaaaaaabbbbbbbbbbbbbb..#####",
-  "#######a...........bbbbbbbbbbbbbb.......",
-  "#######a...........bbbbbbbbaabbbb.......",
-  "#######a......aaa..bbbbbaabaabbbb.......",
-  "#######a......a.a..bbaabaabaabbbbaaa....",
-  "####aaaa......aaa..bbaabaaaaabbbb.aaaaaa",
-  "aaaa...a...........bbaaaaabbbbbbb.....aa",
-  "aa.....a.aaa.......bbaabbbbbbbbbb......a",
-  ".......a.a...a.....bbbbbbbbbbbbbbaaa....",
-  ".......a.a..#a.....bbbbbbbbbbbbbbaaaaa..",
-  ".......a.a..#a.....bbbbbbbbbbbbbbaaaaaa.",
-  ".......a.a.##a.....bbbbbbbbbbbbbaaaaaaa.",
-  ".......a.a..#a.....bbbbbbbbbbaaaaaaaaa..",
-  ".......a.a..#a.....bbbbbbbaaaaaaaaaa....",
-  ".......aaaaa.a.....bbbbaaaaaaaaaaaa.....",
-  "............aaaaaaabaaaaaaaaaaaa........",
-  "........................................",
-  "........................................",
-  "........................................"};
+void xv(string s){
+  system(string("xv ").append(s).append("&").c_str());
+}
 
-int main(){
-  Img8u im(Size(40,38),1);
-  for(ImgIterator<icl8u> it = im.getIterator(0);it.inRegion();++it){
-    int x = it.x();
-    int y = it.y();
-    switch(apc[y][x]){
-      case '.': *it = 255; break;
-      case 'a': *it = 100; break;
-      case 'b': *it = 0; break;
-      default:  *it = 200; break;
-    }  
+int main(int nArgs, char **ppcArg){
+  if(nArgs < 3){
+    printf("to few arguments \n");
+    exit(-1);
   }
-
-  im.scale(Size(100,100));
+  string srcName = ppcArg[1];
+  string dstName = ppcArg[2];
   
-  Size s = im.getSize();
-  Img8u t(Size(s.width*7,s.height),1);
-  int i=0;
+  // READ the image
+  ImgI *image = FileRead(srcName).grab();
+  ImgI *dst   = 0;
 
-  t.setROI(Rect((i++)*s.width,0,100,100));
-  im.deepCopyROI(&t);
-  t.setROI(Rect((i++)*s.width,0,100,100));
+  // Perform binarization
+  Threshold::binarize(image,&dst,127);
+  
+  // WRITE the image
+  FileWrite(dstName).write(dst);
 
-  Threshold::lt(&im, &t, 150); 
-  t.setROI(Rect((i++)*s.width,0,100,100));
-  Threshold::gt(&im, &t, 150); 
-  t.setROI(Rect((i++)*s.width,0,100,100));
-  Threshold::ltgt(&im, &t, 100, 200); 
-  t.setROI(Rect((i++)*s.width, 0,100,100));
-  Threshold::ltVal(&im, &t, 150, 0); 
-  t.setROI(Rect((i++)*s.width,0,100,100));
-  Threshold::gtVal(&im, &t, 150, 255); 
-  t.setROI(Rect((i++)*s.width,0,100,100));
-  Threshold::ltgtVal(&im, &t, 150, 0, 150, 255); 
+  // show images using xv
+  xv(srcName);
+  xv(dstName);  
 
-  FileWrite("./threshold_results.pgm").write(&t); system("xv ./threshold_results.pgm &"); 
-
-  printf("Original colors are [0,100,200,255]\n");
-  printf(": Image order is: \n");
-  printf("orig. - lt(150) - gt(150) - ltgt(100,200) - ltVal(150,0) - gtVal(150,255) - ltgtVal(150,0,150,255)\n");
   return 0;
 }
--- ICLFilter/test/filter.cpp
+++ ICLFilter/test/filter.cpp
@@ -1,6 +1,8 @@
 #include "Median.h"
 #include "Convolution.h"
 #include <stdio.h>
+#include <Timer.h>
+#include "Threshold.h"
 
 using namespace icl;
 
@@ -65,7 +67,39 @@ void test (icl::depth eDepth) {
    delete[] piKernel;
 }
 
+
+int testmy(){
+  Img8u src(Size(1000,1000),1),dst(Size(1000,1000),1);
+  Timer t;
+  printf("Threshold Benchmark!");
+  int N = 100;
+  int tetta = 77;
+
+  for(int i=0;i<N;i++){
+    for(Img8u::iterator s=src.getIterator(0),d=dst.getIterator(0);s.inRegion();++s,++d){
+      (*d) = (*s) > tetta ? 255 : 0;
+    }
+  }
+
+  t.start();
+  for(int i=0;i<N;i++){
+    for(Img8u::iterator s=src.getIterator(0),d=dst.getIterator(0);s.inRegion();++s,++d){
+      (*d) = (*s) > tetta ? 255 : 0;
+    }
+  }
+  t.stopSubTimer();
+
+  for(int i=0;i<N;i++){
+    Threshold::lt(&src,&dst,tetta);
+  }
+  t.stop();
+
+
+  
+  return 0;
+}
 int main (int argc, char *argv[]) {
+  return testmy();
    test (depth8u);
    test (depth32f);
 }
