346af62fcd792eeabbd5e043271651e2d111a550
Christof Elbrechter <c.elbrechter@gmx.de>
2006-09-29 13:23:27 +0000

    readded the old ICLIO/test as ICLIO/examples

---
+++ ICLIO/examples/grabCam.cpp
@@ -0,0 +1,24 @@
+#include "Img.h"
+#include "PWCGrabber.h"
+#include "FileWrite.h"
+
+using namespace std;
+using namespace icl;
+
+int main() 
+{
+  //---- Allocate variables ----
+  PWCGrabber cam(Size(320,240));
+  
+  ImgI* poImg = imgNew(depth8u,Size(320,240));
+  FileWrite ioWrite("outImg","./demoImages","ppm");
+  
+  cam.grab(poImg);
+  ioWrite.write(poImg);
+  
+  cam.grab(poImg);
+  ioWrite.write(poImg);
+
+  cam.grab(poImg);
+  ioWrite.write(poImg);
+}
