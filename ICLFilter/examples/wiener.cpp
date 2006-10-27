5384af4f7b3cd3c7172b6fa8a0a7122e3790762f
ajustus <ajustus@techfak.uni-bielefeld.de>
2006-10-27 08:12:04 +0000

    example for Wiener filtering

---
+++ ICLFilter/examples/wiener.cpp
@@ -0,0 +1,44 @@
+#include "Wiener.h"
+#include "FileWrite.h"
+#include "FileRead.h"
+
+using namespace std;
+using namespace icl;
+using namespace std;
+
+void xv(string s){
+  system(string("xv ").append(s).append("&").c_str());
+}
+
+int main(int nArgs, char **ppcArg){
+  if(nArgs < 3){
+    printf("too few arguments \n");
+    printf("usage: %s srcfile dstfile\n", ppcArg[0]);
+    exit(-1);
+  }
+  string srcName = ppcArg[1];
+  string dstName = ppcArg[2];
+  
+  // READ the image
+	FileRead reader(srcName);
+  ImgI *image = reader.grab();
+  ImgI *dst   = 0;
+
+
+  // Perform binarization
+  icl32f* noise =new icl32f[1];
+  noise[0]=0.5;
+  Size masksize;
+  masksize.width=3;
+  masksize.height=3;
+  Wiener(masksize).FilterWiener(image,&dst,noise);
+  
+  // WRITE the image
+  FileWrite(dstName).write(dst);
+
+  // show images using xv
+  xv(srcName);
+  xv(dstName);  
+
+  return 0;
+}
