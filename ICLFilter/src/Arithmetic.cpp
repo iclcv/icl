4d45dc2001c6c47c543fe0e3aac7b2c236dace10
ajustus <ajustus@techfak.uni-bielefeld.de>
2006-11-08 19:34:30 +0000

    Added support for Arithmetic operations including Add, Sub, Mul, Div, AddC, SubC, MulC, DivC, AbsDiff, Sqr, Sqrt, Ln, Exp, Abs, AbsDiffC. In the moment Fallback Support only for Add, Sub, Mul, Div, AddC, SubC, MulC, DivC

---
+++ ICLFilter/src/Arithmetic.cpp
@@ -0,0 +1,401 @@
+#include "Arithmetic.h"
+#include "Img.h"
+
+namespace icl {
+
+#ifdef WITH_IPP_OPTIMIZATION
+
+  // {{{ ippi-function call templates
+
+  template <typename T, IppStatus (*ippiFunc) (const T*, int, T*, int, IppiSize)>
+  inline void ippi1srcCall(const Img<T> *src, Img<T> *dst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( src && dst );
+    ICLASSERT_RETURN( src->getROISize() == dst->getROISize() );
+    ICLASSERT_RETURN( src->getChannels()==dst->getChannels());
+    for (int c=src->getChannels()-1; c >= 0; --c) {
+      ippiFunc (src->getROIData (c), src->getLineStep(),
+                dst->getROIData (c), dst->getLineStep(),
+                dst->getROISize());
+    }
+  }
+  // }}}
+
+  template <typename T, IppStatus (*ippiFunc) (const T*, int, const T*, int, T*, int, IppiSize)>
+  inline void ippi2srcCall(const Img<T> *src1, const Img<T> *src2, Img<T> *dst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( src1 && src2 && dst );
+    ICLASSERT_RETURN( src1->getROISize() == src2->getROISize() );
+    ICLASSERT_RETURN( src1->getROISize() == dst->getROISize() );
+    ICLASSERT_RETURN( src1->getChannels()==src2->getChannels());
+    ICLASSERT_RETURN( src1->getChannels()==dst->getChannels());
+    for (int c=src1->getChannels()-1; c >= 0; --c) {
+      ippiFunc (src1->getROIData (c), src1->getLineStep(),
+                src2->getROIData (c), src2->getLineStep(),
+                dst->getROIData (c), dst->getLineStep(),
+                dst->getROISize());
+    }
+  }
+  // }}}
+
+
+  template <typename T, IppStatus (*ippiFunc) (const T*, int, const T, T*, int, IppiSize)>
+  inline void ippiCallC(const Img<T> *src, T value, Img<T> *dst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( src && dst );
+    ICLASSERT_RETURN( src->getROISize() == dst->getROISize() );
+    ICLASSERT_RETURN( src->getChannels()==dst->getChannels());
+    for (int c=src->getChannels()-1; c >= 0; --c) {
+      ippiFunc (src->getROIData (c), src->getLineStep(),
+                value,
+                dst->getROIData (c), dst->getLineStep(),
+                dst->getROISize());
+    }
+  }
+  template <typename T,typename R, IppStatus (*ippiFunc) (const T*, int, T*, int, IppiSize, R)>
+  inline void ippiCallAbsDiffC(const Img<T> *src, Img<T> *dst, R value)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( src && dst );
+    ICLASSERT_RETURN( src->getROISize() == dst->getROISize() );
+    ICLASSERT_RETURN( src->getChannels()==dst->getChannels());
+    for (int c=src->getChannels()-1; c >= 0; --c) {
+      ippiFunc (src->getROIData (c), src->getLineStep(),
+                dst->getROIData (c), dst->getLineStep(),
+                dst->getROISize(),
+                value);
+    }
+  }
+  // }}}
+
+  // {{{ function specializations
+  void Arithmetic::Add (const Img32f *src1, const Img32f *src2, Img32f *dst){
+    ippi2srcCall<icl32f,ippiAdd_32f_C1R>(src1,src2,dst);
+  }
+  void Arithmetic::Sub (const Img32f *src1, const Img32f *src2, Img32f *dst){
+    ippi2srcCall<icl32f,ippiSub_32f_C1R>(src1,src2,dst);
+  }
+  void Arithmetic::Mul (const Img32f *src1, const Img32f *src2, Img32f *dst){
+    ippi2srcCall<icl32f,ippiMul_32f_C1R>(src1,src2,dst);
+  }
+  void Arithmetic::MulScale (const Img8u  *src1, const Img8u  *src2, Img8u  *dst){
+    ippi2srcCall<icl8u,ippiMulScale_8u_C1R>(src1,src2,dst);
+  }
+  void Arithmetic::Div (const Img32f *src1, const Img32f *src2, Img32f *dst){
+    ippi2srcCall<icl32f,ippiDiv_32f_C1R>(src1,src2,dst);
+  }
+  void Arithmetic::AbsDiff (const Img32f *src1, const Img32f *src2, Img32f *dst){
+    ippi2srcCall<icl32f,ippiAbsDiff_32f_C1R>(src1,src2,dst);
+  }
+  void Arithmetic::AbsDiff (const Img8u *src1, const Img8u *src2, Img8u *dst){
+    ippi2srcCall<icl8u,ippiAbsDiff_8u_C1R>(src1,src2,dst);
+  }
+  void Arithmetic::Sqr (const Img32f *src, Img32f *dst){
+    ippi1srcCall<icl32f,ippiSqr_32f_C1R>(src,dst);
+  }
+  void Arithmetic::Sqrt (const Img32f *src, Img32f *dst){
+    ippi1srcCall<icl32f,ippiSqrt_32f_C1R>(src,dst);
+  }
+  void Arithmetic::Ln (const Img32f *src, Img32f *dst){
+    ippi1srcCall<icl32f,ippiLn_32f_C1R>(src,dst);
+  }
+  void Arithmetic::Exp (const Img32f *src, Img32f *dst){
+    ippi1srcCall<icl32f,ippiLn_32f_C1R>(src,dst);
+  }
+  void Arithmetic::Abs (const Img32f *src, Img32f *dst){
+    ippi1srcCall<icl32f,ippiAbs_32f_C1R>(src,dst);
+  }
+  void Arithmetic::AddC (const Img32f *src, const icl32f value, Img32f *dst){
+    ippiCallC<icl32f,ippiAddC_32f_C1R>(src,value,dst);
+  }
+  void Arithmetic::SubC (const Img32f *src, const icl32f value, Img32f *dst){
+    ippiCallC<icl32f,ippiSubC_32f_C1R>(src,value,dst);
+  }
+  void Arithmetic::MulC (const Img32f *src, const icl32f value, Img32f *dst){
+    ippiCallC<icl32f,ippiMulC_32f_C1R>(src,value,dst);
+  }
+  void Arithmetic::MulCScale (const Img8u *src, const icl8u value, Img8u *dst){
+    ippiCallC<icl8u,ippiMulCScale_8u_C1R>(src,value,dst);
+  }
+  void Arithmetic::DivC (const Img32f *src, const icl32f value, Img32f *dst){
+    ippiCallC<icl32f,ippiDivC_32f_C1R>(src,value,dst);
+  }
+  void Arithmetic::AbsDiffC (const Img8u *src, int value, Img8u *dst){
+    ippiCallAbsDiffC<icl8u,int,ippiAbsDiffC_8u_C1R>(src,dst,value);
+  }
+  void Arithmetic::AbsDiffC (const Img32f *src, icl32f value, Img32f *dst){
+    ippiCallAbsDiffC<icl32f,icl32f,ippiAbsDiffC_32f_C1R>(src,dst,value);
+  }
+  // }}}
+
+#else
+
+  // {{{ C++ fallback functions
+
+  template <typename T,class ArithmeticOp>
+  void fallbackArithmeticAdd(const Img<T> *src1, const Img<T> *src2, Img<T> *dst,const ArithmeticOp &op)
+    // {{{ open
+  {
+    ICLASSERT_RETURN( src1 && src2 && dst );
+    ICLASSERT_RETURN( src1->getROISize() == src2->getROISize() );
+    ICLASSERT_RETURN( src1->getROISize() == dst->getROISize() );
+    ICLASSERT_RETURN( src1->getChannels() == src2->getChannels() );
+    ICLASSERT_RETURN( src1->getChannels() == dst->getChannels() );
+    for(int c=src1->getChannels()-1; c >= 0; --c) {
+      ImgIterator<T> itSrc1 = const_cast<Img<T>*>(src1)->getROIIterator(c);
+      ImgIterator<T> itSrc2 = const_cast<Img<T>*>(src2)->getROIIterator(c);
+      ImgIterator<T> itDst = dst->getROIIterator(c);
+      for(;itSrc1.inRegion(); ++itSrc1, ++itSrc2, ++itDst){
+        *itDst = op(*itSrc1,*itSrc2);
+      }
+    }
+  }
+  // }}}
+
+  template <typename T,class ArithmeticOp>
+  void fallbackArithmeticAddC(const Img<T> *src, const T value, Img<T> *dst,const ArithmeticOp &op)
+    // {{{ open
+  {
+    ICLASSERT_RETURN( src && dst );
+    ICLASSERT_RETURN( src->getROISize() == dst->getROISize() );
+    ICLASSERT_RETURN( src->getChannels() == dst->getChannels() );
+    for(int c=src->getChannels()-1; c >= 0; --c) {
+      ImgIterator<T> itSrc = const_cast<Img<T>*>(src)->getROIIterator(c);
+      ImgIterator<T> itDst = dst->getROIIterator(c);
+      for(;itSrc.inRegion(); ++itSrc, ++itDst){
+        *itDst = op(*itSrc,value);
+      }
+    }
+  }
+  // }}}
+
+  // }}}
+
+  // {{{ C++ fallback Logical Operators
+
+  template <typename T> class AddOp {
+  public:
+    inline T operator()(T val1,T val2) const { return val1 + val2; }
+  };
+
+  template <typename T> class SubOp {
+  public:
+    inline T operator()(T val1,T val2) const { return val1 - val2; }
+  };
+  template <typename T> class MulOp {
+  public:
+    inline T operator()(T val1,T val2) const { return val1 * val2; }
+  };
+  template <typename T> class DivOp {
+  public:
+    inline T operator()(T val1,T val2) const { return val1 / val2; }
+  };
+  // }}}
+
+  // {{{ C++ fallback function specializations
+  void Arithmetic::Add (const Img32f *src1, const Img32f *src2, Img32f *dst)
+  {
+    fallbackArithmeticAdd<icl32f>(src1, src2,dst,AddOp<icl32f>());
+  }
+  void Arithmetic::Sub (const Img32f *src1, const Img32f *src2, Img32f *dst)
+  {
+    fallbackArithmeticAdd<icl32f>(src1, src2,dst,SubOp<icl32f>());
+  }
+  void Arithmetic::Mul (const Img32f *src1, const Img32f *src2, Img32f *dst)
+  {
+    fallbackArithmeticAdd<icl32f>(src1, src2,dst,MulOp<icl32f>());
+  }
+  void Arithmetic::Div (const Img32f *src1, const Img32f *src2, Img32f *dst)
+  {
+    fallbackArithmeticAdd<icl32f>(src1, src2,dst,DivOp<icl32f>());
+  }
+  
+  void Arithmetic::AddC (const Img32f *src, const icl32f value, Img32f *dst)
+  {
+    fallbackArithmeticAddC<icl32f>(src, value,dst,AddOp<icl32f>());
+  }
+  void Arithmetic::SubC (const Img32f *src, const icl32f value, Img32f *dst)
+  {
+    fallbackArithmeticAddC<icl32f>(src, value,dst,SubOp<icl32f>());
+  }
+  void Arithmetic::MulC (const Img32f *src, const icl32f value, Img32f *dst)
+  {
+    fallbackArithmeticAddC<icl32f>(src, value,dst,MulOp<icl32f>());
+  }
+  void Arithmetic::DivC (const Img32f *src, const icl32f value, Img32f *dst)
+  {
+    fallbackArithmeticAddC<icl32f>(src, value,dst,DivOp<icl32f>());
+  }
+
+  // }}}
+
+#endif
+
+  // {{{ ImgBase* versions
+  void Arithmetic::Add (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc1->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc1)) return;
+    Add(poSrc1->asImg<icl32f>(),poSrc2->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}  
+  void Arithmetic::Sub (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc1->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc1)) return;
+    Sub(poSrc1->asImg<icl32f>(),poSrc2->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+  void Arithmetic::Mul (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc1->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc1)) return;
+    Mul(poSrc1->asImg<icl32f>(),poSrc2->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+  void Arithmetic::Div (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc1->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc1)) return;
+    Div(poSrc1->asImg<icl32f>(),poSrc2->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+
+  void Arithmetic::AddC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    AddC(poSrc->asImg<icl32f>(),value,(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+  void Arithmetic::SubC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    SubC(poSrc->asImg<icl32f>(),value,(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+  void Arithmetic::MulC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    MulC(poSrc->asImg<icl32f>(),value,(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+  void Arithmetic::DivC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    DivC(poSrc->asImg<icl32f>(),value,(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+
+
+#ifdef WITH_IPP_OPTIMIZATION
+
+  void Arithmetic::MulScale (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc1->getDepth() == depth8u);
+    if (!Filter::prepare (ppoDst, poSrc1)) return;
+    MulScale(poSrc1->asImg<icl8u>(),poSrc2->asImg<icl8u>(),(*ppoDst)->asImg<icl8u>());
+  }
+  // }}}
+
+
+  void Arithmetic::MulCScale (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth8u);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    MulCScale(poSrc->asImg<icl8u>(),Cast<icl32f,icl8u>::cast(value),(*ppoDst)->asImg<icl8u>());
+  }
+  // }}}
+
+
+  void Arithmetic::Sqr (const ImgBase *poSrc, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    Sqr(poSrc->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+  void Arithmetic::Sqrt (const ImgBase *poSrc, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    Sqrt(poSrc->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+  void Arithmetic::Ln (const ImgBase *poSrc, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    Ln(poSrc->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+  void Arithmetic::Exp (const ImgBase *poSrc, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    Exp(poSrc->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+  void Arithmetic::Abs (const ImgBase *poSrc, ImgBase **ppoDst)
+  {
+    // {{{ open
+    ICLASSERT_RETURN( poSrc->getDepth() == depth32f);
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    Abs(poSrc->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+  }
+  // }}}
+
+  void Arithmetic::AbsDiff (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
+  {
+    // {{{ open
+    if (!Filter::prepare (ppoDst, poSrc1)) return;
+    switch (poSrc1->getDepth()) {
+      case depth8u: AbsDiff(poSrc1->asImg<icl8u>(),poSrc2->asImg<icl8u>(),(*ppoDst)->asImg<icl8u>()); break;
+      case depth32f: AbsDiff(poSrc1->asImg<icl32f>(),poSrc2->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>()); break;
+    };
+  }
+  // }}}
+
+  void Arithmetic::AbsDiffC (const ImgBase *poSrc, icl32f value, ImgBase **ppoDst)
+  {
+    // {{{ open
+    if (!Filter::prepare (ppoDst, poSrc)) return;
+    switch (poSrc->getDepth()) {
+      case depth8u: AbsDiffC(poSrc->asImg<icl8u>(),Cast<icl32f,int>::cast(value),(*ppoDst)->asImg<icl8u>()); break;
+      case depth32f: AbsDiffC(poSrc->asImg<icl32f>(),value,(*ppoDst)->asImg<icl32f>()); break;
+    };
+  }
+  // }}}
+
+
+#endif
+
+// }}}
+}
---
+++ ICLFilter/src/Arithmetic.h
@@ -0,0 +1,103 @@
+#ifndef ARITHMETIC_H
+#define ARITHMETIC_H
+
+#include "Filter.h"
+#include "Img.h"
+namespace icl {
+   /// Class for Arithmetic Functions
+   /** 
+       Supported operations include Add, Sub, Mul, Div, AddC, SubC, MulC, DivC, AbsDiff, Sqr, Sqrt, Ln, Exp, Abs, AbsDiffC.
+       All arithmetic operations except AbsDiff, AbsDiffC and MulScale are only supported on float typed images, i.e. icl32f, to avoid using a scaleFactor, avoiding icl8u overflows.
+       In the moment, there is no Fallback for AbsDiff, Sqr, Sqrt, Ln, Exp, Abs, AbsDiffC implemented.
+   */
+
+   class Arithmetic : public Filter {
+   public:
+      /// Adds pixel values of two images
+      static void Add (const Img32f  *src1, const Img32f  *src2, Img32f  *dst);
+      /// Subtracts pixel values of two images
+      static void Sub  (const Img32f  *src1, const Img32f  *src2, Img32f  *dst);
+      /// Multiplies pixel values of two images
+      static void Mul (const Img32f  *src1, const Img32f  *src2, Img32f  *dst);
+      /// Divides pixel values of two images
+      static void Div (const Img32f  *src1, const Img32f  *src2, Img32f  *dst);
+      /// Adds a constant to pixel values of an image
+      static void AddC (const Img32f *src, const icl32f value, Img32f *dst);
+      /// Subtracts a constant to pixel values of an image
+      static void SubC (const Img32f *src, const icl32f value, Img32f *dst);
+      /// Multiplies pixel values of an image by a constant
+      static void MulC (const Img32f *src, const icl32f value, Img32f *dst);
+      /// Divides pixel values of an image by a constant
+      static void DivC (const Img32f *src, const icl32f value, Img32f *dst);
+
+
+      /// Adds pixel values of two images - ImgBase Version
+      void Add (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+      /// Subtracts pixel values of two images - ImgBase Version
+      void Sub (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+      /// Multiplies pixel values of two images - ImgBase Version
+      void Mul (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+      /// Divides pixel values of two images - ImgBase Version
+      void Div (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+      /// Adds a constant to pixel values of an image - ImgBase Version
+      void AddC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst);
+      /// Subtracts a constant to pixel values of an image - ImgBase Version
+      void SubC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst);
+      /// Multiplies pixel values of an image by a constant - ImgBase Version
+      void MulC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst);
+      /// Divides pixel values of an image by a constant - ImgBase Version
+      void DivC (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst);
+
+
+#ifdef WITH_IPP_OPTIMIZATION
+      /// Squares pixel values of an image and writes them into the destination image
+      static void Sqr (const Img32f *src, Img32f *dst);
+      /// Computes square roots of pixel values of a source image and writes them into the destination image
+      static void Sqrt (const Img32f *src, Img32f *dst);
+      /// Computes the natural logarithm of pixel values in a source image and writes the results into the destination image
+      static void Ln (const Img32f *src, Img32f *dst);
+      /// Computes the exponential of pixel values in a source image and writes the results into the destination image
+      static void Exp (const Img32f *src, Img32f *dst);
+      /// Computes absolute pixel values of a source image and places them into the destination image
+      static void Abs (const Img32f *src, Img32f *dst);
+      /// Finds the absolute difference between an image and a scalar value.
+      static void AbsDiffC (const Img8u *src, int value, Img8u *dst);
+      /// Finds the absolute difference between an image and a scalar value.
+      static void AbsDiffC (const Img32f *src, icl32f value, Img32f *dst);
+
+      /// Multiplies pixel values of an image by a constant and scales the product
+      static void MulCScale (const Img8u *src, const icl8u value, Img8u *dst);
+      /// Multiplies pixel values of two images and scales the product, operates on icl8u
+      static void MulScale (const Img8u  *src1, const Img8u  *src2, Img8u  *dst);
+      /// Finds the absolute difference between two images
+      static void AbsDiff (const Img32f *src1, const Img32f *src2, Img32f *dst);
+      /// Finds the absolute difference between two images
+      static void AbsDiff (const Img8u *src1, const Img8u *src2, Img8u *dst);
+
+      /// Squares pixel values of an image and writes them into the destination image - ImgBase Version
+      void Sqr (const ImgBase *poSrc, ImgBase **ppoDst);
+      /// Computes square roots of pixel values of a source image and writes them into the destination image - ImgBase Version
+      void Sqrt (const ImgBase *poSrc, ImgBase **ppoDst);
+      /// Computes the natural logarithm of pixel values in a source image and writes the results into the destination image - ImgBase Version
+      void Ln (const ImgBase *poSrc, ImgBase **ppoDst);
+      /// Computes the exponential of pixel values in a source image and writes the results into the destination image - ImgBase Version
+      void Exp (const ImgBase *poSrc, ImgBase **ppoDst);
+      /// Computes absolute pixel values of a source image and places them into the destination image - ImgBase Version
+      void Abs (const ImgBase *poSrc, ImgBase **ppoDst);
+      /// Finds the absolute difference between two images - ImgBase Version
+      void AbsDiff (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+      /// Finds the absolute difference between an image and a scalar value. - ImgBase Version
+      void AbsDiffC (const ImgBase *poSrc, icl32f value, ImgBase **ppoDst);
+      /// Multiplies pixel values of an image by a constant and scales the product - ImgBase Version
+      void MulCScale (const ImgBase *poSrc, const icl32f value, ImgBase **ppoDst);
+      /// Multiplies pixel values of two images and scales the product, operates on icl8u - ImgBase Version
+      void MulScale (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+#endif
+
+
+
+
+   };
+} // namespace icl
+
+#endif
