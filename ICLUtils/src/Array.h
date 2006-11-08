5ef04d4cfa05f31d0993222d49000da684f9c56b
Christof Elbrechter <c.elbrechter@gmx.de>
2006-11-08 22:49:38 +0000

    added the ICLUtils/Array class, which is an extension of the std::vector, that provides additionally access to the data pointer. Its resize function does only alloc new memory on demand. Most implementations of the std-library will do so, but in this way, the array implementation ensures this optimization.

---
+++ ICLUtils/src/Array.h
@@ -0,0 +1,25 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <vector>
+
+namespace icl{
+
+  template <class T>
+  class Array : public std::vector<T>{
+    public:
+    Array(unsigned int dim=0) : std::vector<T>(dim){}
+    Array(const Array &a){   *this = a; }    
+    const T *operator*() const{
+      if(!this->size())return 0;
+      return &((*this)[0]);
+    }
+    T *operator*(){
+      if(!this->size())return 0;
+      return &((*this)[0]);
+    }
+    
+  };
+
+}
+#endif
