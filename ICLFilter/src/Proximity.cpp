b3fcff728d090f423cfb8a996e908a2bdea3e915
ajustus <ajustus@techfak.uni-bielefeld.de>
2006-11-26 14:53:34 +0000

    Added comments, fixed a bug, experimentally support for interleaved images (discovered no difference to planar)

--- ICLFilter/src/Proximity.cpp
+++ ICLFilter/src/Proximity.cpp
@@ -1,5 +1,6 @@
 #include "Proximity.h"
 #include "Img.h"
+#include "ICLcc.h"
 
 namespace icl {
 
@@ -10,15 +11,18 @@ namespace icl {
    template <typename T, IppStatus (*ippiFunc) (const T*, int, IppiSize, const T*, int, IppiSize, icl32f*, int)>
    inline void ippiCall(const Img<T> *src1, const Img<T> *src2, Img32f *dst)
       // {{{ open
-   {  
+   { 
+ICLASSERT_RETURN( src1);	   
+	   ICLASSERT_RETURN( src2);
+	   ICLASSERT_RETURN( dst );
       ICLASSERT_RETURN( src1 && src2 && dst );
       ICLASSERT_RETURN( src1->getROISize() == dst->getROISize() );
       ICLASSERT_RETURN( src1->getChannels() == dst->getChannels() );
       ICLASSERT_RETURN( src1->getChannels() == src2->getChannels() );
-
+      Size srcROI=src1->getSize()-src2->getSize();
       for (int c=src1->getChannels()-1; c >= 0; --c) {
          ippiFunc (src1->getROIData (c), src1->getLineStep(),
-                   src1->getROISize(),
+                   srcROI,
                    src2->getROIData (c), src2->getLineStep(),
                    src2->getROISize(),
                    dst->getROIData (c), dst->getLineStep());
@@ -26,6 +30,73 @@ namespace icl {
    }
    // }}}
 
+     
+   template <IppStatus (*ippiFunc) (const icl8u*, int, IppiSize, const icl8u*, int, IppiSize, icl32f*, int)>
+   inline void ippiColorCall(const Img8u *src1, const Img8u *src2, Img32f *dst, Array<icl8u> m_oBuffer8u[2], Array<icl32f> m_oBuffer32f[3])
+      // {{{ open
+   { 
+      ICLASSERT_RETURN( src1);	   
+      ICLASSERT_RETURN( src2);
+      ICLASSERT_RETURN( dst );
+      ICLASSERT_RETURN( src1->getROISize() == dst->getROISize() );
+      ICLASSERT_RETURN( src1->getChannels() == dst->getChannels() );
+      ICLASSERT_RETURN( src1->getChannels() == src2->getChannels() );
+      m_oBuffer8u[0].resize(src1->getDim()*src1->getChannels());
+      m_oBuffer8u[1].resize(src2->getDim()*src2->getChannels());
+      m_oBuffer32f[2].resize(src1->getDim()*src1->getChannels());
+      planarToInterleaved(src1->asImg<icl8u>(), *(m_oBuffer8u[0]));
+      planarToInterleaved(src2->asImg<icl8u>(), *(m_oBuffer8u[1]));
+	   Size srcROI=src1->getSize()-src2->getSize();
+     ippiFunc (*m_oBuffer8u[0], src1->getLineStep(),
+                   srcROI,
+                   *m_oBuffer8u[1], src2->getLineStep(),
+                   src2->getROISize(),
+                   *m_oBuffer32f[2], dst->getLineStep());
+      interleavedToPlanar(*(m_oBuffer32f[2]),dst->getSize(),src1->getChannels(),dst->asImg<icl32f>());
+   }
+
+   template <IppStatus (*ippiFunc) (const icl32f*, int, IppiSize, const icl32f*, int, IppiSize, icl32f*, int)>
+   inline void ippiColorCall(const Img32f *src1, const Img32f *src2, Img32f *dst/*, Array<icl32f> *m_oBuffer32f*/)
+      // {{{ open
+   { 
+      ICLASSERT_RETURN( src1);	   
+      ICLASSERT_RETURN( src2);
+      ICLASSERT_RETURN( dst );
+      ICLASSERT_RETURN( src1->getROISize() == dst->getROISize() );
+      ICLASSERT_RETURN( src1->getChannels() == dst->getChannels() );
+      ICLASSERT_RETURN( src1->getChannels() == src2->getChannels() );
+	   Array<icl32f> m_oBuffer32f[4];
+      m_oBuffer32f[0].resize(src1->getDim()*src1->getChannels());
+      m_oBuffer32f[1].resize(src2->getDim()*src2->getChannels());
+      m_oBuffer32f[2].resize(src1->getDim()*src1->getChannels());  // ohne *2 absurz, seg fault, warum????
+      planarToInterleaved(src1->asImg<icl32f>(), *(m_oBuffer32f[0]));
+      planarToInterleaved(src2->asImg<icl32f>(), *(m_oBuffer32f[1]));
+	   Size srcROI=src1->getSize()-src2->getSize();
+	   //int ROIOffset=(src1->getSize().width*(src2->getSize().height-1)/2+(src2->getSize()-1)/2)*src1->getChannels();
+
+     ippiFunc (*m_oBuffer32f[0]/*+ROIOffset*/, src1->getLineStep()*src1->getChannels(),
+                   srcROI,
+                   *m_oBuffer32f[1], src2->getLineStep()*src2->getChannels(),
+                   src2->getROISize(),
+                   *m_oBuffer32f[2], src1->getLineStep()*src1->getChannels());
+      interleavedToPlanar(*(m_oBuffer32f[0]),dst->getSize(),src1->getChannels(),dst->asImg<icl32f>());
+      printf("7\n");
+	   /*for (int j=0;j<10;j++){
+      planarToInterleaved(dst->asImg<icl32f>(), *(m_oBuffer32f[0]));
+      interleavedToPlanar(*(m_oBuffer32f[0]),dst->getSize(),src1->getChannels(),dst->asImg<icl32f>());
+	   };*/
+   }
+
+   // }}}
+  
+   
+   
+   
+   
+   
+   
+   
+   
   // }}}
 
 
@@ -42,7 +113,7 @@ namespace icl {
       ippiCall<icl8u,ippiSqrDistanceValid_Norm_8u32f_C1R>(src1,src2,dst);
     }
 
-    void Proximity::CrossCorrFull_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst){
+    void Proximity::CrossCorrFull_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst,bool color){
       ippiCall<icl8u,ippiCrossCorrFull_Norm_8u32f_C1R>(src1,src2,dst);
     }
     void Proximity::CrossCorrSame_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst){
@@ -72,8 +143,17 @@ namespace icl {
       ippiCall<icl32f,ippiSqrDistanceValid_Norm_32f_C1R>(src1,src2,dst);
     }
 
-    void Proximity::CrossCorrFull_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst){
-      ippiCall<icl32f,ippiCrossCorrFull_Norm_32f_C1R>(src1,src2,dst);
+    void Proximity::CrossCorrFull_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst,bool color){
+      int ch=src1->getChannels();
+      if (!color){  
+        ch=1;
+      }
+      printf("CHans:%i\n",ch);
+      switch(ch){
+	      case 3:ippiColorCall<ippiCrossCorrFull_Norm_32f_C3R>(src1,src2,dst);break;
+        case 4:ippiColorCall<ippiCrossCorrFull_Norm_32f_C4R>(src1,src2,dst);break;
+        default:ippiCall<icl32f,ippiCrossCorrFull_Norm_32f_C1R>(src1,src2,dst);break;
+     }
     }
     void Proximity::CrossCorrSame_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst){
       ippiCall<icl32f,ippiCrossCorrSame_Norm_32f_C1R>(src1,src2,dst);
@@ -108,7 +188,7 @@ namespace icl {
       #warning "SqrDistanceValid_Norm is not implemented without IPP optimization";
     }
 
-    void Proximity::CrossCorrFull_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst){
+    void Proximity::CrossCorrFull_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst,bool color){
       #warning "CrossCorrFull_Norm is not implemented without IPP optimization";
     }
     void Proximity::CrossCorrSame_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst){
@@ -138,7 +218,7 @@ namespace icl {
       #warning "SqrDistanceValid_Norm is not implemented without IPP optimization";
     }
 
-    void Proximity::CrossCorrFull_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst){
+    void Proximity::CrossCorrFull_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst,bool color){
       #warning "CrossCorrFull_Norm is not implemented without IPP optimization";
     }
     void Proximity::CrossCorrSame_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst){
@@ -243,18 +323,29 @@ namespace icl {
    // }}}
 
 
-  void Proximity::CrossCorrFull_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
+  void Proximity::CrossCorrFull_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst,bool color)
       // {{{ open
   {
+	  printf("x\n");
     FUNCTION_LOG("");
+	    printf("f\n");
     ICLASSERT_RETURN( poSrc1->getDepth() == poSrc1->getDepth());
+	    printf("g\n");
+	  printf("y\n");
     if (!prepare (ppoDst, poSrc1,depth32f)) return;
-    if (poSrc1->getDepth () == depth8u)
-       CrossCorrFull_Norm(poSrc1->asImg<icl8u>(),poSrc2->asImg<icl8u>(),(*ppoDst)->asImg<icl32f>());
-    else
-       CrossCorrFull_Norm(poSrc1->asImg<icl32f>(),poSrc2->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>());
+printf("b\n");
+    ICLASSERT_RETURN( (*ppoDst)->getDepth() == depth32f);
+    printf("d\n");
+    if (poSrc1->getDepth() == depth8u)
+       CrossCorrFull_Norm(poSrc1->asImg<icl8u>(),poSrc2->asImg<icl8u>(),(*ppoDst)->asImg<icl32f>(), color);
+    else{
+	    printf("_\n");
+       CrossCorrFull_Norm(poSrc1->asImg<icl32f>(),poSrc2->asImg<icl32f>(),(*ppoDst)->asImg<icl32f>(),color);
+    }
+    printf("c\n");
   }
    // }}}
+
 
   void Proximity::CrossCorrValid_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst)
       // {{{ open
--- ICLFilter/src/Proximity.h
+++ ICLFilter/src/Proximity.h
@@ -3,61 +3,90 @@
 
 #include <Filter.h>
 #include "Img.h"
+#include "Array.h"
 namespace icl {
   
-  /// Class for Morphological operations
+  /// Class for computing proximity measures
+  /**  
+  The functions described in this section compute the proximity (similarity) measure between an
+image and a template (another image). These functions may be used as feature detection functions,
+as well as the components of more sophisticated techniques.
+There are several ways to compute the measure of similarity between two images. One way is to
+compute the Euclidean distance, or sum of the squared distances (SSD), of an image and a
+template. The smaller is the value of SSD at a particular pixel, the more similarity exists between
+the template and the image in the neighborhood of that pixel.
+  */
   class Proximity : public Filter {
   public:
-    void SqrDistanceFull_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
-    void SqrDistanceSame_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
-    void SqrDistanceValid_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
+    ///Computes normalized full Euclidean distance between an image and a template.
+    void SqrDistanceFull_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+    ///Computes normalized Euclidean distance between an image and a template.
+    void SqrDistanceSame_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+    ///Computes normalized valid Euclidean distance between an image and a template.
+    void SqrDistanceValid_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
 
-    void CrossCorrFull_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
-    void CrossCorrSame_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
-    void CrossCorrValid_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
+    ///Computes normalized full cross-correlation between an image and a template.
+    /**  impemented with support for interleaved, 3ch and 4ch images, may be removed
+    */
+    void CrossCorrFull_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst, bool color=true);
+    ///Computes normalized cross-correlation between an image and a template.
+    void CrossCorrSame_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+    ///Computes normalized valid cross-correlation between an image and a template.
+    void CrossCorrValid_Norm (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+  
+    ///Computes normalized full correlation coefficients between an image and a template.
+    void CrossCorrFull_NormLevel (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+    ///Computes normalized correlation coefficients between an image and a template.
+    void CrossCorrSame_NormLevel (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
+    ///Computes normalized valid correlation coefficients between an image and a template.  
+    void CrossCorrValid_NormLevel (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst);
 
-    void CrossCorrFull_NormLevel (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
-    void CrossCorrSame_NormLevel (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
-    void CrossCorrValid_NormLevel (const ImgBase *poSrc1, const ImgBase *poSrc2, ImgBase **ppoDst); //32f_C1R od. 8u32f_C1R
-  //protected:
-
-
-
+    ///Computes normalized full Euclidean distance between an image and a template. Img8u version.
     static void SqrDistanceFull_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst);
+    ///Computes normalized Euclidean distance between an image and a template. Img8u version.
     static void SqrDistanceSame_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst);
+    ///Computes normalized valid Euclidean distance between an image and a template. Img8u version.
     static void SqrDistanceValid_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst);
 
-    static void CrossCorrFull_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst);
+    ///Computes normalized full cross-correlation between an image and a template. Img8u version.
+    static void CrossCorrFull_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst, bool color=true);
+    ///Computes normalized cross-correlation between an image and a template. Img8u version.
     static void CrossCorrSame_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst);
+    ///Computes normalized valid cross-correlation between an image and a template. Img8u version.
     static void CrossCorrValid_Norm (const Img8u *src1, const Img8u *src2, Img32f *dst);
 
+    ///Computes normalized full correlation coefficients between an image and a template. Img8u version.
     static void CrossCorrFull_NormLevel (const Img8u *src1, const Img8u *src2, Img32f *dst);
+    ///Computes normalized correlation coefficients between an image and a template. Img8u version.
     static void CrossCorrSame_NormLevel (const Img8u *src1, const Img8u *src2, Img32f *dst);
+    ///Computes normalized valid correlation coefficients between an image and a template. Img8u version.
     static void CrossCorrValid_NormLevel (const Img8u *src1, const Img8u *src2, Img32f *dst);
 
+    ///Computes normalized full Euclidean distance between an image and a template. Img32f version.
     static void SqrDistanceFull_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst);
+    ///Computes normalized Euclidean distance between an image and a template. Img32f version.
     static void SqrDistanceSame_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst);
+    ///Computes normalized valid Euclidean distance between an image and a template. Img32f version.
     static void SqrDistanceValid_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst);
 
-    static void CrossCorrFull_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst);
+    ///Computes normalized full cross-correlation between an image and a template. Img32f version.
+    static void CrossCorrFull_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst, bool color=true);
+    ///Computes normalized cross-correlation between an image and a template. Img32f version.
     static void CrossCorrSame_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst);
+    ///Computes normalized valid cross-correlation between an image and a template. Img32f version.
     static void CrossCorrValid_Norm (const Img32f *src1, const Img32f *src2, Img32f *dst);
 
+    ///Computes normalized full correlation coefficients between an image and a template. Img32f version.
     static void CrossCorrFull_NormLevel (const Img32f *src1, const Img32f *src2, Img32f *dst);
+    ///Computes normalized correlation coefficients between an image and a template. Img32f version.
     static void CrossCorrSame_NormLevel (const Img32f *src1, const Img32f *src2, Img32f *dst);
+    ///Computes normalized valid correlation coefficients between an image and a template. Img32f version.
     static void CrossCorrValid_NormLevel (const Img32f *src1, const Img32f *src2, Img32f *dst);
-
-
-
-
-
-
-
-
-
-
-
-
+    private:
+  // buffer for 3ch and 4ch interleaved image, may be removed 
+  Array<icl8u> m_oBuffer8u[2];
+  // buffer for 3ch and 4ch interleaved image, may be removed 
+  Array<icl32f> m_oBuffer32f[3];
   };
 } // namespace icl
 #endif
