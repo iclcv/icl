779078f9799b9ae49b9e58b6888e2013e02693e9
Christof Elbrechter <c.elbrechter@gmx.de>
2006-12-01 13:19:53 +0000

    added documentation

--- ICLFilter/src/LUT.h
+++ ICLFilter/src/LUT.h
@@ -6,9 +6,15 @@
 #include <vector>
 
 namespace icl {
+
+  /// class for applying table lookup transformation to Img8u images
    class LUT : public Filter {
    public:
+     /// simple lut transformation dst(p) = lut(src(p))
      static void simple(Img8u *src, Img8u *dst, const std::vector<icl8u>& lut); 
+
+     /// specialization of a lut transformation to reduce the number colors levels image a given image
+     /** TODO: some more details */
      static void reduceBits(Img8u *src, Img8u *dst, icl8u levels);
    };
 } // namespace icl
