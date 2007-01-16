a753d8c29ed5531355f7366579ed221df4d433b7
Christof Elbrechter <c.elbrechter@gmx.de>
2007-01-16 17:06:49 +0000

    added the KMeans2D class of the ICLBlob package (not tested!) documentation follows soon !

---
+++ ICLBlob/src/KMeans2D.h
@@ -0,0 +1,32 @@
+#ifndef KMEANS2D_H
+#define KMEANS2D_H
+
+#include <vector>
+
+namespace icl{
+  class KMeans2D{
+    public:
+    static float euclNorm(float *a, float *b);
+    typedef std::vector<float*> vectorset;
+    typedef std::vector<int> intvec;
+    typedef float (*distfunc) (float*, float*);
+    
+    KMeans2D(int k, distfunc f=&KMeans2D::euclNorm);
+    void add(float x,float y);
+    void run(int maxSteps, float minQuantisationError=0, const vectorset &prototypes=vectorset());
+    void setK(int k);
+    void clear();
+
+    int getK() const;
+    float getError() const;
+    const vectorset &getPrototypes() const;
+
+    protected:
+    float m_fError;          /**< current quantisation error */
+    vectorset m_vecCBV;      /**< list of codebook vectors */
+    vectorset m_vecData;     /**< currently used data vectors */
+    distfunc m_funcDist;     /**< used distance functions */
+  };
+}
+
+#endif
