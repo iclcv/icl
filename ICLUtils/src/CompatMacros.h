0265a79138b1978bbae85463abec024e309203af
freinhar <freinhar@techfak.uni-bielefeld.de>
2007-01-30 10:39:43 +0000

    USE_MATH_DEFINES added

--- ICLUtils/src/CompatMacros.h
+++ ICLUtils/src/CompatMacros.h
@@ -3,6 +3,9 @@
 
 #ifdef WIN32
 #	define IPP_DECL __stdcall
+#	ifndef _USE_MATH_DEFINES
+#	define _USE_MATH_DEFINES
+#	endif
 #	define rint  ::System::Math::Round
 #	define round ::System::Math::Round
 #else
