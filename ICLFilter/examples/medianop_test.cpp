e98123391640409e302c78add8d9067d8d1ee8af
Christof Elbrechter <c.elbrechter@gmx.de>
2007-02-28 10:41:08 +0000

    added a test application for ICLFilter/MedianOp class

---
+++ ICLFilter/examples/medianop_test.cpp
@@ -0,0 +1,29 @@
+#include <MedianOp.h>
+#include <TestImages.h>
+#include <FileWriter.h>
+
+using namespace icl;
+using namespace std;
+
+int main(){
+  ImgBase *image = TestImages::create("parrot",formatRGB,depth8u);
+  
+  MedianOp mos[] = { 
+    MedianOp(Size(3,3)),
+    MedianOp(Size(5,5)),
+    MedianOp(Size(4,4)) 
+  };
+
+
+  for(int i=0;i<3;i++){
+    ImgBase *dst = 0;
+    mos[i].apply(image,&dst);
+    char buf[100];
+    sprintf(buf,"image_%d.ppm",i);
+    FileWriter(buf).write(dst);
+    delete dst;
+    dst = 0;
+  }
+  
+  return 0;
+}
