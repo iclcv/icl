9958368aacc83ad8ef84019f33f393fe75457920
Christof Elbrechter <c.elbrechter@gmx.de>
2007-03-01 09:06:08 +0000

    tested the logic example: where is that seg-fault ???

--- ICLFilter/examples/logic.cpp
+++ ICLFilter/examples/logic.cpp
@@ -46,23 +46,25 @@ printf("c\n");
 
 printf("e\n");
 
-/*
-  for (int i=1;i<3;i++){
-    uArith->setOpType((UnaryLogicalOp::optype)i);
-    
-    uArith->apply(&src,&dst);
-    printf("\nMode:%d <32f>\n",i);
-    for(ImgIterator<icl32f> it2 = (dst->asImg<icl32f>())->getIterator(0);it2.inRegion();++it2){
-      printf("%f,",*it2);
-    }
-    
-    uArith->apply(&src,&dst);
 
-      printf("\nMode:%d <8u>\n",i);
+  for (int i=1;i<3;i++){
+    uLogic->setOpType((UnaryLogicalOp::optype)i);
+    
+    /*
+        uLogic->apply(&src,&dst);
+        printf("\nMode:%d <32f>\n",i);
+        for(ImgIterator<icl32f> it2 = (dst->asImg<icl32f>())->getIterator(0);it2.inRegion();++it2){
+        printf("%f,",*it2);
+        }
+    */
+    
+    uLogic->apply(&src,&dst);
+
+    printf("\nMode:%d <8u>\n",i);
     for(ImgIterator<icl8u> it2 = (dst->asImg<icl8u>())->getIterator(0);it2.inRegion();++it2){
       printf("%d,",*it2);
     }
   }
-*/
+
    return 0;
 }
