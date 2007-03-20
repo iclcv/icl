500fcb1202708be8419a5195a5650ce329b0f2f1
ajustus <ajustus@techfak.uni-bielefeld.de>
2007-03-20 10:39:59 +0000

    updated example for logical operations

--- ICLFilter/examples/logic.cpp
+++ ICLFilter/examples/logic.cpp
@@ -9,21 +9,26 @@ int main(int nArgs, char **ppcArg){
   Img8u src(Size(2,2),1);
   Img8u srcb(Size(2,2),1);
   ImgIterator<icl8u> it = src.getIterator(0);
-  *it = 1; it++;
-  *it = 1; it++;
-  *it = 1; it++;
-  *it = 1;
+  *it = 255; it++;
+  *it = 255; it++;
+  *it = 0; it++;
+  *it = 0;
   
   it = srcb.getIterator(0);
-  *it = 1; it++;
-  *it = 1; it++;
-  *it = 1; it++;
-  *it = 1;
+  *it = 255; it++;
+  *it = 0; it++;
+  *it = 255; it++;
+  *it = 0;
 
   
   
-    printf("Original Data\n");
+    printf("Original Data A\n");
   for(ImgIterator<icl8u> it2 = src.getIterator(0);it2.inRegion();++it2){
+    printf("%d,",*it2);
+  }
+  printf("\n");
+	   printf("Original Data B\n");
+  for(ImgIterator<icl8u> it2 = srcb.getIterator(0);it2.inRegion();++it2){
     printf("%d,",*it2);
   }
   printf("\n");
@@ -46,25 +51,59 @@ printf("c\n");
 
 printf("e\n");
 
+printf ("Mode\n0==AND\n1==OR\n2==XOR\n3==NOT Using Unary, Data A, val:0/n");
 
-  for (int i=1;i<3;i++){
+  for (int i=0;i<4;i++){
     uLogic->setOpType((UnaryLogicalOp::optype)i);
-    
-    /*
-        uLogic->apply(&src,&dst);
-        printf("\nMode:%d <32f>\n",i);
-        for(ImgIterator<icl32f> it2 = (dst->asImg<icl32f>())->getIterator(0);it2.inRegion();++it2){
-        printf("%f,",*it2);
-        }
-    */
-    
+   
     uLogic->apply(&src,&dst);
-
     printf("\nMode:%d <8u>\n",i);
     for(ImgIterator<icl8u> it2 = (dst->asImg<icl8u>())->getIterator(0);it2.inRegion();++it2){
       printf("%d,",*it2);
     }
+		printf("\n");
   }
+
+printf ("Mode\n0==AND\n1==OR\n2==XOR\n3==NOT Using Unary, Data A, val:255/n");
+	uLogic->setValue(255);
+  for (int i=0;i<4;i++){
+    uLogic->setOpType((UnaryLogicalOp::optype)i);
+
+    uLogic->apply(&src,&dst);
+    printf("\nMode:%d <8u>\n",i);
+    for(ImgIterator<icl8u> it2 = (dst->asImg<icl8u>())->getIterator(0);it2.inRegion();++it2){
+      printf("%d,",*it2);
+    }
+    printf("\n");
+  }
+	uLogic->setValue(0);
+printf ("Mode\n0==AND\n1==OR\n2==XOR\n3==NOT Using Unary, Data B, val:0/n");
+
+  for (int i=0;i<4;i++){
+    uLogic->setOpType((UnaryLogicalOp::optype)i);
+
+    uLogic->apply(&srcb,&dst);
+    printf("\nMode:%d <8u>\n",i);
+    for(ImgIterator<icl8u> it2 = (dst->asImg<icl8u>())->getIterator(0);it2.inRegion();++it2){
+      printf("%d,",*it2);
+    }
+    printf("\n");
+  }
+
+printf ("Mode\n0==AND\n1==OR\n2==XOR\n3==NOT Using Unary, Data B, val:255/n");
+  uLogic->setValue(255);
+  for (int i=0;i<4;i++){
+    uLogic->setOpType((UnaryLogicalOp::optype)i);
+
+    uLogic->apply(&srcb,&dst);
+    printf("\nMode:%d <8u>\n",i);
+    for(ImgIterator<icl8u> it2 = (dst->asImg<icl8u>())->getIterator(0);it2.inRegion();++it2){
+      printf("%d,",*it2);
+    }
+    printf("\n");
+  }
+
+
 
    return 0;
 }
