92a552272335cb59f4d3b5bfd53aa58aba733ea7
Christof Elbrechter <c.elbrechter@gmx.de>
2007-04-02 12:44:39 +0000

    added some new grabber sources

---
+++ ICLIO/src/iclDefaultGrabEngine.cpp
@@ -0,0 +1,60 @@
+#include <iclDefaultGrabEngine.h>
+#include <iclUnicapDevice.h>
+#include <iclStackTimer.h>
+
+namespace icl{
+  DefaultGrabEngine::DefaultGrabEngine(UnicapDevice *device, bool useDMA):
+    m_poDevice(device), m_iCurrBuf(0), m_bUseDMA(useDMA), m_bStarted(false){
+    
+    UnicapFormat fmt = m_poDevice->getCurrentUnicapFormat();
+    for(int i=0;i<NBUFS;i++){
+      m_oBuf[i].buffer_size = useDMA ? 0 : fmt.getBufferSize();
+      m_oBuf[i].data = useDMA ? 0 :new unsigned char[fmt.getBufferSize()];
+      m_oBuf[i].type = useDMA ? UNICAP_BUFFER_TYPE_SYSTEM : UNICAP_BUFFER_TYPE_USER;
+      fmt.getUnicapFormat()->buffer_type = useDMA ? UNICAP_BUFFER_TYPE_SYSTEM : UNICAP_BUFFER_TYPE_USER;
+    }
+    device->setFormat(fmt);
+  }
+  DefaultGrabEngine::~DefaultGrabEngine(){
+    unicap_stop_capture (m_poDevice->getUnicapHandle()); 
+    for(int i=0;i<NBUFS;i++){
+      if(m_oBuf[i].type == UNICAP_BUFFER_TYPE_USER && m_oBuf[i].data){
+        delete [] m_oBuf[i].data;
+      }
+    }    
+  }
+  
+  void DefaultGrabEngine::lockGrabber(){ 
+    // no longer needed because of using double buffering
+  }
+  void DefaultGrabEngine::unlockGrabber(){ 
+    // no longer needed because of using double buffering
+  }
+  
+  const icl8u *DefaultGrabEngine::getCurrentFrameUnconverted(){
+    BENCHMARK_THIS_FUNCTION;
+    
+    if(!m_bStarted){
+      unicap_start_capture(m_poDevice->getUnicapHandle());
+      m_bStarted = true;
+      unicap_queue_buffer(m_poDevice->getUnicapHandle(),&m_oBuf[NEXT_IDX()]);
+    }
+    
+    unicap_data_buffer_t *returned_buffer;
+    static const int MAX_TRYS = 10;
+    static const long WAIT_TIME = 100000;
+    int i=0;
+    for(;i<MAX_TRYS;i++){
+      if( SUCCESS (unicap_wait_buffer (m_poDevice->getUnicapHandle(), &returned_buffer))){
+        break;
+      }else{
+        usleep(WAIT_TIME);
+      }
+    }if(i==MAX_TRYS){
+      ERROR_LOG("Failed to wait for the buffer to be filled! ( tried "<<MAX_TRYS<<" times)");        
+    }
+    unicap_queue_buffer(m_poDevice->getUnicapHandle(),&m_oBuf[NEXT_IDX()]);
+        
+    return returned_buffer->data;
+  }
+}
