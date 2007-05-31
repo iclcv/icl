56a54d16e7189f990b3bcb59f160d0636ad2da3e
hhobein <hhobein@techfak.uni-bielefeld.de>
2007-05-31 15:42:38 +0000

    -added iclCamThread stuff for christof

--- ICLQt/examples/twopwccamtest2.cpp
+++ ICLQt/examples/twopwccamtest2.cpp
@@ -1,4 +1,5 @@
 #include <iclDrawWidget.h>
+#include <iclCamThread.h>
 #include <iclImg.h>
 #include <iclImgBase.h>
 #include <iclPWCGrabber.h>
@@ -9,52 +10,22 @@
 #include <QGridLayout>
 #include <QPushButton>
 #include <iclTimer.h>
-
-int w=320;
-int h=240;
+#include <QTimer>
 
 using namespace icl;
 using namespace std;
-class CamThread : public QThread {
-public:
-  CamThread():widget(new ICLDrawWidget(0)),
-              widget2(new ICLDrawWidget(0)),
-              grabber2(new PWCGrabber(Size(w,h),30,0)),
-              grabber(new PWCGrabber(Size(w,h),30,1)) {
-    //    grabber->setDesiredSize(Size(w,h));
-    //grabber2->setDesiredSize(Size(w,h));
-    widget->setGeometry(200,200,w,h);
-    widget2->setGeometry(200+w+50,200,w,h);
-    widget->show();
-    widget2->show();
-  }
-  
-  virtual void run(){
-    while(1){
-      const ImgBase *grabbedImage = grabber->grab();
-      const ImgBase *grabbedImage2 = grabber2->grab();
-
-      widget->setImage(grabbedImage);
-      widget->update();
-      widget2->setImage(grabbedImage2);
-      widget2->update();
-    }
-  }
-  
-private:
-  ICLDrawWidget *widget;
-  ICLDrawWidget *widget2;
-
-  PWCGrabber *grabber2;
-  PWCGrabber *grabber;
-};
-
 
 
 //int main(int nArgs, char **ppcArg){
 int main(int nArgs, char *ppcArg[]){
   QApplication a(nArgs,ppcArg);
-  CamThread x;
-  x.start();
+  CamThread x(0);
+  CamThread y(1);
+  QTimer t;
+  QObject::connect(&t,SIGNAL(timeout()),&x,SLOT(update()));
+  QObject::connect(&t,SIGNAL(timeout()),&y,SLOT(update()));
+  
+  t.start(100);
+  
   return a.exec();
 }
---
+++ ICLQt/src/iclCamThread.cpp
@@ -0,0 +1,1 @@
+#include <iclCamThread.h>
