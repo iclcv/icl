726781a80fcdb9f6ec0b97f9c779b1348f7ef768
Christof Elbrechter <c.elbrechter@gmx.de>
2007-08-14 16:11:44 +0000

    added some documentation (not finished) including some screenshots

--- ICLQt/src/iclGUI.h
+++ ICLQt/src/iclGUI.h
@@ -10,6 +10,7 @@
 #include <iclWidget.h>
 #include <iclDrawWidget.h>
 
+#include <QApplication>
 
 /** \cond */
 class QLayout;
@@ -23,9 +24,93 @@ namespace icl{
   class GUIWidget;
   /** \endcond */
 
+  /// Simple but powerful GUI Toolkit for the ICL
+  /** \section INTRO Introduction
+      When working on computer-vision tasks, one whats to have some graphical user interface 
+      to show the current images. This can be achieved by the use of the ICLQt's ICLWidget and 
+      ICLDrawWidget classes. \n
+      However in many cases this GUI components are not enough, e.g.:
+      - a value shall be adjusted at rumtime (using a slider, a spinbox or a simple textfield)
+      - a mode shall be swiched (using a radiobutton group or a combobox)
+      - the application shall have a "pause"-button which stops the iteration thread temporarily
+      - ...
+      
+      Actually all this capabilities can be implemented using the powerful Qt-Framework. But
+      you will encounter some tricky problems then:
+      - You must syncronize Qt's even-loop with the working thread
+      - You have to handle user interaction using Qt's slots and signals
+      - You have to create QObject classes using the Q_OBJECT macro and run Qt's meta-object-
+        compiler (moc) on it. (Yet this isn't possible inside of an "expamples" or "application"-
+        folder of the ICL makefile structure)
+      - ...
+      - And not at least: You have to layout your GUI using QLayouts, QWidgets and QSizePolicys
+      
+      Of course, a Qt-nerd will say "OK, but where is the problem!", but most of the ICL- users
+      including me long for a framwork that allows you to "create a slider, and access its current
+      value" with not more than 5 lines of additional code.\n
+      The new ICL-GUI API enables you to create a slider with one expression, and to access its value
+      with another expression. The following example will demonstrate this:
+      \code
+      #include <iclGUI.h>
+      using namespace icl;
+      int main(int n, char**ppc){
+        QApplication app(n,ppc);
+        GUI g("slider(0,100,50)[@out=the slider value]");
+        g.show();
+      return app.exec();
+      }
+      \endcode
+      To access the current value of this slider from the working thread you just have to call:
+      \code
+      int val = g.getValue<int>("the slider value");
+      \endcode
+      This will show the following GUI (with the very beatiful gnome desktop) 
+      \image html Image01_ASlider.jpg
+      
+      \section CG Complex GUI's
+      Ok! now let us create some more complex GUI:
+      \code
+      #include <iclGUI.h>
+      #include <iclQuick.h>
+      
+      // create a nice image, which should be shown (using iclQuick)
+      Img8u image = cvt8u(scale(create("parrot"),0.2));
+      
+      int main(int n, char**ppc){
+        QApplication app(n,ppc);
 
-  /// GUI class 
-  /** TODO: Document this !! */
+        // create the top level gui component (a vertical layouted box here)
+        // gui objects can be filled with other objects using the "<<" operator
+        GUI gui("vbox");
+      
+        // add a slider to the hbox (range 0-100, current value=50)
+        gui << "slider(0,100,50)[@out=width]";
+
+        // add another slider
+        gui << "slider(0,100,50)[@out=height]";
+
+        // add an image component (ensure its size to be at least 10x10 cells 
+        // (a cell has a size of 15x15 pixels)  
+        gui << "image[@inp=image@minsize=10x10]";
+      
+        // show the top level gui (this will create all components internally
+        // and recursively. Before the top-level "show" function is called,
+        // the gui data is unaccessible
+        gui.show();
+      
+        // get the images drawing context (of type ICLWidget*) and induce it 
+        // to show a new image
+        gui.getValue<ICLWidget*>("image")->setImage(&image);
+      
+        // start Qt's event loop
+        return app.exec();
+      }
+      
+      \endcode
+      \image html Image02_FirstImage.jpg
+
+
+  */
   class GUI{
     public:
     static const int CELLW = 20;
