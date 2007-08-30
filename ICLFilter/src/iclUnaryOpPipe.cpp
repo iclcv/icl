c25e4ba652f8345db59e97a797cf5ce9b27ba180
Christof Elbrechter <c.elbrechter@gmx.de>
2007-08-30 09:09:17 +0000

    add a utility class called UnaryOpPipe to the ICLFilter package

---
+++ ICLFilter/src/iclUnaryOpPipe.cpp
@@ -0,0 +1,48 @@
+#include <iclUnaryOpPipe.h>
+#include <iclUnaryOp.h>
+#include <iclImgBase.h>
+
+namespace icl{
+  
+  UnaryOpPipe::UnaryOpPipe(){}
+  
+  UnaryOpPipe::~UnaryOpPipe(){
+    for(int i=0;i<getLength();i++){
+      delete ops[i];
+      delete ims[i];
+    }
+  }
+  
+  void UnaryOpPipe::add(UnaryOp *op, ImgBase*im){
+    ops.push_back(op);
+    ims.push_back(im);
+  }
+  
+  void UnaryOpPipe::apply(const ImgBase *src, ImgBase **dst){
+    int length = getLength();
+    switch(length){
+      case 0: ERROR_LOG("length must be > 0"); break;
+      case 1: getOp(0)->apply(src,dst);
+      default:
+        getOp(0)->apply(src,&getImage(0));
+        for(int i=1;i<length-1;i++){
+          getOp(i)->apply(getImage(i-1),&getImage(i));
+        }
+        getOp(length-1)->apply(getImage(length-2),dst);
+        break;
+    }
+  }
+  
+  int UnaryOpPipe::getLength() const {
+    return (int)ops.size();
+  }
+  ImgBase *&UnaryOpPipe::getLastImage() {
+    return ims[getLength()-1];
+  }
+  UnaryOp *&UnaryOpPipe::getOp(int i) {
+    return ops[i];
+  }
+  ImgBase *&UnaryOpPipe::getImage(int i) {
+    return ims[i];
+  }
+}
---
+++ ICLFilter/src/iclUnaryOpPipe.h
@@ -0,0 +1,104 @@
+#ifndef ICL_UNARY_OP_PIPE_H
+#define ICL_UNARY_OP_PIPE_H
+#include <vector>
+#include <iclUnaryOp.h>
+namespace icl{
+  
+  /** \cond */
+  class ImgBase;
+  /** \endcond */
+  
+  /// Utility class that helps applying UnaryOps one after another
+  /** Consider a default computer vision system, which has some
+      preprocessing steps in the following operation order:
+      -# resize the image to a given size
+      -# calculate a feature map (e.g. a weighted sum of the color channels)
+      -# binarize the feature map
+      -# apply some morphological operation on this feature map
+      
+      To facilitate this, you can easily use a UnaryOpPipe, which
+      internally creates a queue of UnaryOp instances and their
+      individual result images (as ImgBase*). Once you have created
+      this Pipe under a certain name (e.g. "MyProprocessor") you
+      can easily add or remove specific preprocessing steps, without
+      having to consider fall-outs elsewhere in your code.
+      
+      Take a look on the following example:
+      \code
+      #include <iclQuick.h>
+      #include <iclUnaryOpPipe.h>
+      #include <iclScaleOp.h>
+      #include <iclWeightedSumOp.h>
+      #include <iclUnaryCompareOp.h>
+      #include <iclMorphologicalOp.h>
+      
+      int main(){
+        // create an input image (the nice parrot here!)
+        ImgQ inputImage = create("parrot");
+      
+        // create a weight vector (used later on by an instance of the WeightedSumOp class)
+        vector<icl64f> weights(3); weights[0] = 0.2; weights[1] = 0.5; weights[0] = 0.3;
+      
+        // create a dilation mask (used later on by an instance of the MorphologicalOp class)
+        char mask[9] = {1,1,1,1,1,1,1,1,1};
+      
+        // create the empty pipe
+        UnaryOpPipe pipe;
+      
+        // add the UnaryOp's in the correct order
+        pipe.add(new ScaleOp(0.5,0.5));
+        pipe.add(new WeightedSumOp(weights));
+        pipe.add(new UnaryCompareOp(UnaryCompareOp::lt,128));
+        pipe.add(new MorphologicalOp(Size(3,3),mask,MorphologicalOp::dilate3x3));
+      
+        // apply this pipe on the source image (use the last image as destination)
+        pipe.apply(&inputImage,&pipe.getLastImage());
+      
+        // show the result
+        show(cvt(pipe.getLastImage()));
+      
+        return 0;
+      }
+      \endcode
+  **/
+  class UnaryOpPipe : public UnaryOp{
+    public:
+    /// create an empty pipe
+    UnaryOpPipe();
+    
+    /// Destructor
+    ~UnaryOpPipe();
+    
+    /// add a new op to the end of this pipe
+    void add(UnaryOp *op, ImgBase*im=0);
+    
+    /// applies all ops sequentially 
+    virtual void apply(const ImgBase *src, ImgBase **dst);
+    
+    /// returns the number of contained ops
+    int getLength() const;
+    
+    /// returns the op at given index
+    UnaryOp *&getOp(int i);
+    
+    /// returns the result buffer image at given index
+    ImgBase *&getImage(int i);
+    
+    /// returns the last image (which is not used by default)
+    /** This image is only used, if it is given a 2nd parameter to
+        the apply function
+        \code
+        MyPipe.apply(mySourceImagePtr,&(MyPipe.getLastImage());
+        \endcode
+    **/
+    ImgBase *&getLastImage();  
+    
+    private:
+    /// Internal buffer of ops
+    std::vector<UnaryOp*> ops;
+    
+    /// Internal buffer of result images
+    std::vector<ImgBase*> ims;
+  };
+}
+#endif
