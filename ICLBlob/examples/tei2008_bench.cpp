7d7cd5f5d81fd163de320b4cb9c7a9b5f37fac1a
Christof Elbrechter <c.elbrechter@gmx.de>
2007-10-02 11:00:26 +0000

    added a small benchmark example

---
+++ ICLBlob/examples/tei2008_bench.cpp
@@ -0,0 +1,142 @@
+#include <iclQuick.h>
+#include <iclPositionTracker.h>
+#include <iclThread.h>
+#include <iclGUI.h>
+
+#include <iclFPSEstimator.h>
+#include <iclArray.h>
+#include <iclStringUtils.h>
+#include <iclImgRegionDetector.h>
+#include <iclDCGrabber.h>
+
+typedef Array<int> vec;
+
+GUI gui;
+
+static inline vec getCenters(const Img8u &image, Img8u *dstRet){
+  static ImgRegionDetector rd;
+  rd.setRestrictions(10,10000,1,255);
+  
+  static Img8u dst(image.getSize(),formatGray);
+  
+  int w = image.getWidth();
+  int h = image.getHeight();
+  const int dim = w*h;
+  const icl8u *r = image.getData(0);
+  const icl8u *g = image.getData(1);
+  const icl8u *b = image.getData(2);
+  
+  
+  icl8u *d = dst.getData(0);
+  
+  for(int i=0;i<dim;++i){
+    d[i] = 255 * ( (r[i] > 100) && (g[i] < 100) && (b[i] < 100) );
+  }
+  
+  const std::vector< BlobData > &bd = rd.detect(&dst);
+  
+  static vec v;
+  v.clear();
+
+  for(unsigned int i=0;i<bd.size();++i){
+    Point p = bd[i].getCOG();
+    v.push_back(p.x);
+    v.push_back(p.y);
+  }
+  if(dstRet){
+    dst.deepCopy(dstRet);
+  }
+  return v;
+}
+
+Mutex datamutex;
+vec DATA;
+
+Mutex drawhandlemutex;
+static DrawHandle *dh;
+
+class WorkThreadA : public Thread{
+public:
+  WorkThreadA(){
+    vector<DCDevice> devs = DCGrabber::getDeviceList();
+    if(!devs.size()){
+      ERROR_LOG("no device found");
+      exit(-1);
+    }
+    grabber = new DCGrabber(devs[0]);
+    grabber->setDesiredSize(Size(640,480));
+    gui << "draw[@handle=image@minsize=32x24]";
+    gui << ( GUI("hbox") 
+             << "label()[@handle=l1@label=vision fps]"
+             << "label()[@handle=l2@label=linass fps]"
+           );
+    gui.show();
+    dh = &gui.getValue<DrawHandle>("image");
+  }
+  virtual void run(){
+
+    while(1){
+      static Img8u dst;
+      const ImgBase *image = grabber->grab();
+
+      vec v = getCenters(*(image->asImg<icl8u>()),&dst);
+
+      datamutex.lock();
+      DATA = v;
+      datamutex.unlock();
+
+      drawhandlemutex.lock();
+      (*dh) = &dst;
+      drawhandlemutex.unlock();
+
+
+      msleep(10);
+    }
+  }
+private:
+  DCGrabber *grabber;
+};
+
+class WorkThreadB : public Thread{
+public:
+  virtual void run(){
+    PositionTracker<int> pt;
+    while(1){
+
+      datamutex.lock();
+      vec v = DATA;
+      datamutex.unlock();
+
+      drawhandlemutex.lock();
+      ICLDrawWidget *w = **dh;
+      w->lock();       
+      w->reset();
+      if(v.size()){
+        pt.pushData(v.data(),v.size()/2);
+
+        w->color(255,0,0);
+        for(unsigned int i=0;i<v.size();i+=2){
+          w->sym(v[i],v[i+1],ICLDrawWidget::symCross);
+          static char buf[100];
+          w->text(toStr(pt.getID(i/2),buf),v[i],v[i+1],10);
+        }
+      }
+      w->unlock();
+      drawhandlemutex.unlock();
+      w->update();
+
+      msleep(10);        
+    }
+  }
+};
+
+
+int main(int n, char  **ppc){
+  QApplication app(n,ppc);
+  WorkThreadA a;
+  WorkThreadB b;
+  a.start();
+  b.start();
+  
+  return app.exec();
+}
