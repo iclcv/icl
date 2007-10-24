a120ab1398867b8320686e67b08f69cb8fd45949
Christof Elbrechter <c.elbrechter@gmx.de>
2007-10-24 13:16:12 +0000

    fixed some bugs in the LUTOp3Channel class (shift values > 0)

--- ICLFilter/src/iclLUTOp3Channel.cpp
+++ ICLFilter/src/iclLUTOp3Channel.cpp
@@ -1,10 +1,14 @@
 #include "iclLUTOp3Channel.h"
+#include <iclException.h>
+#include <math.h>
 
 namespace icl{  
-  
+
   template<class T> 
-  LUTOp3Channel<T>::LUTOp3Channel(Plugin *p):m_poPlugin(0){
-    m_oLUT = Img<T>(Size(1,16777216),1);
+  LUTOp3Channel<T>::LUTOp3Channel(Plugin *p,icl8u shift):m_poPlugin(0),m_ucShift(shift){
+    ICLASSERT_THROW(shift<8,ICLException("invalid shift value ( must be < 8 )"));
+    int dim = (int)pow(256>>m_ucShift,3);
+    m_oLUT = Img<T>(Size(dim,1),1);
     setPlugin(p);
   }
   
@@ -12,30 +16,50 @@ namespace icl{
   LUTOp3Channel<T>::~LUTOp3Channel(){
     setPlugin(0);
   }
-  
+
   namespace{
+    
+  
+    
 #define CAST(x) Cast<srcT,icl8u>::cast(x)
     template<class dstT, class srcT>
-    void apply_lut_op_3(const Img<srcT> &src, Img<dstT> &dst, dstT *lut){
+    void apply_lut_op_3(const Img<srcT> &src, Img<dstT> &dst, dstT *lut, icl8u shift){
+      const unsigned int fac1 = 256>>shift;
+      const unsigned int fac2 = fac1*fac1;
       if(src.hasFullROI() && dst.hasFullROI()){
         const srcT * pSrcR = src.getData(0); 
         const srcT * pSrcG = src.getData(1); 
         const srcT * pSrcB = src.getData(2);
         dstT *pDst = dst.getData(0);
         dstT *pDstEnd = pDst+dst.getDim();
-        while(pDst < pDstEnd){
-          *pDst++  = lut[ CAST(*pSrcR++) + 256*CAST(*pSrcG++) + 65536*CAST(*pSrcB++) ];
+
+        
+        if(shift){
+          while(pDst < pDstEnd){
+            *pDst++  = lut[ (CAST(*pSrcR++)>>shift) + (fac1*(CAST(*pSrcG++)>>shift)) + (fac2*(CAST(*pSrcB++)>>shift)) ];
+          }
+        }else{
+          while(pDst < pDstEnd){
+            *pDst++  = lut[ CAST(*pSrcR++) + 256*CAST(*pSrcG++) + 65536*CAST(*pSrcB++) ];
+          }
         }
+
       }else{
         ConstImgIterator<srcT> itSrcR= src.getROIIterator(0);        
         ConstImgIterator<srcT> itSrcG= src.getROIIterator(1);        
         ConstImgIterator<srcT> itSrcB= src.getROIIterator(2);        
         
         ImgIterator<dstT> itDst = dst.getROIIterator(0);
-        
-        while(itDst.inRegion()){
-          *itDst++  = lut[ CAST(*itSrcR++) + 256*CAST(*itSrcG++) + 65536*CAST(*itSrcB++) ];
-        }        
+        if(shift){
+          while(itDst.inRegion()){
+            *itDst++  = lut[ (CAST(*itSrcR++)>>shift) + (fac1*(CAST(*itSrcG++)>>shift)) + (fac2*(CAST(*itSrcB++)>>shift)) ];
+            
+          }        
+        }else{
+          while(itDst.inRegion()){
+            *itDst++  = lut[ CAST(*itSrcR++) + 256*CAST(*itSrcG++) + 65536*CAST(*itSrcB++) ];
+          }        
+        }
       }
     }
 #undef CAST
@@ -49,7 +73,7 @@ namespace icl{
       ERROR_LOG("unable to prepare output image");
     }
     switch(src->getDepth()){
-#define ICL_INSTANTIATE_DEPTH(D) case depth##D: apply_lut_op_3(*(src->asImg<icl##D>()), *((*dst)->asImg<T>()), m_oLUT.getData(0)); break;
+#define ICL_INSTANTIATE_DEPTH(D) case depth##D: apply_lut_op_3(*(src->asImg<icl##D>()), *((*dst)->asImg<T>()), m_oLUT.getData(0), m_ucShift); break;
       ICL_INSTANTIATE_ALL_DEPTHS;
 #undef ICL_INSTANTIATE_DEPTH
       default: ICL_INVALID_DEPTH;
@@ -62,13 +86,27 @@ namespace icl{
       if(m_poPlugin) delete m_poPlugin;
       m_poPlugin = p;
       T *lut = m_oLUT.getData(0);
-      for(int r = 0; r < 256; ++r){
-        for(int g = 0; g < 256; ++g){
-          for(int b = 0; b < 256; ++b){
-            lut[r+ 256*g + 65536*b] = p->transform( r,g,b );
+
+      if(m_ucShift){
+        const unsigned int fac1 = 256>>m_ucShift;
+        const unsigned int fac2 = fac1*fac1;
+        const unsigned int inc = pow(2,m_ucShift);// ? 2<<(m_ucShift-1) : 1;
+        for(int r = 0; r < 256; r+=inc){
+          for(int g = 0; g < 256; g+=inc){
+            for(int b = 0; b < 256; b+=inc){
+              lut[(r>>m_ucShift) + (fac1*(g>>m_ucShift)) + (fac2*(b>>m_ucShift)) ] = p->transform( r,g,b );
+            }
           }
-        }
-      } 
+        } 
+      }else{
+        for(int r = 0; r < 256; ++r){
+          for(int g = 0; g < 256; ++g){
+            for(int b = 0; b < 256; ++b){
+              lut[r+ 256*g + 65536*b] = p->transform( r,g,b );
+            }
+          }
+        } 
+      }
     }else{
       if(m_poPlugin){
         delete m_poPlugin;
--- ICLFilter/src/iclLUTOp3Channel.h
+++ ICLFilter/src/iclLUTOp3Channel.h
@@ -8,12 +8,12 @@
 
 namespace icl {
 
-  /// class for applying table look-up transformation to 3-channel integer-valued images \ingroup OTHER
+  /// class for applying table look-up transformation to 3-channel integer-valued images \ingroup UNARY
   /** In many applications it is necessary to create a feature map on 3-channel 
       input images. These feature map creation procedure can take much time,
       depending on the complexity of the calculated feature. In a very simple
       case for instance, the LUTOp3Channel can be used to create thresholded 
-      color-distance map using the the euclidian distance to a reference color and a
+      color-distance map using the the euclidean distance to a reference color and a
       threshold for each 
       pixel of the source image. This operation can be applied on each input
       (r,g,b)-Tuple by the following function (which is basically implemented in
@@ -46,7 +46,7 @@ namespace icl {
       \endcode
 
       When the LUT is created internally, it can be used for fast feature map calculation
-      using a simple table lookup:
+      using a simple table look-up:
       \code
       \#define CAST(x) Cast<srcT,icl8u>::cast(x)       /// !!
 
@@ -76,16 +76,48 @@ namespace icl {
       \section BENCH Benchmarks
       
       The default Plugins operation of threshold an implicitly created 
-      euclidian distance map takes about <b>18msec</b> on an 640x480 3-channel icl8u
+      euclidean distance map takes about <b>18msec</b> on an 640x480 3-channel icl8u
       input image. This can be accelerated by 61% to <b>11msec</b> (1.6Mhz Pentium M)
-      by using the LUTOp3Channel class. This benefit is not very hight, but the 
-      euclidian distance function is only one possible function
+      by using the LUTOp3Channel class. This benefit is not <em>very</em> high, but the 
+      euclidean distance function is only one possible function
       \f[ 
       f:R^3~\rightarrow~R
       \f]
-      which can be implemented using the lut table. Consider, that all of these
-      possibly very complex operations will not take more then the above mesured 
+      which can be implemented using the 3 channel lut. Consider, that all of these
+      possibly very complex operations will not take more then the above measured 
       <b>11msec</b>.
+
+      \section SHIFT Optimization "Shift"
+      In some applications, it is sufficient to use a decreased resolution for each
+      channel range. E.g. if we use only 7Bit per channel, we can reduce the LUT size
+      from \f$2^{24} \mbox{bytes} = 16.8\mbox{MB}\f$ to \f$2^{21} \mbox{bytes} = 2.1\mbox{MB}\f$
+      further reduction of the quantization level of each channel to 6Bit results in a
+      \f$2^{18} \mbox{bytes}=262\mbox{kB}\f$ LUT.\n
+      The benefit of this optimization is at first of course the saving of a large part of 
+      necessary memory, but in a second view, it also increases processing speed even beyond
+      the speed enhancement achieved using a LUT. The reason for the speed advantage
+      compared with the full size LUT can be explained by the cpu's cache management: When 
+      using a 16.8MB data array which is nearly randomly accessed, the data can not be 
+      cached permanently, so the cache flow in influenced negatively. The following table
+      illustrates some benchmark results using different bit shifts, compared on a 640x480 
+      rgb icl8u image.
+      
+      <table>
+      <tr> <td><b>Shift</b></td>  <td><b>LUT size</b></td> <td><b>Time*</b></td></tr>
+      <tr> <td>0 (no shift)</td>  <td>16.8MB</td>          <td>11ms</td></tr>
+      <tr> <td>1</td>             <td>2.2MB</td>           <td>8ms</td></tr>
+      <tr> <td>2</td>             <td>262kB</td>           <td>4.3ms</td></tr>
+      <tr> <td>3</td>             <td>33kB</td>            <td>3.5ms</td></tr>
+      <tr> <td>4</td>             <td>4kB</td>             <td>3.5ms</td></tr>
+      <tr> <td>5</td>             <td>512 Bytes</td>       <td>3.5ms</td></tr>
+      <tr> <td>6</td>             <td>64 Bytes</td>        <td>3.5ms</td></tr>
+      <tr> <td>7</td>             <td>8 Bytes</td>         <td>3.5ms</td></tr>
+      </table>
+      
+      A bit shift value of 2 seems to be the best compromise between data resolution 
+      vs. speed and not at least memory usage. <b>But note:</b> There are some applications,
+      where the data resolution can not be scaled down using bit shifts.
+      
   **/
   template<class T>
   class LUTOp3Channel : public UnaryOp, public Uncopyable {
@@ -104,7 +136,7 @@ namespace icl {
       /** @param ref1 first channel reference color value 
           @param ref2 second channel reference color value           
           @param ref3 third channel reference color value 
-          @param thresh euclidian distance threshold
+          @param thresh euclidean distance threshold
       */
       Plugin(int ref1, int ref2, int ref3, int thresh){
         m_aiRef[0] = ref1;
@@ -140,15 +172,17 @@ namespace icl {
       /// internal reference colors
       int m_aiRef[3];
 
-      /// euclidian distance threshold
+      /// euclidean distance threshold
       int m_iThresh;
     };
 
     /// creates a LUT object with given lut (LUT-mode)
     /** @param p plugin to use for the creation of the internal lut.
                  <b>Note:</b>the LUTOp3Channel takes the ownership
-                 of the given plugin */
-    LUTOp3Channel(Plugin *p=0);
+                 of the given plugin 
+        @param shift see \ref SHIFT
+    */
+    LUTOp3Channel(Plugin *p=0, icl8u shift=0);
     
     /// Destructor
     virtual ~LUTOp3Channel();
@@ -166,15 +200,23 @@ namespace icl {
         */
     void setPlugin(Plugin *p);
     
-    
-    
+    /// removes the internal plugin so it is not deleted with the LUTOp
+    Plugin *removePlugin(){
+      Plugin *p = m_poPlugin;
+      m_poPlugin = 0;
+      return p;
+    }
+
     private:
     
-    /// Image that hold the lut data
+    /// Image that holds the lut data
     Img<T> m_oLUT;
     
     /// Current plugin pointer
     Plugin *m_poPlugin;
+    
+    /// channel range increment (...)
+    icl8u m_ucShift;
   };
 } // namespace icl
 
