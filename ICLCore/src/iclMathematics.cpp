dd4e5198594b0f6f9c653fb0da3684ccb05c5ad8
Christof Elbrechter <c.elbrechter@gmx.de>
2007-11-26 14:25:08 +0000

    fixed gauss-random bug now we use the Num-Rec. version

--- ICLCore/src/iclMathematics.cpp
+++ ICLCore/src/iclMathematics.cpp
@@ -61,15 +61,23 @@ namespace icl{
   }
   
   //--------------------------------------------------------------------------
-  double gaussRandom(double mean, double var){
-    FUNCTION_LOG("");
-    double x1 = random(1.0);
-    double x2 = random(1.0);
-    
-    if(random(1.0) < 0.5){
-      return mean+var*var*sqrt(-2*var*log(x1))*cos(2*var*M_PI*x2);
-    }else{
-      return -(mean+var*var*sqrt(-2*var*log(x1))*cos(2*var*M_PI*x2));
+  double gaussRandom(double mu, double sigma){
+    static bool haveNextGaussian = false;
+    static double nextGaussian = 0;
+    if(haveNextGaussian){
+      haveNextGaussian = false;
+      return nextGaussian*sigma + mu;
+    } else{
+      double v1(0), v2(0), s(0);
+      do{
+        v1 = 2 * random(1.0)-1;
+        v2 = 2 * random(1.0)-1;
+        s = v1*v1 + v2*v2;
+      }while(s>=1 || s == 0);
+      double fac = sqrt(-2.0*log(s)/s);
+      nextGaussian = v2 * fac;
+      haveNextGaussian = true;
+      return v1 * fac * sigma + mu;
     }
   }   
 
