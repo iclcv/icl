41b1bf7ff4eda5c4e18c33330c796f47267487b3
Christof Elbrechter <c.elbrechter@gmx.de>
2008-01-08 10:16:28 +0000

    no changes but test of post-commit hoot

--- ICLIO/src/iclDCGrabber.h
+++ ICLIO/src/iclDCGrabber.h
@@ -120,6 +120,7 @@ namespace icl{
     /// Internally used buffer images
     ImgBase *m_poImage, *m_poImageTmp;
     
+
     /// Internally used image converter
     /** This converter is used, if the wrapped DCGrabberThread
         was not able to satisfy all desired parameter claims.*/
