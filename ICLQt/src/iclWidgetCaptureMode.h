c8306567ecc620f868a2ad0d578d4db4478b1878
Christof Elbrechter <c.elbrechter@gmx.de>
2008-04-16 13:14:16 +0000

    forgot to commit a file

---
+++ ICLQt/src/iclWidgetCaptureMode.h
@@ -0,0 +1,12 @@
+#ifndef ICL_WIDGET_CAPTURE_MODE_H
+#define ICL_WIDGET_CAPTURE_MODE_H
+
+namespace icl{
+  enum ICLWidgetCaptureMode{
+    capturingStarted,
+    capturingStopped,
+    capturingPaused
+  };  
+}
+
+#endif
