e6e47953a862017ba69dd5aab47b80b2e436d12b
Christof Elbrechter <c.elbrechter@gmx.de>
2008-04-18 18:55:37 +0000

    fixed remaining bugs in ICLDCDeviceFeatures and added documentation. TODO integrate this new class into the DCDevice class

--- ICLIO/src/iclDCDeviceFeatures.cpp
+++ ICLIO/src/iclDCDeviceFeatures.cpp
@@ -4,7 +4,11 @@
 #include <iclRange.h>
 namespace icl{
   
+
+  
   std::string str(dc1394feature_t t){
+    // {{{ open
+
 #define X(x) if(t==DC1394_FEATURE_##x) return #x;
     X(BRIGHTNESS);X(EXPOSURE);X(SHARPNESS);X(WHITE_BALANCE);X(HUE);
     X(SATURATION);X(GAMMA);X(SHUTTER);X(GAIN);X(IRIS);X(FOCUS);
@@ -14,7 +18,12 @@ namespace icl{
     return "undefined";
 #undef X
     }
+
+  // }}}
+
   template<> dc1394feature_t parse<dc1394feature_t>(const std::string &s){
+    // {{{ open
+
 #define X(x) if(s==#x) return DC1394_FEATURE_##x;      
     X(BRIGHTNESS);X(EXPOSURE);X(SHARPNESS);X(WHITE_BALANCE);X(HUE);
     X(SATURATION);X(GAMMA);X(SHUTTER);X(GAIN);X(IRIS);X(FOCUS);
@@ -25,12 +34,14 @@ namespace icl{
   }
 #undef X
 
-  
+  // }}}
+
   class DCDeviceFeaturesImpl{
   public:
     
-    
     DCDeviceFeaturesImpl(DCDevice &dev):dev(dev){
+      // {{{ open
+
       dc1394_feature_get_all(dev.getCam(),&features);
 
       for(int i=0;i<DC1394_FEATURE_NUM;++i){
@@ -41,9 +52,13 @@ namespace icl{
         }
       }
     }
+
+    // }}}
     ~DCDeviceFeaturesImpl(){}
 
     void setProperty(const std::string &name, const std::string &value){
+      // {{{ open
+
       std::vector<std::string> pl = getPropertyList();
       ICLASSERT_RETURN(find(pl.begin(),pl.end(),name) != pl.end());
       
@@ -74,6 +89,8 @@ namespace icl{
         dc1394_feature_set_value(dev.getCam(),info->id,info->value);
       }
     }
+
+    // }}}
     
     std::vector<std::string> getPropertyList(){
       // {{{ open
@@ -91,6 +108,7 @@ namespace icl{
               v.push_back(name);
             }
           }else if(modes.modes[i] == DC1394_FEATURE_MODE_AUTO){
+            // this is a hack -> we should ensure that mode manual is available too
             v.push_back(it->first+"-mode");            
           }
         }
@@ -100,13 +118,10 @@ namespace icl{
 
     // }}}
 
-    /**
-        - "range" the propertyis a double value in a given range 
-        - "value-list" the property is a double value in a list of possible values
-        - "menu" the property  is a string value in a list of possible values
-        - "command" property param has no additional parameters (this feature is 
-        **/
+ 
     std::string getType(const std::string &name){
+      // {{{ open
+
       dc1394feature_info_t *info = getInfoPtr(name);
       ICLASSERT_RETURN_VAL(info,"");
       
@@ -116,8 +131,12 @@ namespace icl{
         return "range"; 
       }
     }
+
+    // }}}
     
     std::string getInfo(const std::string &name){
+      // {{{ open
+
       dc1394feature_info_t *info = getInfoPtr(name);
       ICLASSERT_RETURN_VAL(info,"");
       if(name.length() > 5 && name.substr(name.length()-5)=="-mode"){
@@ -127,8 +146,12 @@ namespace icl{
       }
       return "";
     }
+
+    // }}}
     
     std::string getValue(const std::string &name){
+      // {{{ open
+
       if(name.length() > 5 && name.substr(name.length()-5)=="-mode"){
         dc1394feature_info_t *info = getInfoPtr(name);
         ICLASSERT_RETURN_VAL(info,"");
@@ -150,8 +173,12 @@ namespace icl{
       }
       return "";
     }
+
+    // }}}
     
     void show(){
+      // {{{ open
+
       printf("############## libdc1394 info : \n");
       for(std::map<std::string,dc1394feature_info_t*>::iterator it = featureMap.begin(); it != featureMap.end(); ++it){
         dc1394_feature_print (it->second,stdout);
@@ -169,17 +196,30 @@ namespace icl{
         printf("---------------------------\n");
       }
     }
+
+    // }}}
     
 
     
   private:
     dc1394feature_info_t *getInfoPtr(const std::string &name){
-      std::map<std::string,dc1394feature_info_t*>::iterator it = featureMap.find(name);
-      if(it != featureMap.end()){
-        return it->second;
+      // {{{ open
+
+      unsigned int l = name.length();
+      if(l > 5 && name.substr(l-5)=="-mode"){
+        return getInfoPtr(name.substr(0,l-5));
+      }else if(l > 3 && ((name.substr(l-3) == "_RV")||(name.substr(l-3)=="_BU"))){
+        return getInfoPtr(name.substr(0,l-3));
+      }else{
+        std::map<std::string,dc1394feature_info_t*>::iterator it = featureMap.find(name);
+        if(it != featureMap.end()){
+          return it->second;
+        }
       }
       return 0;
     }
+
+    // }}}
     
     DCDevice dev;
     dc1394featureset_t features;       
@@ -187,19 +227,82 @@ namespace icl{
   };
 
   void DCDeviceFeaturesImplDelOp::delete_func(DCDeviceFeaturesImpl *impl){
+    // {{{ open
+
     ICL_DELETE(impl);
   }
+
+  // }}}
   
   
   DCDeviceFeatures::DCDeviceFeatures(DCDevice &dev):
+    // {{{ open
+
     ParentSC(dev.isNull() ? 0 : new DCDeviceFeaturesImpl(dev)){}
+
+  // }}}
   
   DCDeviceFeatures::DCDeviceFeatures():
-    ParentSC(0){}
-  
+    // {{{ open
+
+     ParentSC(0){}
+
+  // }}}
+
   void DCDeviceFeatures::show() const{
+    // {{{ open
+
     ICLASSERT_RETURN(!isNull());
     impl->show();
   }
+
+  // }}}
+
+  void DCDeviceFeatures::setProperty(const std::string &name, const std::string &value){
+    // {{{ open
+
+    ICLASSERT_RETURN(!isNull());
+    impl->setProperty(name,value);
+  }
+
+  // }}}
+  
+  std::vector<std::string> DCDeviceFeatures::getPropertyList(){
+    // {{{ open
+
+    ICLASSERT_RETURN_VAL(!isNull(),std::vector<std::string>());
+    return impl->getPropertyList();
+  }
+
+  // }}}
+  
+  std::string DCDeviceFeatures::getType(const std::string &name){
+    // {{{ open
+
+    ICLASSERT_RETURN_VAL(!isNull(),"");
+    return impl->getType(name);
+  }
+
+  // }}}
+  
+  std::string DCDeviceFeatures::getInfo(const std::string &name){
+    // {{{ open
+
+    ICLASSERT_RETURN_VAL(!isNull(),"");
+    return impl->getInfo(name);
+  }
+
+  // }}}
+  
+  std::string DCDeviceFeatures::getValue(const std::string &name){
+    // {{{ open
+
+    ICLASSERT_RETURN_VAL(!isNull(),"");
+    return impl->getValue(name);
+  }
+
+  // }}}
+  
+
 }
 
--- ICLIO/src/iclDCDeviceFeatures.h
+++ ICLIO/src/iclDCDeviceFeatures.h
@@ -13,12 +13,67 @@ namespace icl{
     static void delete_func(DCDeviceFeaturesImpl *impl);
   };
   /** endcond */
-  
+
+  /// Utility class for handling DC-Device features \ingroup G_DC  
+  /** The DCDeviceFeautre class provides read/write access to the following features of
+      DC1394 devices (if supported by the camera):
+      - Brightness
+      - Sharpness
+      - white-balance (splitted in RV and BU component)
+      - hue
+      - saturation
+      - shutter
+      - gain
+      - iris
+      - focus
+      - temperature
+      - trigger (currently not fully supported!)
+      - trigger delay(currently not fully supported!)
+      - white shading
+      - frame rate
+      - zoom
+      - pan
+      - tilt
+      - optical filter
+      - capture size
+      - capture quality
+
+      
+      The class interface is adapted to the get/set Property interface of the ICL Grabber interface.
+      In addition, it is derived from the ShallowCopyable interface to allow cheap copying.
+  */
   class DCDeviceFeatures : public ShallowCopyable<DCDeviceFeaturesImpl,DCDeviceFeaturesImplDelOp> {
     public:
+    /// Base constructor create a null Object
     DCDeviceFeatures();
+
+    /// Default constructor with given DCDevice struct
     DCDeviceFeatures(DCDevice &dev);
+
+
+    /// Sets a property to a new value
+    /** call getPropertyList() to see which properties are supported 
+        @copydoc icl::Grabber::setProperty(const std::string&, const std::string&)
+    **/
+    void setProperty(const std::string &name, const std::string &value);
     
+    /// returns a list of properties, that can be set using setProperty
+    /** @return list of supported property names **/
+    std::vector<std::string> getPropertyList();
+ 
+    /// get type of property
+    /** \copydoc icl::Grabber::getType(const std::string &)*/
+    std::string getType(const std::string &name);
+    
+    /// get information of a properties valid values values
+    /** \copydoc icl::Grabber::getInfo(const std::string &)*/
+    std::string getInfo(const std::string &name);
+    
+    /// returns the current value of a given property
+    /** \copydoc icl::Grabber::getValue(const std::string &)*/
+    std::string getValue(const std::string &name);
+    
+    /// shows all available features and current values to std::out
     void show() const;
     
   };
