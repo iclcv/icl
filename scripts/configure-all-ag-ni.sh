1dfa45e275120763ae85ac3976058d7ed8eaedc8
Christof Elbrechter <c.elbrechter@gmx.de>
2008-09-30 14:57:06 +0000

    some minor bug fixes

--- scripts/configure-all-ag-ni.sh
+++ scripts/configure-all-ag-ni.sh
@@ -1,1 +1,1 @@
-./configure --with-SVS-Root=/vol/vision/SVS/4.2 --prefix=/home/celbrech/share/NEW-ICL-Test-Install --with-UNICAP-Root=/vol/video --with-LIBDC-Root=/vol/video
+./configure --with-SVS-Root=/vol/vision/SVS/4.2 --prefix=/vol/vision/ICL/4.0 --with-UNICAP-Root=/vol/video --with-LIBDC-Root=/vol/video
