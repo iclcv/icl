d267b9cf65dbf02f22ae8c4efa8ea403adb8e751
Christof Elbrechter <c.elbrechter@gmx.de>
2008-12-17 14:14:04 +0000

    added some additional configuration scripts for optimized, optimized with -march=native and for debug

---
+++ scripts/configure-all-ag-ni-native.sh
@@ -0,0 +1,1 @@
+CXXFLAGS="-march=native -O4 -funroll-loops" ./configure --with-SVS-Root=/vol/vision/SVS/4.2 --prefix=/vol/vision/ICL/4.0 --with-UNICAP-Root=/vol/video --with-LIBDC-Root=/vol/video --with-IPP-Root=/vol/vision/IPP/6.0
