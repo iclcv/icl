b1437ca9d127eda19f9ac0fe76d34e2945308ce5
Christof Elbrechter <c.elbrechter@gmx.de>
2009-01-22 21:02:23 +0000

    minor changes in the compile checker

--- scripts/compile_checker/main.cpp
+++ scripts/compile_checker/main.cpp
@@ -1,9 +1,10 @@
 #include "create_combinations.h"
 #include "show_arglists.h"
 
-
 int main(int n, char **ppc){
-  std::vector<arglist> l = create_combinations();
-  show_arglists(l,false);
+  //std::vector<arglist> l = create_combinations();
+  //show_arglists(l,false);
+  
+
   return 0;
 }
