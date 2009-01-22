ca431c94adca44b8db698dda20be05bff1f391fd
Christof Elbrechter <c.elbrechter@gmx.de>
2009-01-22 09:30:09 +0000

    oops forgot to add an example file

---
+++ ICLCore/examples/reduce-channels-test.cpp
@@ -0,0 +1,18 @@
+#include <iclImg.h>
+
+using namespace icl;
+
+void rgb2gray(const icl8u src[3], icl8u dst[1]) {
+  *dst = (src[0]+src[1]+src[2])/3;
+}
+
+int main(){
+  Img8u src(Size::VGA,formatRGB);  
+  Img8u dst(Size::VGA,formatGray);
+
+  src.reduce_channels<icl8u,3,1,void(*)(const icl8u src[3], icl8u dst[1])>(dst,rgb2gray);
+  
+
+  
+  
+}
