0f89b2788508a4accfb7da26d5758e79da5cf807
Christof Elbrechter <c.elbrechter@gmx.de>
2009-01-26 14:44:01 +0000

    readded the configure native script

---
+++ scripts/configure-all-ag-ni-native.sh
@@ -0,0 +1,3 @@
+CXXFLAGS="-O4 -funroll-loops -march=native" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0
+
+#test
