7959cb9b51c42d0f756c767d3e9136694f842dc9
Christof Elbrechter <c.elbrechter@gmx.de>
2009-03-16 19:38:20 +0000

    adapted agni configure scripts to use Qt from /vol/qt/4.5

--- scripts/configure-all-ag-ni-native.sh
+++ scripts/configure-all-ag-ni-native.sh
@@ -1,3 +1,3 @@
-CXXFLAGS="-O4 -funroll-loops -march=native" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0
+CXXFLAGS="-O4 -funroll-loops -march=native" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-QT-Root=/vol/qt/4.5
 
 #test
