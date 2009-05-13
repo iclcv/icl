bc6e913acebcc621569549dae1bfab8648ef389d
Christof Elbrechter <c.elbrechter@gmx.de>
2009-05-13 13:18:29 +0000

    added gzi configure-script

---
+++ scripts/configure-all-gzi.sh
@@ -0,0 +1,1 @@
+CXXFLAGS="-O4 -funroll-loops" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/gzi/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0
