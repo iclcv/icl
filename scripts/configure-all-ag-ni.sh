ff8779eaf0212885c9b24f54309315363cd898db
Christof Elbrechter <c.elbrechter@gmx.de>
2009-10-15 11:12:59 +0000

    fixed OpenCV dependency checking  * now OpenCV 1.x can also be used

--- scripts/configure-all-ag-ni.sh
+++ scripts/configure-all-ag-ni.sh
@@ -1,4 +1,4 @@
-CXXFLAGS="-O4 -funroll-loops" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-QT-Root=/vol/qt/4.5 --with-LIBMESASR-Root=/vol/nivision --with-XINE-Root=/vol/nivision --with-OPENCV-Root=/vol/nivision/OpenCV2
+CXXFLAGS="-O4 -funroll-loops" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-QT-Root=/vol/qt/4.5 --with-LIBMESASR-Root=/vol/nivision --with-XINE-Root=/vol/nivision
 
 
 #test
