664d3fcbf9ff9638d571ba0bedbf449553c566d6
Christof Elbrechter <c.elbrechter@gmx.de>
2009-10-19 09:56:47 +0000

    added a  tiny script, which automatically adds write permissions for all members of the nivision group by extracting the current install prefix from the ICL/Makefile.
    The script has to be executed from the ICL base directory!
    
    Whenever someone installs the ICL in the AGNI system, this
    script must be called afterwards, unless the users UMASK gives group users
    write permissions by default (e.g. 0002, which is of course not
    really recommended)

---
+++ scripts/fix-install-permissions-agni.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+V=`./VERSION.sh`
+P=`cat Makefile | grep 'prefix = ' | cut -d' ' -f3 | grep -v '${prefix}'`
+
+echo "VERSION is $V"
+echo "PREFIX is $P"
+echo ""
+echo "adding write permissions for group nivision using:"
+echo "> chmod -R g+w $P" 
+echo ""
+echo "please press enter if you want to succeed!"
+
+read ENTER
+
+if [ "$ENTER" = "" ] ; then
+    echo "please wait a sec ..."
+    chmod -R g+w $P ;
+    echo "permissions changed!" ;
+else
+    echo "aborted!" ;
+fi
