b17235597c40845d79ba03b66c74605d9fadb894
Christof Elbrechter <c.elbrechter@gmx.de>
2009-10-28 12:39:18 +0000

    added demo application for color spaces

---
+++ ICLCC/examples/colorspace-online-demo.cpp
@@ -0,0 +1,26 @@
+#include <iclCommon.h>
+
+GUI gui;
+
+void init(){
+  gui << "image[@handle=image@minsize=16x12]"
+      << "combo(Gray,RGB,HLS,YUV,LAB,Chroma,Matrix)"
+         "[@out=cs@label=color space@maxsize=100x3]";
+  
+  gui.show();
+}
+
+void run(){
+  gui_string(cs);
+  static GenericGrabber g(FROM_PROGARG("-input"));
+  g.setDesiredSize(Size::VGA);
+  g.setDesiredFormat(parse<format>(cs));
+  g.setIgnoreDesiredParams(false);
+  
+  gui["image"] = g.grab();
+  gui["image"].update();
+}
+
+int main(int n, char **args){
+  return ICLApplication(n,args,"-input(2)",init,run).exec();
+}
