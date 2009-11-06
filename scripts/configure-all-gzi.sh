e87057e0a85abaeead26c682777a1de29f3fd18b
Christof Elbrechter <c.elbrechter@gmx.de>
2009-11-06 14:55:08 +0000

    adapted gzi configure script (please note: we compile for 32bit here)

--- scripts/configure-all-gzi.sh
+++ scripts/configure-all-gzi.sh
@@ -1,1 +1,1 @@
-CXXFLAGS="-O4 -funroll-loops" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/gzi/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --without-IPP
+CXXFLAGS='-m32 -O3' LDDFLAGS=-m32 CFLAGS='-m32 -O3' LFLAGS=-m32 ./configure --prefix=/vol/nivision/ICL/gzi/4.5.1 --with-LIBDC-Root=/vol/nivision --with-XINE-Root=/vol/nivision --with-UNICAP-Root=/vol/nivision --without-IPP --without-MKL --without-SVS
