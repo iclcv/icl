be00c999bedfa2a2ffc64b0e1abb34f35700c9c6
Christof Elbrechter <c.elbrechter@gmx.de>
2009-11-09 14:10:42 +0000

    Added a combined documentation to the former one. In this new documentation, all packages are linked into. Furthermore the ICL-documentation side menu was upgraded to show an 'All Packages'-Link as first entry. The combined documentation is not built automatically in order to increase documentation build-speed.
    The combined documentation build-target is named alldoc

--- scripts/create-doc-menu.sh
+++ scripts/create-doc-menu.sh
@@ -1,12 +1,18 @@
 cd doc
 cp menu.html-template menu.html
+
 A="<!-- ICL_PROJECT_ENTRIES -->"
 B="<tr><td> <a href=\"../"
 C="\/index.html\" target=\"MainFrame\">"
 D="</a></td>\n"
+
+echo "creating entry menu entry for 'All Packages'"
+sed -i "s|$A|${B}ALL${C}All Packages$D$A|g" menu.html
+
 for T in $@ ; do 
-    echo "creating entry menu entry for package $T"
+    echo "creating menu entry for package $T"
     sed -i "s|$A|$B$T$C$T$D$A|g" menu.html
 done 
+
 
 # Old::        $menuTable .= "<tr><td> <a href=\"../".$project."/index.html\" target=\"MainFrame\">".$project."</a></td>\n";
