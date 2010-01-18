6fb62835249d8e6d061881dc33e6b81ecab403d4
Christof Elbrechter <c.elbrechter@gmx.de>
2010-01-18 17:55:56 +0000

    ICL5: changed configure files for

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-MYCONFIGURE="configure.ac-`uname -m`"
+autoreconf --install --force 
 
-if test -f $MYCONFIGURE; then  
-    echo "Preparing for `uname -m` architecture"
-    if test -L configure.ac; then
-        rm -f configure.ac
-    fi
-    ln -s $MYCONFIGURE configure.ac
-    autoreconf --install --force 
-else
-    echo "No configure.ac for `uname -m` architecture available"
-    echo "Aborting ..."
-    exit 1
-fi
+
+#MYCONFIGURE="configure.ac-`uname -m`"
+#
+#if test -f $MYCONFIGURE; then  
+#    echo "Preparing for `uname -m` architecture"
+#    if test -L configure.ac; then
+#        rm -f configure.ac
+#    fi
+#    ln -s $MYCONFIGURE configure.ac
+#
+#else
+#    echo "No configure.ac for `uname -m` architecture available"
+#    echo "Aborting ..."
+#    exit 1
+#fi
