ed48668045799c56b0642f617a4e8d1533f5867f
Christof Elbrechter <c.elbrechter@gmx.de>
2010-01-27 18:14:28 +0000

    adapted configure script for debug build

--- scripts/configure-all-ag-ni-debug.sh
+++ scripts/configure-all-ag-ni-debug.sh
@@ -1,1 +1,1 @@
-CXXFLAGS="-O0 -g3" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-QT-Root=/vol/qt/4.5
+CXXFLAGS="-O0 -g3" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh`-debug --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-LIBMESASR-Root=/vol/nivision --with-XINE-Root=/vol/nivision
