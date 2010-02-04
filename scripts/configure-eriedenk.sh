d06c557ea7af9884490cbde575876fff7dd4e839
Eckard Riedenklau <eriedenk@techfak.uni-bielefeld.de>
2010-02-04 08:09:39 +0000

    added personal config file.

---
+++ scripts/configure-eriedenk.sh
@@ -0,0 +1,4 @@
+CXXFLAGS="-O4 -funroll-loops" ./configure --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/usr --with-LIBDC-Root=/usr --with-IPP-Root=/vol/nivision/IPP/6.0 --with-XINE-Root=/usr
+
+
+#test
