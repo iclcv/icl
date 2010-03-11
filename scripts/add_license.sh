c0f6a1974ec8751e97dfa2598dcf0d02c04cc6d9
Christof Elbrechter <c.elbrechter@gmx.de>
2010-03-11 09:48:37 +0000

    adapted the license script:
    Now you can add the current license text to all files ...
    scripts/add_license -add -doit  
    
    .. and remove it:
    scripts/add_license -remove -doit

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
-#LICENSE_TEXT/********************************************************************************* 
-#LICENSE_TEXT**
-#LICENSE_TEXT** Copyright (C) 2006-2010 neuroinformatics group (vision)
-#LICENSE_TEXT** University of Bielefeld
-#LICENSE_TEXT** Contact: nivision@techfak.uni-bielefeld.de
-#LICENSE_TEXT**
-#LICENSE_TEXT** This file is part of the __ICL_PACKAGE__ module of ICL
-#LICENSE_TEXT**
-#LICENSE_TEXT** Commercial License
-#LICENSE_TEXT** Commercial usage of ICL is possible and must be negotiated with us.
-#LICENSE_TEXT** See our website www.iclcv.org for more details
-#LICENSE_TEXT**
-#LICENSE_TEXT** GNU General Public License Usage
-#LICENSE_TEXT** Alternatively, this file may be used under the terms of the GNU
-#LICENSE_TEXT** General Public License version 3.0 as published by the Free Software
-#LICENSE_TEXT** Foundation and appearing in the file LICENSE.GPL included in the
-#LICENSE_TEXT** packaging of this file.  Please review the following information to
-#LICENSE_TEXT** ensure the GNU General Public License version 3.0 requirements will be
-#LICENSE_TEXT** met: http://www.gnu.org/copyleft/gpl.html.
-#LICENSE_TEXT**
-#LICENSE_TEXT***********************************************************************************/ 
+# this script is used to add and to remove the ICL-license text from all files, 
+# IMPORTANT:
+# In case of removing the license text in order to change it before re-adding
+# the new text, always remove the text before changing it. The remove step is
+# a little simple: is simply counts the lines of the current license text and 
+# removes that much preceding lines from each file. If you already changed the
+# current license text, and the new version has more or less lines, you corrupt
+# all files without being able to fix it afterwards with this script
+
+#LICENSE_TEXT/*************************************************************************** 
+#LICENSE_TEXT**                                                                        **
+#LICENSE_TEXT** Copyright (C) 2006-2010 neuroinformatics group (vision)                **
+#LICENSE_TEXT**                         University of Bielefeld                        **
+#LICENSE_TEXT**                         nivision@techfak.uni-bielefeld.de              **
+#LICENSE_TEXT**                                                                        **
+#LICENSE_TEXT** This file is part of the __ICL_PACKAGE__ module of ICL __ICL_SPACE__   **
+#LICENSE_TEXT**                                                                        **
+#LICENSE_TEXT** Commercial License                                                     **
+#LICENSE_TEXT** Commercial usage of ICL is possible and must be negotiated with us.    **
+#LICENSE_TEXT** See our website www.iclcv.org for more details                         **
+#LICENSE_TEXT**                                                                        **
+#LICENSE_TEXT** GNU General Public License Usage                                       **
+#LICENSE_TEXT** Alternatively, this file may be used under the terms of the GNU        **
+#LICENSE_TEXT** General Public License version 3.0 as published by the Free Software   **
+#LICENSE_TEXT** Foundation and appearing in the file LICENSE.GPL included in the       **
+#LICENSE_TEXT** packaging of this file.  Please review the following information to    **
+#LICENSE_TEXT** ensure the GNU General Public License version 3.0 requirements will be **
+#LICENSE_TEXT** met: http://www.gnu.org/copyleft/gpl.html.                             **
+#LICENSE_TEXT**                                                                        **
+#LICENSE_TEXT***************************************************************************/ 
 #LICENSE_TEXT
 
 function usage () {
     echo "usage: " ;
     echo " scripts/add_license.sh <-add|-remove> [-doit]" ;
+    echo ""
+    echo "this script can be used to add/remove the ICL license text. The license text"
+    echo "is embedded into this script, so you have to change the script itself if you"
+    echo "want to adapt the license text. "
     exit -1;
 }
 
@@ -52,18 +65,31 @@ cat $0 | grep -e '^#LICENSE_TEXT' | sed 's|#LICENSE_TEXT||g' > ./.license_text.t
 echo "adding this license text:"
 cat ./.license_text.txt
 
+ALL_NUM_FILES=0
 for PACKAGE in ICLAlgorithms ICLBlob ICLCC ICLCore ICLFilter ICLGeom ICLIO ICLQt ICLQuick ICLUtils ICLOpenCV ; do
-    cat $0 | grep -e '^#LICENSE_TEXT' | sed 's|#LICENSE_TEXT||g' | sed "s|__ICL_PACKAGE__|$PACKAGE|g" > ./.license_text.txt
+    CHARS=`echo $PACKAGE | wc -c`
+    MISSING=`echo 29-$CHARS | bc -l`
+    unset BEAUTY
+    for (( i=0 ; $i < $MISSING ; i++ )) ; do
+        BEAUTY="$BEAUTY " ;
+    done
+    cat $0 | grep -e '^#LICENSE_TEXT' | sed 's|#LICENSE_TEXT||g' | sed "s|__ICL_PACKAGE__|$PACKAGE|g" | sed "s|__ICL_SPACE__|$BEAUTY|g"> ./.license_text.txt
     echo "processing package $PACKAGE"
     HEADERS=`find include/$PACKAGE -iname *.h` ;
     SOURCES=`find $PACKAGE/src -iname *.cpp` ;
     EXAMPLES=`find $PACKAGE/examples -iname *.cpp` ;
+    NUMFILES=`echo $HEADERS $SOURCES $EXAMPLES | wc -w`
     for FILE in $HEADERS $SOURCES $EXAMPLES ; do
         if [ "$ADD" == "TRUE" ] ; then
             if [ "$DOIT" = "TRUE" ] ;then
                 echo "  adding license to $FILE"
-                cat ./.license_text.txt $FILE > ./.tmp_file.tmp ;
-                mv ./.tmp_file.tmp $FILE ;
+                ENSURE=`cat $FILE | grep 'GNU General Public License Usage'`
+                if [ "$ENSHURE" = "" ] ; then
+                    cat ./.license_text.txt $FILE > ./.tmp_file.tmp ;
+                    mv ./.tmp_file.tmp $FILE ;
+                else
+                    echo "  error token \"GNU General Public License Usage\" was already found in file (skipping)" ;
+                fi
             else
                 echo "  doing as if adding license to $FILE"
                 echo "  cat ./.license_text.txt $FILE > ./.tmp_file.tmp " ;
@@ -88,6 +114,9 @@ for PACKAGE in ICLAlgorithms ICLBlob ICLCC ICLCore ICLFilter ICLGeom ICLIO ICLQt
             fi
         fi
     done
+    ALL_NUM_FILES=`echo "$ALL_NUM_FILES+$NUMFILES" | bc -l`
 done
+
+echo processed $ALL_NUM_FILES files
 
 rm -rf ./.license_text.txt ./.tmp_file.tmp
