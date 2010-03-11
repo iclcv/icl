8de3fca8c5798b82fb3f7e48cd48150d6ad826b9
Christof Elbrechter <c.elbrechter@gmx.de>
2010-03-11 12:55:10 +0000

    added license-text to all header-, source- and example files

--- ICLUtils/examples/regular-expression-test.cpp
+++ ICLUtils/examples/regular-expression-test.cpp
@@ -1,3 +1,47 @@
+/*************************************************************************** 
+**                                                                        **
+** Copyright (C) 2006-2010 neuroinformatics group (vision)                **
+**                         University of Bielefeld                        **
+**                         nivision@techfak.uni-bielefeld.de              **
+**                                                                        **
+** This file is part of the ICLUtils module of ICL                        **
+**                                                                        **
+** Commercial License                                                     **
+** Commercial usage of ICL is possible and must be negotiated with us.    **
+** See our website www.iclcv.org for more details                         **
+**                                                                        **
+** GNU General Public License Usage                                       **
+** Alternatively, this file may be used under the terms of the GNU        **
+** General Public License version 3.0 as published by the Free Software   **
+** Foundation and appearing in the file LICENSE.GPL included in the       **
+** packaging of this file.  Please review the following information to    **
+** ensure the GNU General Public License version 3.0 requirements will be **
+** met: http://www.gnu.org/copyleft/gpl.html.                             **
+**                                                                        **
+***************************************************************************/ 
+
+/*************************************************************************** 
+**                                                                        **
+** Copyright (C) 2006-2010 neuroinformatics group (vision)                **
+** University of Bielefeld                                                **
+** Contact: nivision@techfak.uni-bielefeld.de                             **
+**                                                                        **
+** This file is part of the ICLUtils module of ICL                        **
+**                                                                        **
+** Commercial License                                                     **
+** Commercial usage of ICL is possible and must be negotiated with us.    **
+** See our website www.iclcv.org for more details                         **
+**                                                                        **
+** GNU General Public License Usage                                       **
+** Alternatively, this file may be used under the terms of the GNU        **
+** General Public License version 3.0 as published by the Free Software   **
+** Foundation and appearing in the file LICENSE.GPL included in the       **
+** packaging of this file.  Please review the following information to    **
+** ensure the GNU General Public License version 3.0 requirements will be **
+** met: http://www.gnu.org/copyleft/gpl.html.                             **
+**                                                                        **
+***************************************************************************/ 
+
 #include <ICLUtils/StringUtils.h>
 #include <ICLUtils/Macros.h>
 
--- include/ICLIO/UnicapGrabEngine.h
+++ include/ICLIO/UnicapGrabEngine.h
@@ -1,3 +1,25 @@
+/*************************************************************************** 
+**                                                                        **
+** Copyright (C) 2006-2010 neuroinformatics group (vision)                **
+**                         University of Bielefeld                        **
+**                         nivision@techfak.uni-bielefeld.de              **
+**                                                                        **
+** This file is part of the ICLIO module of ICL                           **
+**                                                                        **
+** Commercial License                                                     **
+** Commercial usage of ICL is possible and must be negotiated with us.    **
+** See our website www.iclcv.org for more details                         **
+**                                                                        **
+** GNU General Public License Usage                                       **
+** Alternatively, this file may be used under the terms of the GNU        **
+** General Public License version 3.0 as published by the Free Software   **
+** Foundation and appearing in the file LICENSE.GPL included in the       **
+** packaging of this file.  Please review the following information to    **
+** ensure the GNU General Public License version 3.0 requirements will be **
+** met: http://www.gnu.org/copyleft/gpl.html.                             **
+**                                                                        **
+***************************************************************************/ 
+
 #ifndef ICL_Unicap_GRAB_ENGINE_H
 #define ICL_Unicap_GRAB_ENGINE_H
 #include <ICLCore/Types.h>
