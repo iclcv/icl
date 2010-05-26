71899d8551e4a1c1e1aeb3861dacd39cccbdc0ca
Erik Weitnauer <eweitnauer@gmail.com>
2010-05-26 13:11:50 +0000

    added copyright notices to all test-template files

--- ICLCC/test/test-template.cpp
+++ ICLCC/test/test-template.cpp
@@ -1,3 +1,31 @@
+/********************************************************************
+**                Image Component Library (ICL)                    **
+**                                                                 **
+** Copyright (C) 2006-2010 Neuroinformatics, CITEC                 **
+**                         University of Bielefeld                 **
+**                Contact: nivision@techfak.uni-bielefeld.de       **
+**                Website: www.iclcv.org                           **
+**                                                                 **
+** File   : ICLCC/test/test-template.cpp                           **
+** Module : ICLCC                                                  **
+** Authors: Erik Weitnauer, Christof Elbrechter                    **
+**                                                                 **
+**                                                                 **
+** Commercial License                                              **
+** ICL can be used commercially, please refer to our website       **
+** www.iclcv.org for more details.                                 **
+**                                                                 **
+** GNU General Public License Usage                                **
+** Alternatively, this file may be used under the terms of the     **
+** GNU General Public License version 3.0 as published by the      **
+** Free Software Foundation and appearing in the file LICENSE.GPL  **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the GNU General Public License  **
+** version 3.0 requirements will be met:                           **
+** http://www.gnu.org/copyleft/gpl.html.                           **
+**                                                                 **
+*********************************************************************/
+
 #include <gtest/gtest.h>
 
 TEST(TestCaseName, EmptyTest) {
