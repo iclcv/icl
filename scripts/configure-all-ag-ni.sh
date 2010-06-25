0d34dbaafd781febded71289497b2648180f4230
Christof Elbrechter <c.elbrechter@gmx.de>
2010-06-25 22:19:05 +0000

    found solution for OpenCV linking problem (finally)

--- scripts/configure-all-ag-ni.sh
+++ scripts/configure-all-ag-ni.sh
@@ -1,4 +1,4 @@
-CXXFLAGS="-O4 -funroll-loops" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-LIBMESASR-Root=/vol/nivision --with-XINE-Root=/vol/nivision --with-GTEST-Root=/vol/nivision --with-OPENCV-Root=/vol/nivision
+CXXFLAGS="-O4 -funroll-loops" LDFLAGS="-L/vol/nivision/lib -Wl,-rpath=/vol/nivision/lib" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh` --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-LIBMESASR-Root=/vol/nivision --with-XINE-Root=/vol/nivision --with-GTEST-Root=/vol/nivision --with-OPENCV-Root=/vol/nivision
 
 
 #test
