b4c5e9239b83bc1dbc7fa7fe04c97c9c6eaad80c
Christof Elbrechter <c.elbrechter@gmx.de>
2010-06-28 14:11:14 +0000

    added configure script for stable build

---
+++ scripts/configure-all-ag-ni-stable.sh
@@ -0,0 +1,4 @@
+CXXFLAGS="-O4 -funroll-loops" LDFLAGS="-L/vol/nivision/lib -Wl,-rpath=/vol/nivision/lib" ./configure --with-SVS-Root=/vol/nivision/SVS/4.2 --prefix=/vol/nivision/ICL/`./VERSION.sh`-stable --with-UNICAP-Root=/vol/nivision --with-LIBDC-Root=/vol/nivision --with-IPP-Root=/vol/nivision/IPP/6.0 --with-LIBMESASR-Root=/vol/nivision --with-XINE-Root=/vol/nivision --with-GTEST-Root=/vol/nivision/share --with-OPENCV-Root=/vol/nivision
+
+
+#test
