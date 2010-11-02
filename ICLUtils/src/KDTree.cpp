af1f86affb5331dcf649b423cfa905b26000406a
Christian Groszewski <cgroszew@techfak.uni-bielefeld.de>
2010-11-02 13:12:25 +0000

    added implementation of kd-trees

---
+++ ICLUtils/src/KDTree.cpp
@@ -0,0 +1,221 @@
+/********************************************************************
+**                Image Component Library (ICL)                    **
+**                                                                 **
+** Copyright (C) 2006-2010 CITEC, University of Bielefeld          **
+**                         Neuroinformatics Group                  **
+** Website: www.iclcv.org and                                      **
+**          http://opensource.cit-ec.de/projects/icl               **
+**                                                                 **
+** File   : ICLUtils/src/KDTree.cpp                                **
+** Module : ICLUtils                                               **
+** Authors: Christian Groszewski                                   **
+**                                                                 **
+**                                                                 **
+** Commercial License                                              **
+** ICL can be used commercially, please refer to our website       **
+** www.iclcv.org for more details.                                 **
+**                                                                 **
+** GNU General Public License Usage                                **
+** Alternatively, this file may be used under the terms of the     **
+** GNU General Public License version 3.0 as published by the      **
+** Free Software Foundation and appearing in the file LICENSE.GPL  **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the GNU General Public License  **
+** version 3.0 requirements will be met:                           **
+** http://www.gnu.org/copyleft/gpl.html.                           **
+**                                                                 **
+** The development of this software was supported by the           **
+** Excellence Cluster EXC 277 Cognitive Interaction Technology.    **
+** The Excellence Cluster EXC 277 is a grant of the Deutsche       **
+** Forschungsgemeinschaft (DFG) in the context of the German       **
+** Excellence Initiative.                                          **
+**                                                                 **
+*********************************************************************/
+#include <ICLUtils/KDTree.h>
+
+namespace icl{
+KDTree::KDTree(std::vector<DynMatrix<double> > &list){
+	if(list.size()>0){
+		buildTree(list, 0, &root);
+	}
+}
+
+KDTree::KDTree(std::vector<DynMatrix<double> *> &list){
+	if(list.size()>0){
+		buildTree(list, 0, &root);
+	}
+}
+
+KDTree::~KDTree(){}
+
+struct sort_by_axis{
+	unsigned int axis;
+	sort_by_axis(unsigned int axis):axis(axis){}
+	bool operator()(const DynMatrix<double> &a, const DynMatrix<double> &b) const{
+		return a[axis] < b[axis];
+	}
+	bool operator()(const DynMatrix<double> *a, const DynMatrix<double> *b) const{
+		return (*a)[axis] < (*b)[axis];
+	}
+};
+
+void KDTree::buildTree(std::vector<DynMatrix<double> > &list, unsigned int depth, Node *node){
+	if(list.size() == 1){
+		node->point = &(list.at(0));
+		return;
+	}
+	unsigned int k = (list.at(0)).rows();
+	unsigned int axis = depth % k;
+	std::sort(list.begin(),list.end(),sort_by_axis(axis));
+	//sortList(list, axis);
+
+	unsigned int median = list.size()/2;
+
+	node->left = new Node();
+	node->median = double((list.at(median)).at(0,axis));
+	std::vector<DynMatrix<double> > sublist;
+	for(unsigned int i=0;i<median;++i){
+		sublist.push_back(list.at(i));
+	}
+	buildTree(sublist,depth+1,node->left);
+
+	node->right = new Node();
+	sublist.clear();
+	for(unsigned int i=median;i<list.size();++i){
+		sublist.push_back(list.at(i));
+	}
+	buildTree(sublist,depth+1,node->right);
+}
+
+void KDTree::buildTree(std::vector<DynMatrix<double> *> &list,unsigned int depth, Node *node){
+	if(list.size() <= 1){
+		node->point = list.at(0);
+		return;
+	}
+	unsigned int k = list.at(0)->rows();
+	unsigned int axis = depth % k;
+	std::sort(list.begin(),list.end(),sort_by_axis(axis));
+	//sortList(list, axis);
+
+	unsigned int median = list.size()/2;
+
+	node->left = new Node();
+	node->median = double((list.at(median))->at(0,axis));
+	std::vector<DynMatrix<double>* > sublist;
+	for(unsigned int i=0;i<median;++i){
+		sublist.push_back(list.at(i));
+	}
+	buildTree(sublist,depth+1,node->left);
+
+	node->right = new Node();
+	sublist.clear();
+	for(unsigned int i=median;i<list.size();++i){
+		sublist.push_back(list.at(i));
+	}
+	buildTree(sublist,depth+1,node->right);
+}
+
+void KDTree::sortList(std::vector<DynMatrix<double> > &list, unsigned int dim){
+	DynMatrix<double> temp;
+	unsigned int minIndex = 0;
+	for(unsigned int i=0;i<list.size()-1;++i){
+		minIndex = i;
+		for(unsigned int j=(i+1);j<list.size();++j){
+			if((list.at(minIndex)).at(0,dim) < (list.at(j)).at(0,dim)){
+				minIndex = j;
+			}
+		}
+		if(minIndex > i){
+			temp = list.at(i);
+			list.at(i) = list.at(minIndex);
+			list.at(minIndex) = temp;
+		}
+	}
+}
+
+void KDTree::sortList(std::vector<DynMatrix<double>* > &list, unsigned int dim){
+	DynMatrix<double> *temp=0;
+	unsigned int minIndex = 0;
+	for(unsigned int i=0;i<list.size()-1;++i){
+		minIndex = i;
+		for(unsigned int j=(i+1);j<list.size();++j){
+			if(list.at(minIndex)->at(0,dim) < list.at(j)->at(0,dim)){
+				minIndex = j;
+			}
+		}
+		if(minIndex > i){
+			temp = list.at(i);
+			list.at(i) = list.at(minIndex);
+			list.at(minIndex) = temp;
+		}
+	}
+}
+
+void KDTree::releaseTree(){
+	if(root.right)
+		delete root.right;
+	if(root.left)
+		delete root.left;
+}
+
+DynMatrix<double>* KDTree::nearestNeighbour(DynMatrix<double> &point){
+	unsigned int k = point.rows();
+	unsigned int depth = 0;
+	unsigned int axis = depth % k;
+	Node *n = &root;
+	while(1){
+		if(point[axis]<n->median){
+			if(n->left)
+				n = n->left;
+			else
+				return n->point;
+		} else {
+			if(n->right)
+				n = n->right;
+			else
+				return n->point;
+		}
+		depth += 1;
+		axis = depth % k;
+	}
+}
+
+DynMatrix<double>* KDTree::nearestNeighbour(DynMatrix<double> *point){
+	unsigned int k = point->rows();
+	unsigned int depth = 0;
+	unsigned int axis = depth % k;
+	Node *n = &root;
+	while(1){
+		if((*point)[axis]<n->median){
+			if(n->left)
+				n = n->left;
+			else
+				return n->point;
+		} else {
+			if(n->right)
+				n = n->right;
+			else
+				return n->point;
+		}
+		depth += 1;
+		axis = depth % k;
+	}
+}
+
+void KDTree::print(){
+	print(&root);
+}
+
+void KDTree::print(Node *n){
+	if(n->left){
+		print(n->left);
+	}
+	if(n->right){
+		print(n->right);
+	}
+	if(n->point){
+		std::cout << (*(n->point))[0] << "  " << (*(n->point))[1] << "  " << (*(n->point))[2]  << std::endl;
+		//SHOW(*(n->point));
+	}
+}
+}
---
+++ include/ICLUtils/KDTree.h
@@ -0,0 +1,148 @@
+/********************************************************************
+**                Image Component Library (ICL)                    **
+**                                                                 **
+** Copyright (C) 2006-2010 CITEC, University of Bielefeld          **
+**                         Neuroinformatics Group                  **
+** Website: www.iclcv.org and                                      **
+**          http://opensource.cit-ec.de/projects/icl               **
+**                                                                 **
+** File   : include/ICLUtils/KDTree.h                              **
+** Module : ICLUtils                                               **
+** Authors: Christian Groszewski                                   **
+**                                                                 **
+**                                                                 **
+** Commercial License                                              **
+** ICL can be used commercially, please refer to our website       **
+** www.iclcv.org for more details.                                 **
+**                                                                 **
+** GNU General Public License Usage                                **
+** Alternatively, this file may be used under the terms of the     **
+** GNU General Public License version 3.0 as published by the      **
+** Free Software Foundation and appearing in the file LICENSE.GPL  **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the GNU General Public License  **
+** version 3.0 requirements will be met:                           **
+** http://www.gnu.org/copyleft/gpl.html.                           **
+**                                                                 **
+** The development of this software was supported by the           **
+** Excellence Cluster EXC 277 Cognitive Interaction Technology.    **
+** The Excellence Cluster EXC 277 is a grant of the Deutsche       **
+** Forschungsgemeinschaft (DFG) in the context of the German       **
+** Excellence Initiative.                                          **
+**                                                                 **
+*********************************************************************/
+#ifndef ICL_KDTREE_H_
+#define ICL_KDTREE_H_
+
+#include <ICLUtils/DynMatrix.h>
+#include <ICLUtils/Macros.h>
+#include <vector>
+namespace icl{
+
+/**
+ This class implements a simple kd-tree. You can create an object of this class
+ with or without pointdata. After creating a kd-tree without data you can use
+ the buildTree function to insert datapoints or to rebuild the kd-tree with new
+ datapoints. Note: deleting the pointdata outside of this class means kd-tree is
+ referencing deleted pointers, so the tree is invalid an should be deleted.
+ */
+class KDTree{
+private:
+	///Keeps data of node
+	struct Node{
+		///left node
+		Node *left;
+		///right node
+		Node *right;
+		///point in leafnode, else null
+		DynMatrix<double> *point;
+		///median of dimension
+		double median;
+
+		///Constructor
+		Node():left(0),right(0),point(0),median(0.0){}
+
+		///Destructor
+		~Node(){
+			if(left){
+				delete left;
+				left = 0;
+			}
+			if(right){
+				delete right;
+				right = 0;
+			}
+		}
+	};
+
+	///the root node of the tree
+	Node root;
+
+	///internal print call
+	void print(Node *node);
+	///internal call to fill the KDTree with data
+	void buildTree(std::vector<DynMatrix<double> > &list,unsigned int depth, Node *node);
+	///internal call to fill the KDTree with data
+	void buildTree(std::vector<DynMatrix<double> *> &list,unsigned int depth, Node *node);
+	///internal call to sort list by dimension of the vector (unused, instead std::sort)
+	void sortList(std::vector<DynMatrix<double> > &list, unsigned int dim);
+	///internal call to sort list by dimension of the vector (unused, instead std::sort)
+	void sortList(std::vector<DynMatrix<double>* > &list, unsigned int dim);
+	///internal call to release data from KDTree
+	void releaseTree();
+
+public :
+	///Constructor
+	/** Creates a new KDTree object, with data from list.
+	 * @param list list of points for  the kd-tree
+	 */
+	KDTree(std::vector<DynMatrix<double> > &list);
+	///Constructor
+	///Constructor
+	/** Creates a new KDTree object, with data from list.
+	 * @param list list of points for  the kd-tree
+	 */
+	KDTree(std::vector<DynMatrix<double>* > &list);
+	///Constructor
+	///Constructor
+	/** Creates a new KDTree object, with data from list.
+	 * @param list list of points for  the kd-tree
+	 */
+	KDTree(){}
+	///Destructor
+	~KDTree();
+
+	///builds a kd-tree
+	/** Fills empty kd-tree or the current one with new data.
+	 * @param list list of points for the kd-tree
+	 */
+	void buildTree(std::vector<DynMatrix<double> *> &list){
+		releaseTree();
+		buildTree(list,0,&root);
+	}
+
+	///builds a kd-tree
+	/** Fills empty KDTree object or the current one with new data.
+	 * @param list list of points for the kd-tree
+	 */
+	void buildTree(std::vector<DynMatrix<double> > &list){
+		releaseTree();
+		buildTree(list,0,&root);
+	}
+
+	///Prints the tree on standard output.
+	void print();
+
+	///Returns pointer to nearest neighbour to passed point.
+	/**@param point the point to search nearest neighbor for
+	 * @return the pointer to nearest neighbour*/
+	DynMatrix<double>* nearestNeighbour(DynMatrix<double> &point);
+
+	///Returns pointer to nearest neighbour to passed point.
+	/**@param point the point to search nearest neighbor for
+	 * @return the pointer to nearest neighbour*/
+	DynMatrix<double>* nearestNeighbour(DynMatrix<double> *point);
+
+};
+}
+#endif /* ICL_KDTREE_H_ */
