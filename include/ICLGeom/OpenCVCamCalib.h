86dedcc11a5b624782dcab7c27ec1800a427b420
Christian Groszewski <cgroszew@techfak.uni-bielefeld.de>
2011-01-23 22:11:32 +0000

    added cmake conf info for mac build, comments include in CmakeLists.txt where to get things from

--- include/ICLGeom/OpenCVCamCalib.h
+++ include/ICLGeom/OpenCVCamCalib.h
@@ -45,8 +45,10 @@
 #ifdef HAVE_OPENCV211
 #include <opencv2/highgui/highgui_c.h>
 #include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/core/core_c.h>
-#include <opencv2/core/types_c.h>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
+//#include <opencv2/core/core_c.h>
+//#include <opencv2/core/types_c.h>
 #else
 #include <cv.h>
 #endif
