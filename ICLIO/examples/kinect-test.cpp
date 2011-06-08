24541bcda6c1479e019cddf31028ff9fbff52c5d
Andreas Raster <lazor@affenbande.org>
2011-06-08 13:16:37 +0000

    made the rather spartanian kinect-test actually do something usefull

--- ICLIO/examples/kinect-test.cpp
+++ ICLIO/examples/kinect-test.cpp
@@ -1,8 +1,29 @@
 #include <ICLQuick/Quick.h>
 #include <ICLIO/GenericGrabber.h>
 
+#include <sstream>
+#include <iostream>
+#include <vector>
+
 int main(int argc, char *argv[])
 {
-  icl::GenericGrabber("kinectc",0);
-  return 0;
+  bool c = true;
+  int n = 0;
+  
+  while(c) {
+    try {
+      std::stringstream pss;
+      pss << "kinectc=" << n;
+      
+      icl::GenericGrabber("kinectc",pss.str());
+
+      n++;
+    } catch(ICLException e) {
+      c = false;
+    }
+  }
+
+  std::cout << "Found " << n << " Kinect Devices." << std::endl;
+  
+  return !n;
 }
