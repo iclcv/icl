042b05585f5da71611591adc77b2f1349224428f
Christof Elbrechter <c.elbrechter@gmx.de>
2011-12-12 20:44:20 +0000

    added the first working prototype for a post_install script, which is now located in scripts/post_install.cmake The script is included as install(SCRIPT ...) from the scripts/CMakeLists.txt. scripts is added as very last cmake sub-director in order to ensure, that the post-install script is executed AFTER installing all the other stuff.
    TODO: the script still needs to be adapted so that it actually
    adds group-write permission for everything, that is installed

--- scripts/post_install.cmake
+++ scripts/post_install.cmake
@@ -1,11 +1,8 @@
-message(STATUS "in script post_install.cmake")
+message(STATUS "executing ICL/scripts/post_install.cmake")
 execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/lib RESULT_VARIABLE ICL_CHMOD_LIB)
 execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig RESULT_VARIABLE ICL_CHMOD_PKGCFG)
 execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/bin RESULT_VARIABLE ICL_CHMOD_BIN)
 execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/doc RESULT_VARIABLE ICL_CHMOD_DOC)
-
-
-message(STATUS "result of lib: ${ICL_CHMOD_LIB}")
 
 if(ICL_CHMOD_LIB)
   message(STATUS "unable to change permissions of ${CMAKE_INSTALL_PREFIX}/${LIBRARY_OUTPUT_PATH}")
@@ -34,20 +31,4 @@ else()
   message(STATUS "changed permissions of ${CMAKE_INSTALL_PREFIX}/${DOC_DIR}")
 endif()
 
-
-
-
-# [args1...]]
-#                  [COMMAND <cmd2> [args2...] [...]]
-#                  [WORKING_DIRECTORY <directory>]
-#                  [TIMEOUT <seconds>]
-#                  [RESULT_VARIABLE <variable>]
-#                  [OUTPUT_VARIABLE <variable>]
-#                  [ERROR_VARIABLE <variable>]
-#                  [INPUT_FILE <file>]
-#                  [OUTPUT_FILE <file>]
-#                  [ERROR_FILE <file>]
-#                  [OUTPUT_QUIET]
-#                  [ERROR_QUIET]
-#                  [OUTPUT_STRIP_TRAILING_WHITESPACE]
-#                  [ERROR_STRIP_TRAILING_WHITESPACE])
+#todo: make the script to adapt all installed files!
