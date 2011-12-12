fb6f146a4097996c2285512e3d71b93299a39ad7
Christof Elbrechter <c.elbrechter@gmx.de>
2011-12-12 20:35:14 +0000

    post install hook does something ..

--- post_install.cmake
+++ post_install.cmake
@@ -1,12 +1,39 @@
-#add_custom_target(postinstall chmod -f g+w ${CMAKE_INSTALL_PREFIX}/${LIBRARY_OUTPUT_PATH} || { echo "unable to change# permissions for lib" \; exit 1\; }
-#					COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig || { "echo unable to# change permissions for lib/pkgconfig" \; exit 1\; }
-#					COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/${EXECUTABLE_OUTPUT_PATH} || { echo "unable to change permissions for bin" \; exit 1\; }
-#					COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/${DOC_DIR} || { echo "unable to change permissions for doc" \; exit 1\; })
+message(STATUS "in script post_install.cmake")
+execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/lib RESULT_VARIABLE ICL_CHMOD_LIB)
+execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig RESULT_VARIABLE ICL_CHMOD_PKGCFG)
+execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/bin RESULT_VARIABLE ICL_CHMOD_BIN)
+execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/doc RESULT_VARIABLE ICL_CHMOD_DOC)
 
-execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/${LIBRARY_OUTPUT_PATH} OUTPUT_QUIET ERROR_QUIET RESULT_VARIABLE ICL_CHMOD_LIB)
-execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig OUTPUT_QUIET ERROR_QUIET RESULT_VARIABLE ICL_CHMOD_PKGCFG)
-execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/${EXECUTABLE_OUTPUT_PATH} OUTPUT_QUIET ERROR_QUIET RESULT_VARIABLE ICL_CHMOD_BIN)
-execute_process(COMMAND chmod -f g+w ${CMAKE_INSTALL_PREFIX}/${DOC_DIR} OUTPUT_QUIET ERROR_QUIET RESULT_VARIABLE ICL_CHMOD_DOC)
+
+message(STATUS "result of lib: ${ICL_CHMOD_LIB}")
+
+if(ICL_CHMOD_LIB)
+  message(STATUS "unable to change permissions of ${CMAKE_INSTALL_PREFIX}/${LIBRARY_OUTPUT_PATH}")
+else()
+  message(STATUS "changed permissions of ${CMAKE_INSTALL_PREFIX}/${LIBRARY_OUTPUT_PATH}")
+endif()
+
+
+if(ICL_CHMOD_PKGCFG)
+  message(STATUS "unable to change permissions of ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig")
+else()
+  message(STATUS "changed permissions of ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig")
+endif()
+
+
+if(ICL_CHMOD_BIN)
+  message(STATUS "unable to change permissions of ${CMAKE_INSTALL_PREFIX}/${EXECUTABLE_OUTPUT_PATH}")
+else()
+  message(STATUS "changed permissions of ${CMAKE_INSTALL_PREFIX}/${EXECUTABLE_OUTPUT_PATH}")
+endif()
+
+
+if(ICL_CHMOD_DOC)
+  message(STATUS "unable to change permissions of ${CMAKE_INSTALL_PREFIX}/${DOC_DIR}")
+else()
+  message(STATUS "changed permissions of ${CMAKE_INSTALL_PREFIX}/${DOC_DIR}")
+endif()
+
 
 
 
