759151773208caa897a4ef078b7a54fe09c9a240
Christof Elbrechter <c.elbrechter@gmx.de>
2012-06-01 12:58:55 +0000

    added test

---
+++ ICLUtils/examples/svd-test.cpp
@@ -0,0 +1,51 @@
+/********************************************************************
+**                Image Component Library (ICL)                    **
+**                                                                 **
+** Copyright (C) 2006-2012 CITEC, University of Bielefeld          **
+**                         Neuroinformatics Group                  **
+** Website: www.iclcv.org and                                      **
+**          http://opensource.cit-ec.de/projects/icl               **
+**                                                                 **
+** File   : ICLUtils/examples/svd-test.cpp                         **
+** Module : ICLUtils                                               **
+** Authors: Christof Elbrechter                                    **
+**                                                                 **
+**                                                                 **
+** Commercial License                                              **
+** ICL can be used commercially, please refer to our website       **
+** www.iclcv.org for more details.                                 **
+**                                                                 **
+** GNU General Public License Usage                                **
+** Alternatively, this file may be used under the terms of the     **
+** GNU General Public License version 3.0 as published by the      **
+** Free Software Foundation and appearing in the file LICENSE.GPL  **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the GNU General Public License  **
+** version 3.0 requirements will be met:                           **
+** http://www.gnu.org/copyleft/gpl.html.                           **
+**                                                                 **
+** The development of this software was supported by the           **
+** Excellence Cluster EXC 277 Cognitive Interaction Technology.    **
+** The Excellence Cluster EXC 277 is a grant of the Deutsche       **
+** Forschungsgemeinschaft (DFG) in the context of the German       **
+** Excellence Initiative.                                          **
+**                                                                 **
+*********************************************************************/
+
+#include <ICLUtils/DynMatrixUtils.h>
+#include <ICLUtils/Random.h>
+
+using namespace icl;
+
+int main(){
+  typedef double real;
+  
+  DynMatrix<real> a(100,10);
+  std::fill(a.begin(),a.end(),URand(0,1));
+
+  Time t=Time::now();
+  DynMatrix<real> U,s,Vt;
+  a.svd(U,s,Vt);
+  //  SHOW(U);
+  SHOW(t.age().toMilliSecondsDouble());
+}
