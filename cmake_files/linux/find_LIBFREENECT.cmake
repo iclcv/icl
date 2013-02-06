a2879c0ccdb589f91df45a2c54c85802330d73fe
Christof Elbrechter <c.elbrechter@gmx.de>
2013-02-06 14:49:33 +0000

    added special check for differenct libfreenect directory

--- cmake_files/linux/find_LIBFREENECT.cmake
+++ cmake_files/linux/find_LIBFREENECT.cmake
@@ -1,1 +1,8 @@
-icl_check_external_package(LIBFREENECT libfreenect.h freenect lib include/libfreenect TRUE TRUE)
+set(on ${ICL_XDEP_LIBFREENECT_ON})
+
+icl_check_external_package(LIBFREENECT libfreenect.h freenect lib include/libfreenect FALSE TRUE)
+
+if(NOT HAVE_LIBFREENECT_COND AND ${on})
+  set(ICL_XDEP_LIBFREENECT_ON TRUE)
+  icl_check_external_package(LIBFREENECT libfreenect.h freenect lib include TRUE TRUE)
+endif()
