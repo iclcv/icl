e39c727ad8d9434b4cd9c7131fc29c7639a8696c
Christof Elbrechter <c.elbrechter@gmx.de>
2013-02-12 16:52:40 +0000

    fixed bug in new linesample to avoid seg-fault if buffer is not initialized

--- ICLGeom/examples/kinect-recorder.cpp
+++ ICLGeom/examples/kinect-recorder.cpp
@@ -9,10 +9,20 @@ GenericImageOutput c_out,d_out,both_out;
 
 void init(){
   if(pa("-s")){
-    c_in.init("kinectc","kinectc=0");
-    d_in.init("kinectd","kinectd=0");
-    c_out.init("file",*pa("-s")+"-color-######.bicl");
-    d_out.init("file",*pa("-s")+"-depth-######.bicl");
+
+    if(pa("-c")){
+      c_in.init(pa("-c"));
+    }else{
+      c_in.init("kinectc","kinectc=0");
+    }
+    
+    if(pa("-d")){
+      d_in.init(pa("-d"));
+    }else{
+      d_in.init("kinectd","kinectd=0");
+    }
+    c_out.init("file",*pa("-s")+"/color-######.bicl");
+    d_out.init("file",*pa("-s")+"/depth-######.bicl");
 
     gui << Image().handle("depth").minSize(16,12).label("depth image")
         << Image().handle("color").minSize(16,12).label("color image");
@@ -32,9 +42,15 @@ void init(){
     if(pa("-co")) c_out.init(pa("-co"));
   }
   
-  // Size s = pa("-size");
-  //if(!c_in.isNull()) c_in.useDesired(s);
-  //if(!d_in.isNull()) d_in.useDesired(s);
+  if(pa("-ds")){
+    Size s = pa("-ds");
+    if(!d_in.isNull()) d_in.useDesired(s);
+  }
+
+  if(pa("-cs")){
+    Size s = pa("-cs");
+    if(!c_in.isNull()) c_in.useDesired(s);
+  }
 
   gui << Show();
 }
@@ -53,7 +69,12 @@ void run(){
 }
 
 int main(int n, char **args){
-  return ICLApp(n,args,"-depth-input|-d(2) -color-input|-c(2) -size(size=VGA)"
+  pa_explain("-s","deines a single output directory, where captured images\n"
+             "are stored. If no input (-d) or (-c) is given, by default,\n"
+             "the kinect color and depth camera are used");
+
+  return ICLApp(n,args,"-depth-input|-d(2) -color-input|-c(2) "
+                "-depth-size|-ds(size) -color-size|-cs(size) "
                 "-depth-output|-do(2) -color-output|-oc(2) "
                 "-simple-io-params|-s(output-file-base-name)",init,run).exec();
 }
