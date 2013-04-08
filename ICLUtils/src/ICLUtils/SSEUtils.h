25dfa4c5a936926936a371270d6669d157f5c6ab
Sergius Gaulik <sgaulik@gmail.com>
2013-04-08 20:06:06 +0000

    added a new header SSEUtils

---
+++ ICLUtils/src/ICLUtils/SSEUtils.h
@@ -0,0 +1,41 @@
+/********************************************************************
+**                Image Component Library (ICL)                    **
+**                                                                 **
+** Copyright (C) 2006-2013 CITEC, University of Bielefeld          **
+**                         Neuroinformatics Group                  **
+** Website: www.iclcv.org and                                      **
+**          http://opensource.cit-ec.de/projects/icl               **
+**                                                                 **
+** File   : ICLUtils/src/ICLUtils/SSEUtils.h                       **
+** Module : ICLUtils                                               **
+** Authors: Sergius Gaulik                                         **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+** The development of this software was supported by the           **
+** Excellence Cluster EXC 277 Cognitive Interaction Technology.    **
+** The Excellence Cluster EXC 277 is a grant of the Deutsche       **
+** Forschungsgemeinschaft (DFG) in the context of the German       **
+** Excellence Initiative.                                          **
+**                                                                 **
+********************************************************************/
+
+#pragma once
+
+#if defined __SSE2__ || _MSC_VER >= 1300
+  #include "emmintrin.h"
+  #define __ICL_SSE2__
+  #if defined __SSE3__ || _MSC_VER >= 1500
+    #include "pmmintrin.h"
+    #define __ICL_SSE3__
+  #endif
+#endif
+
