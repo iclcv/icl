c7b88ed6d32c512ac3b4f7d29eb533880a18357f
Christof Elbrechter <c.elbrechter@gmx.de>
2013-11-20 17:48:17 +0000

    more steps towards abstract canvas

--- ICLCore/src/ICLCore/AbstractCanvas.h
+++ ICLCore/src/ICLCore/AbstractCanvas.h
@@ -44,40 +44,57 @@ namespace icl{
       public:
       typedef math::FixedMatrix<float,3,3> Transform;
       typedef core::Color4D32f Color;
-
+      struct ClipRect{
+        ClipRect(float minx=0, float maxx=0, float miny=0, float maxy=0): 
+          minx(minx),maxy(maxx),miny(miny),maxy(maxy){}
+        float minx;
+        float miny;
+        float maxx;
+        float maxy;
+      };
+      
       protected:
       struct State{
-        Transform T;
-        Color linecolor;
-        Color fillcolor;
+        AbstractCanvas::Transform T;
+        AbstractCanvas::Color linecolor;
+        AbstractCanvas::Color fillcolor;
         float linewidth;
         float pointsize;
         float symsize;
         float fontsize;
         std::string fontname;
-        const Rect32f clip;
+        ClipRect clip;
+        
+        inline State():
+          T(AbstractCanvas::Transform::id()),
+          linecolor(255,0,0,255),fillcolor(0,0,0,0),
+          linewidth(1),pointsize(1),symsize(10),
+          fontsize(10),fontname("arial"),
+          antialiasing(false){}
       };
       
       State state;
       std::vector<State> stack;
-      
-      bool antialiasing;
-      Rect32f viewport;
-
-      inline Point32f transform(float x, float y){
-        return Point32f(stack.T(0,0)*x + stack.T(1,0)*y + stack.T(2,0),
-                        stack.T(0,1)*x + stack.T(1,1)*y + stack.T(2,1));
+  
+      inline Point32f transform(float x, float y) const{
+        return Point32f(state.T(0,0)*x + state.T(1,0)*y + state.T(2,0),
+                        state.T(0,1)*x + state.T(1,1)*y + state.T(2,1));
+      }
+      inline bool clip(float x, float y) const{
+        return ((x >= state.clip.minx) && (x<= state.clip.maxx) 
+                && (y >= state.clip.miny) && (y <= state.clip.maxy));
       }
                         
       
       public:
       
-      AbstractCanvas():T(Transform::id()),
-        linecolor(255,0,0,255),fillcolor(0,0,0,0),
-        linewidth(1),pointsize(1),symsize(10),
-      antialiasing(false),fontname("arial"){}
-
+      AbstractCanvas(){}
       virtual ~AbstractCanvas(){}
+      
+      virtual void draw_point_internal(const Point32f &p) = 0;
+      virtual void draw_line_internal(const Point32f &a, const Point32f &b) = 0;
+      virtual void fill_triangle_internal(const Point32f &a, const Point32f &b, const Point32f &c) = 0;
+      virtual void draw_ellipse_internal(const Point32f &c,  const Point32f &axis1, const Point32f &axis2);
       
       public:
 
@@ -90,16 +107,67 @@ namespace icl{
         stack.pop_back();
       }
       
-      virtual void point(float x, float y) = 0;
-      virtual void line(float x0, float y0, float x1, float y1)  = 0;
-      virtual void triangle(float x0, float y0, float x1, float y1, float x2, float y2) = 0;
-      virtual void sym(char c, float x, float y) = 0;
-      virtual void linestrip(int n, const float *xs, const float *ys, int xStride=1, int yStride=1) = 0;
-      virtual void rect(float x, float y, float w, float h) = 0;
-      virtual void circle(float cx, float cy, float r) = 0;
-      virtual void ellipse(float x, float x, float w, float h) = 0;
-      virtual void image(const ImgBase *image, float xanchor, float yanchor) = 0;
-      virtual void text(const std::string &t, float x, float y) = 0;
+      virtual void point(float x, float y){
+        draw_point_internal(transform(x,y));
+      }
+      virtual void line(float x0, float y0, float x1, float y1){
+        draw_line_internal(transform(x0,y0),transform(x1,y1));
+      }
+      virtual void triangle(float x0, float y0, float x1, float y1, float x2, float y2){
+        const Point32f a = transform(x0,y0);
+        const Point32f b = transform(x1,y1);
+        const Point32f c = transform(x2,y2);
+        fill_triangle_internal(a,b,c);
+        draw_line_internal(a,b);
+        draw_line_internal(b,c);
+        draw_line_internal(c,a);
+      }
+      
+      virtual void sym(char c, float x, float y){
+        // draw lines out of symbols (regarding size)
+        // symbols are o . x + * t(riangle) s(quare)
+      }
+      
+      virtual void linestrip(int n, const float *xs, const float *ys, int xStride=1, int yStride=1, bool loop=false){
+        if(n<2) return;
+        Point32f a = transform(xs[0],ys[0]),b;
+        for(int i=1;i<n;++i){
+          b = transform(xs[i*xStride],ys[i*yStride]);
+          draw_line_internal(a,b);
+          a = b;
+        }
+        if(loop){
+          draw_line_internal(a,transform(xs[0],ys[0]));          
+        }
+      }
+      
+      virtual void rect(float x, float y, float w, float h){
+        const Point32f ul = transform(x,y);
+        const Point32f ur = transform(x+w,y);
+        const Point32f lr = transform(x+w,y+h);
+        const Point32f ll = transform(x,y+h);
+        fill_triangle_internal(ul,ur,lr);
+        fill_triangle_internal(ul,lr,ll);
+        draw_line_internal(ul,ur);
+        draw_line_internal(ur,lr);
+        draw_line_internal(lr,ll);
+        draw_line_internal(ll,ul);
+      }
+      virtual void circle(float cx, float cy, float r){
+        // TODO
+      }
+      virtual void ellipse(float x, float x, float w, float h){
+        // TODO: virtual void draw_ellipse_internal(const Point32f &c,  const Point32f &axis1, const Point32f &axis2);
+        
+      }
+      
+      virtual void image(const ImgBase *image, float xanchor, float yanchor){
+        // estimate the four corners and use rectification to perform sampling
+      }
+
+      virtual void text(const std::string &t, float x, float y){
+        // fallback: use image and a hardcoded font (including hardcoded size)
+      }
             
       
       virtual void linecolor(float r, float g, float b, float a){
