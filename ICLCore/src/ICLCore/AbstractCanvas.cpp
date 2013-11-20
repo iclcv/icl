b88e438092100558ddc6c14f086d905d0d7470d6
Christof Elbrechter <c.elbrechter@gmx.de>
2013-11-20 21:30:42 +0000

    added some basic implementations for the new Abstract canvas class and a corresponding demo application

---
+++ ICLCore/src/ICLCore/AbstractCanvas.cpp
@@ -0,0 +1,161 @@
+/********************************************************************
+**                Image Component Library (ICL)                    **
+**                                                                 **
+** Copyright (C) 2006-2013 CITEC, University of Bielefeld          **
+**                         Neuroinformatics Group                  **
+** Website: www.iclcv.org and                                      **
+**          http://opensource.cit-ec.de/projects/icl               **
+**                                                                 **
+** File   : ICLCore/src/ICLCore/AbstractCanvas.cpp                 **
+** Module : ICLCore                                                **
+** Authors: Christof Elbrechter                                    **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+** The development of this software was supported by the           **
+** Excellence Cluster EXC 277 Cognitive Interaction Technology.    **
+** The Excellence Cluster EXC 277 is a grant of the Deutsche       **
+** Forschungsgemeinschaft (DFG) in the context of the German       **
+** Excellence Initiative.                                          **
+**                                                                 **
+********************************************************************/
+
+#include <ICLCore/AbstractCanvas.h>
+#include <ICLUtils/StringUtils.h>
+
+namespace icl{
+  namespace core{
+    
+    
+    
+    void AbstractCanvas::point(float x, float y){
+      draw_point_internal(transform(x,y));
+    }
+    void AbstractCanvas::line(float x0, float y0, float x1, float y1){
+      draw_line_internal(transform(x0,y0),transform(x1,y1));
+    }
+    void AbstractCanvas::triangle(float x0, float y0, float x1, float y1, float x2, float y2){
+      const Point32f a = transform(x0,y0);
+      const Point32f b = transform(x1,y1);
+      const Point32f c = transform(x2,y2);
+      fill_triangle_internal(a,b,c);
+      draw_line_internal(a,b);
+      draw_line_internal(b,c);
+      draw_line_internal(c,a);
+    }
+      
+    void AbstractCanvas::sym(char c, float x, float y) throw (ICLException){
+      const float s = state.symsize;
+      switch(c){
+        case 'x':
+          line(x-s,y-s,x+s,y+s);
+          line(x-s,y+s,x+s,y-s);
+          break;
+        case '+':
+          line(x,y-s,x,y+s);
+          line(x-s,y,y+s,y);
+          break;
+        case '*':
+          sym('x',x,y);
+          sym('+',x,y);
+          break;
+        case 'o':
+          // TODO: temporarily deactivate fill
+          push();
+          fillcolor(0,0,0,0);
+          circle(x,y,s);
+          pop();
+          break;
+        case '.':
+          point(x,y);
+          break;
+        case 't':
+          triangle(x,y-s, x+s, y+s, x-s, y+s);
+          break;
+        case 's':
+          rect(x-s,y-s,2*s,2*s);
+          break;
+        default:
+          throw ICLException("undefined symbol type: " + str(c));
+      }
+    }
+      
+    void AbstractCanvas::linestrip(int n, const float *xs, const float *ys, 
+                                   int xStride, int yStride, bool loop){
+      if(n<2) return;
+      Point32f a = transform(xs[0],ys[0]),b;
+      for(int i=1;i<n;++i){
+        b = transform(xs[i*xStride],ys[i*yStride]);
+        draw_line_internal(a,b);
+        a = b;
+      }
+      if(loop){
+        draw_line_internal(a,transform(xs[0],ys[0]));          
+      }
+    }
+      
+    void AbstractCanvas::rect(float x, float y, float w, float h){
+      const Point32f ul = transform(x,y);
+      const Point32f ur = transform(x+w,y);
+      const Point32f lr = transform(x+w,y+h);
+      const Point32f ll = transform(x,y+h);
+      fill_triangle_internal(ul,ur,lr);
+      fill_triangle_internal(ul,lr,ll);
+      draw_line_internal(ul,ur);
+      draw_line_internal(ur,lr);
+      draw_line_internal(lr,ll);
+      draw_line_internal(ll,ul);
+    }
+
+    void AbstractCanvas::circle(float cx, float cy, float r){
+      ellipse(cx-r,cy-r,2*r,2*r);
+    }
+
+    void AbstractCanvas::ellipse(float cx, float cy, float rx, float ry){
+      const Point32f c = transform(cx,cy);
+      const Point32f h = transform(cx+rx,cy) - c;
+      const Point32f v = transform(cx,cy+ry) - c;
+      draw_ellipse_internal(c,h,v);
+    }
+      
+    void AbstractCanvas::image(const ImgBase *image, float xanchor, float yanchor, float alpha, 
+                               scalemode sm, bool centered) throw (ICLException){
+      ICLASSERT_THROW(image,ICLException("AbstractCanvas::image: input image is null"));
+
+      if(centered){
+        const float x = xanchor, y=yanchor, w=image->getWidth()/2., h=image->getHeight()/2.;
+        const Point32f ul = transform(x-w,y-h);
+        const Point32f ur = transform(x+w,y-h);
+        const Point32f lr = transform(x+w,y+h);
+        const Point32f ll = transform(x-w,y+h);
+        draw_image_internal(ul,ur,lr,ll,alpha,sm);
+
+      }else {
+        const float x = xanchor, y=yanchor, w=image->getWidth(), h=image->getHeight();
+        const Point32f ul = transform(x,y);
+        const Point32f ur = transform(x+w,y);
+        const Point32f lr = transform(x+w,y+h);
+        const Point32f ll = transform(x,y+h);
+        draw_image_internal(ul,ur,lr,ll,alpha,sm);
+      }
+    }
+
+    void AbstractCanvas::text(const std::string &t, float x, float y, bool centered){
+      ERROR_LOG("AbstractCanvas::text: text rendering is not implemented yet");
+      //        draw_text_internal(t, transform(x,y), 
+      // fallback: use image and a hardcoded font (including hardcoded size) 
+      // we need some kind of a font metrics ...
+    }
+            
+      
+    
+  }
+}
--- ICLCore/src/ICLCore/AbstractCanvas.h
+++ ICLCore/src/ICLCore/AbstractCanvas.h
@@ -34,11 +34,17 @@
 #include <ICLUtils/Uncopyable.h>
 #include <ICLMath/FixedMatrix.h>
 #include <ICLCore/Color.h>
-#include <ICLCore/Rect32f.h>
+#include <ICLUtils/Rect32f.h>
 
 
 namespace icl{
+
+  using namespace math;
+  using namespace utils;
+
   namespace core{
+    
+
     
     class AbstractCanvas : public utils::Uncopyable{
       public:
@@ -46,10 +52,10 @@ namespace icl{
       typedef core::Color4D32f Color;
       struct ClipRect{
         ClipRect(float minx=0, float maxx=0, float miny=0, float maxy=0): 
-          minx(minx),maxy(maxx),miny(miny),maxy(maxy){}
+          minx(minx),maxx(maxx),miny(miny),maxy(maxy){}
         float minx;
-        float miny;
         float maxx;
+        float miny;
         float maxy;
       };
       
@@ -69,14 +75,13 @@ namespace icl{
           T(AbstractCanvas::Transform::id()),
           linecolor(255,0,0,255),fillcolor(0,0,0,0),
           linewidth(1),pointsize(1),symsize(10),
-          fontsize(10),fontname("arial"),
-          antialiasing(false){}
+          fontsize(10),fontname("arial"){}
       };
       
       State state;
       std::vector<State> stack;
   
-      inline Point32f transform(float x, float y) const{
+      inline utils::Point32f transform(float x, float y) const{
         return Point32f(state.T(0,0)*x + state.T(1,0)*y + state.T(2,0),
                         state.T(0,1)*x + state.T(1,1)*y + state.T(2,1));
       }
@@ -91,10 +96,20 @@ namespace icl{
       AbstractCanvas(){}
       virtual ~AbstractCanvas(){}
       
-      virtual void draw_point_internal(const Point32f &p) = 0;
-      virtual void draw_line_internal(const Point32f &a, const Point32f &b) = 0;
-      virtual void fill_triangle_internal(const Point32f &a, const Point32f &b, const Point32f &c) = 0;
-      virtual void draw_ellipse_internal(const Point32f &c,  const Point32f &axis1, const Point32f &axis2);
+      virtual void draw_point_internal(const utils::Point32f &p) = 0;
+      virtual void draw_line_internal(const utils::Point32f &a, 
+                                      const utils::Point32f &b) = 0;
+      virtual void fill_triangle_internal(const utils::Point32f &a, 
+                                          const utils::Point32f &b,
+                                          const utils::Point32f &c) = 0;
+      virtual void draw_ellipse_internal(const utils::Point32f &c,
+                                         const utils::Point32f &axis1,
+                                         const utils::Point32f &axis2) = 0;
+      virtual void draw_image_internal(const utils::Point32f &ul, 
+                                       const utils::Point32f &ur, 
+                                       const utils::Point32f &lr, 
+                                       const utils::Point32f &ll,
+                                       float alpha, scalemode sm) = 0;
       
       public:
 
@@ -102,73 +117,25 @@ namespace icl{
         stack.push_back(state);
       }
       virtual void pop() throw (utils::ICLException){
-        ICLASSERT_THROW(stack.size(), ICLException("AbstractCanvas::pop: stack is empty"));
+        ICLASSERT_THROW(stack.size(), utils::ICLException("AbstractCanvas::pop: stack is empty"));
         state = stack.back();
         stack.pop_back();
       }
       
-      virtual void point(float x, float y){
-        draw_point_internal(transform(x,y));
-      }
-      virtual void line(float x0, float y0, float x1, float y1){
-        draw_line_internal(transform(x0,y0),transform(x1,y1));
-      }
-      virtual void triangle(float x0, float y0, float x1, float y1, float x2, float y2){
-        const Point32f a = transform(x0,y0);
-        const Point32f b = transform(x1,y1);
-        const Point32f c = transform(x2,y2);
-        fill_triangle_internal(a,b,c);
-        draw_line_internal(a,b);
-        draw_line_internal(b,c);
-        draw_line_internal(c,a);
-      }
-      
-      virtual void sym(char c, float x, float y){
-        // draw lines out of symbols (regarding size)
-        // symbols are o . x + * t(riangle) s(quare)
-      }
-      
-      virtual void linestrip(int n, const float *xs, const float *ys, int xStride=1, int yStride=1, bool loop=false){
-        if(n<2) return;
-        Point32f a = transform(xs[0],ys[0]),b;
-        for(int i=1;i<n;++i){
-          b = transform(xs[i*xStride],ys[i*yStride]);
-          draw_line_internal(a,b);
-          a = b;
-        }
-        if(loop){
-          draw_line_internal(a,transform(xs[0],ys[0]));          
-        }
-      }
-      
-      virtual void rect(float x, float y, float w, float h){
-        const Point32f ul = transform(x,y);
-        const Point32f ur = transform(x+w,y);
-        const Point32f lr = transform(x+w,y+h);
-        const Point32f ll = transform(x,y+h);
-        fill_triangle_internal(ul,ur,lr);
-        fill_triangle_internal(ul,lr,ll);
-        draw_line_internal(ul,ur);
-        draw_line_internal(ur,lr);
-        draw_line_internal(lr,ll);
-        draw_line_internal(ll,ul);
-      }
-      virtual void circle(float cx, float cy, float r){
-        // TODO
-      }
-      virtual void ellipse(float x, float x, float w, float h){
-        // TODO: virtual void draw_ellipse_internal(const Point32f &c,  const Point32f &axis1, const Point32f &axis2);
-        
-      }
-      
-      virtual void image(const ImgBase *image, float xanchor, float yanchor){
-        // estimate the four corners and use rectification to perform sampling
-      }
-
-      virtual void text(const std::string &t, float x, float y){
-        // fallback: use image and a hardcoded font (including hardcoded size)
-      }
-            
+      virtual void point(float x, float y);
+      virtual void line(float x0, float y0, float x1, float y1);
+      virtual void triangle(float x0, float y0, float x1, float y1, float x2, float y2);
+      virtual void sym(char c, float x, float y) throw (utils::ICLException);
+      virtual void linestrip(int n, const float *xs, const float *ys, 
+                             int xStride=1, int yStride=1, bool loop=false);
+      virtual void rect(float x, float y, float w, float h);
+      virtual void circle(float cx, float cy, float r);
+      virtual void ellipse(float cx, float cy, float rx, float ry);
+      virtual void image(const ImgBase *image, float xanchor, 
+                         float yanchor, float alpha, 
+                         scalemode sm=interpolateLIN, 
+                         bool centered=false)  throw (utils::ICLException);
+      virtual void text(const std::string &t, float x, float y, bool centered = false);
       
       virtual void linecolor(float r, float g, float b, float a){
         state.linecolor = Color(r,g,b,a);
@@ -204,6 +171,7 @@ namespace icl{
       
       virtual void transform(float tx, float ty, float angle){
         if(angle){
+          const float sa = sin(angle), ca = cos(angle);
           state.T = Transform(sa,ca,tx,
                               -ca,sa,ty,
                               0,0,1)*state.T;
