2a4e1cc2c57026552ea8281e4a88ef9da1ea2b98
SciRes <info@scires.de>
2014-12-10 13:22:13 +0000

    added the cutfree adjacency feature

---
+++ ICLGeom/src/ICLGeom/CutfreeAdjacencyFeatureExtractor.cpp
@@ -0,0 +1,85 @@
+/***********************************************************************
+ **                Image Component Library (ICL)                      **
+ **                                                                   **
+ ** Copyright (C) 2006-2013 CITEC, University of Bielefeld            **
+ **                         Neuroinformatics Group                    **
+ ** Website: www.iclcv.org and                                        **
+ **          http://opensource.cit-ec.de/projects/icl                 **
+ **                                                                   **
+ ** File   : ICLGeom/src/ICLGeom/CutfreeAdjacencyFeatureExtractor.cpp **
+ ** Module : ICLGeom                                                  **
+ ** Authors: Andre Ueckermann                                         **
+ **                                                                   **
+ **                                                                   **
+ ** GNU LESSER GENERAL PUBLIC LICENSE                                 **
+ ** This file may be used under the terms of the GNU Lesser General   **
+ ** Public License version 3.0 as published by the                    **
+ **                                                                   **
+ ** Free Software Foundation and appearing in the file LICENSE.LGPL   **
+ ** included in the packaging of this file.  Please review the        **
+ ** following information to ensure the license requirements will     **
+ ** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                  **
+ **                                                                   **
+ ** The development of this software was supported by the             **
+ ** Excellence Cluster EXC 277 Cognitive Interaction Technology.      **
+ ** The Excellence Cluster EXC 277 is a grant of the Deutsche         **
+ ** Forschungsgemeinschaft (DFG) in the context of the German         **
+ ** Excellence Initiative.                                            **
+ **                                                                   **
+ **********************************************************************/
+
+#include <ICLGeom/CutfreeAdjacencyFeatureExtractor.h>
+#include <ICLGeom/PlanarRansacEstimator.h>
+
+namespace icl {
+  namespace geom {
+    struct CutfreeAdjacencyFeatureExtractor::Data {
+	    Data(Mode mode) {
+        if(mode==BEST || mode==GPU){
+          ransac=new PlanarRansacEstimator(PlanarRansacEstimator::GPU);
+        }else{
+          ransac=new PlanarRansacEstimator(PlanarRansacEstimator::CPU);
+        }
+	    }
+
+	    ~Data() {
+	    }
+      
+      PlanarRansacEstimator* ransac;
+    };
+    
+    
+    CutfreeAdjacencyFeatureExtractor::CutfreeAdjacencyFeatureExtractor(Mode mode) :
+	    m_data(new Data(mode)) {
+    }
+
+
+    CutfreeAdjacencyFeatureExtractor::~CutfreeAdjacencyFeatureExtractor() {
+	    delete m_data;
+    }
+    
+
+    math::DynMatrix<bool> CutfreeAdjacencyFeatureExtractor::apply(core::DataSegment<float,4> &xyzh, 
+                std::vector<std::vector<int> > &surfaces, math::DynMatrix<bool> &testMatrix, float euclideanDistance, 
+                int passes, int tolerance, core::Img32s labelImage){
+      math::DynMatrix<bool> cutfreeMatrix(testMatrix);
+      for(unsigned int x=0; x<cutfreeMatrix.rows(); x++){
+        cutfreeMatrix(x,x)=false;
+      }
+      math::DynMatrix<PlanarRansacEstimator::Result> result = m_data->ransac->apply(xyzh, surfaces, 
+                    cutfreeMatrix, euclideanDistance, passes, tolerance, 
+                    PlanarRansacEstimator::ON_ONE_SIDE, labelImage);
+      
+      for(unsigned int x=0; x<result.rows(); x++){
+        for(unsigned int y=0; y<result.cols(); y++){
+          if(result(x,y).nacc>=result(x,y).acc){
+            cutfreeMatrix(x,y)=false;
+            cutfreeMatrix(y,x)=false;
+          }
+        }
+      }
+      return cutfreeMatrix;
+    }
+       
+  } // namespace geom
+}
---
+++ ICLGeom/src/ICLGeom/CutfreeAdjacencyFeatureExtractor.h
@@ -0,0 +1,74 @@
+/********************************************************************
+**                Image Component Library (ICL)                    **
+**                                                                 **
+** Copyright (C) 2006-2013 CITEC, University of Bielefeld          **
+**                         Neuroinformatics Group                  **
+** Website: www.iclcv.org and                                      **
+**          http://opensource.cit-ec.de/projects/icl               **
+**                                                                 **
+** File   : ICLGeom/src/ICLGeom/CutfreeAdjacencyFeatureExtractor.h **
+** Module : ICLGeom                                                **
+** Authors: Andre Ueckermann                                       **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+** The development of this software was supported by the           **
+** Excellence Cluster EXC 277 Cognitive Interaction Technology.    **
+** The Excellence Cluster EXC 277 is a grant of the Deutsche       **
+** Forschungsgemeinschaft (DFG) in the context of the German       **
+** Excellence Initiative.                                          **
+**                                                                 **
+********************************************************************/
+
+#pragma once
+
+#include <ICLCore/DataSegment.h>
+#include <ICLCore/Img.h>
+
+namespace icl{
+  namespace geom{
+    /**
+       This class implements the cutfree adjacency feature for feature-graph based segmentation.*/
+    class CutfreeAdjacencyFeatureExtractor{
+  	
+     public:
+     
+      enum Mode {BEST, GPU, CPU};
+      
+      /// Constructor
+      /** Constructs an object of this class.
+          @param mode GPU, CPU and BEST (default) */
+      CutfreeAdjacencyFeatureExtractor(Mode mode=BEST); 
+  	
+      /// Destructor
+      ~CutfreeAdjacencyFeatureExtractor();
+  	      
+      /// Calculates the cutfree adjacency feature matrix.
+      /** @param xyzh the xyzh DataSegment from the PointCloudObject class
+          @param surfaces the vector of surface id vectors
+          @param testMatrix the initial boolean test matrix (1 test, 0 dont test, preferably an adjacency matrix)
+          @param euclideanDistance the maximum euclidean distance for RANSAC in mm
+          @param passes the RANSAC passes
+          @param tolerance the RANSAC tolerance in number of points (outlier)
+          @param labelImage the label image
+          @return the boolean cutfree adjacency matrix */
+       math::DynMatrix<bool> apply(core::DataSegment<float,4> &xyzh, 
+                std::vector<std::vector<int> > &surfaces, math::DynMatrix<bool> &testMatrix, float euclideanDistance, 
+                int passes, int tolerance, core::Img32s labelImage);
+  	  		            
+     private:
+     
+      struct Data;  //!< internal data type
+      Data *m_data; //!< internal data pointer
+                 
+    };
+  } // namespace geom
+}
