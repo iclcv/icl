a37e484687768bb6a25d4281a790cf6c119ce6b5
Christof Elbrechter <c.elbrechter@gmx.de>
2015-05-28 14:55:21 +0000

    added option to build BUILD_WITH_XYZ_OPTIONAL=TRUE for all dependencies that are searched using ICL_FIND_PACKAGE. Remove FindIPP.cmake file by moving the content to the main CMakeLists file

--- cmake/Modules/ICLHelperMacros.cmake
+++ cmake/Modules/ICLHelperMacros.cmake
@@ -347,3 +347,37 @@ FUNCTION(ADD_ICL_DEFINITIONS)
   ADD_DEFINITIONS(${ARGV})
   LIST(APPEND ICL_DEFINITIONS ${ARGV})
 ENDFUNCTION()
+
+
+function(get_ipp_version _INCLUDE_DIR)
+  set(_VERSION_STR)
+  set(_MAJOR)
+  set(_MINOR)
+  set(_BUILD)
+
+  # read IPP version info from file
+  file(STRINGS ${_INCLUDE_DIR}/ippversion.h STR1 REGEX "IPP_VERSION_MAJOR")
+  file(STRINGS ${_INCLUDE_DIR}/ippversion.h STR2 REGEX "IPP_VERSION_MINOR")
+  file(STRINGS ${_INCLUDE_DIR}/ippversion.h STR3 REGEX "IPP_VERSION_BUILD")
+  file(STRINGS ${_INCLUDE_DIR}/ippversion.h STR4 REGEX "IPP_VERSION_STR")
+  
+  if (NOT STR3)
+    file(STRINGS ${_INCLUDE_DIR}/ippversion.h STR3 REGEX "IPP_VERSION_UPDATE")
+  endif(NOT STR3)
+  
+  # extract info and assign to variables
+  string(REGEX MATCHALL "[0-9]+" _MAJOR ${STR1})
+  string(REGEX MATCHALL "[0-9]+" _MINOR ${STR2})
+  string(REGEX MATCHALL "[0-9]+" _BUILD ${STR3})
+  string(REGEX MATCHALL "[0-9]+[.]+[0-9]+[^\"]+|[0-9]+[.]+[0-9]+" _VERSION_STR ${STR4})
+  
+  # export info to parent scope
+  set(IPP_VERSION_STR   ${_VERSION_STR} PARENT_SCOPE)
+  set(IPP_VERSION_MAJOR ${_MAJOR}       PARENT_SCOPE)
+  set(IPP_VERSION_MINOR ${_MINOR}       PARENT_SCOPE)
+  set(IPP_VERSION_BUILD ${_BUILD}       PARENT_SCOPE)
+  
+  MESSAGE(STATUS "           IPP Version: ${_MAJOR}.${_MINOR}.${_BUILD} [${_VERSION_STR}]")
+  
+  return()
+endfunction()
