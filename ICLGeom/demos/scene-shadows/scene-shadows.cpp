b21709be3e2341b7b57f1195c749f9898ae044ed
Christof Elbrechter <c.elbrechter@gmx.de>
2015-10-17 21:09:33 +0000

    actually used the normalization in the scene-shadows demo

--- ICLGeom/demos/scene-shadows/scene-shadows.cpp
+++ ICLGeom/demos/scene-shadows/scene-shadows.cpp
@@ -90,7 +90,7 @@ void init(){
     scene.getLight(i).setDiffuse(GeomColor(255 / lights,255 / lights,255 / lights,255));
     scene.getLight(i).setPosition(Vec(-4,10,-30,1));
     scene.getLight(i).setSpecular(GeomColor(255 / lights,255 / lights,255 / lights,255));
-    scene.getLight(i).getShadowCam()->setNorm(Vec(4,-10,30,1));
+    scene.getLight(i).getShadowCam()->setNorm(Vec(4,-10,30,1),true);
   }
 
   float so = pa("-so");
@@ -146,7 +146,7 @@ void run(){
 
   for(unsigned int i = 0; i < lights; i++) {
     scene.getLight(i).setPosition(Vec(h,r * cos(timer * 1.f / float(i + 1)),r * sin(timer * 1.f / float(i + 1)),1.f));
-    scene.getLight(i).getShadowCam()->setNorm(Vec(-h,-r * cos(timer * 1.f / float(i + 1)),-r * sin(timer * 1.f / float(i + 1)),1.f));
+    scene.getLight(i).getShadowCam()->setNorm(Vec(-h,-r * cos(timer * 1.f / float(i + 1)),-r * sin(timer * 1.f / float(i + 1)),1.f), true);
   }
   scene.getCamera(0).setFocalLength(gui["f"]); // update the camera's focal length
   
