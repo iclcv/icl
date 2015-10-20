5cfcac3170f62bd97e8770d1ae5a095e89088a14
Christof Elbrechter <c.elbrechter@gmx.de>
2015-10-20 13:34:01 +0000

    fixed max tilt in marker detector

--- ICLMarkers/src/ICLMarkers/FiducialDetectorPluginForQuads.cpp
+++ ICLMarkers/src/ICLMarkers/FiducialDetectorPluginForQuads.cpp
@@ -62,10 +62,10 @@ namespace icl{
       
       deactivateProperty("quads.create region graph");
   
-      addProperty("max tilt","range","[1,50]",10,0,
+      addProperty("max tilt","range","[1,200]",150,0,
                   "Maximum tilt for quad markers (1: only perfect squares are\n"
                   "detected, 5-10 most tilted quads are detected, >10\n"
-                  "also very tilted rectangular quads are detected.");
+                  "also very tilted rectangular quads are detected (not linear).");
       addProperty("return rejected quads","flag","",false,0,
                   "If set to true, also quads that were not identified as valid markers\n"
                   "are retuned (as dummy markers with an invalid ID");
