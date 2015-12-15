423c5562f5671a0dc919cdba9a27d09ed3753fef
Christof Elbrechter <c.elbrechter@gmx.de>
2015-12-15 11:57:19 +0000

    partly fixed the PhysicsPaper3s tendency to crash when adding intersecting folds while self-collision is activated

--- ICLPhysics/src/ICLPhysics/PhysicsWorld.h
+++ ICLPhysics/src/ICLPhysics/PhysicsWorld.h
@@ -53,20 +53,20 @@ namespace icl{
     class Constraint;
 
     /// A physical world that handles physical objects
-		class ICLPhysics_API PhysicsWorld : public utils::Lockable, public utils::Uncopyable{
+    class ICLPhysics_API PhysicsWorld : public utils::Lockable, public utils::Uncopyable{
       
-		public:
-
-			enum BulletSolverType {
-				SequentialImpulseConstraintSolver,
-				MLCP_Dantzig,
-				NNCG,
-				Lemke,
-				Default = SequentialImpulseConstraintSolver
-			};
-
-		private:
-
+      public:
+      
+      enum BulletSolverType {
+        SequentialImpulseConstraintSolver,
+        MLCP_Dantzig,
+        NNCG,
+        Lemke,
+        Default = SequentialImpulseConstraintSolver
+      };
+      
+      private:
+      
       friend class PhysicsObject;
       /// internal data structure (hidden)
       struct Data;
@@ -74,18 +74,18 @@ namespace icl{
       /// internal data pointer
       Data *data;
       
-			void setSolver(BulletSolverType type);
-
-		protected:
+      void setSolver(BulletSolverType type);
+      
+      protected:
 
       /// removes contactpoints (used when the collisionshape of an object has changed)
       void removeContactPoints(PhysicsObject *obj);
 
-		public:
-
+      public:
+      
       /// constructor with given config file name
-			PhysicsWorld(BulletSolverType solver_type = Default);
-
+      PhysicsWorld(BulletSolverType solver_type = Default);
+      
       /// Destructor
       ~PhysicsWorld();
       
@@ -112,8 +112,8 @@ namespace icl{
           the last call of this method is used */
       void step(float dtSeconds=-1, int maxSubSteps=10, float fixedTimeStep=1.f/120.f);
 
-			/// returns the last delta of time in seconds as a double value.
-			double getLastTimeDelta();
+      /// returns the last delta of time in seconds as a double value.
+      double getLastTimeDelta();
       
       ///check collision of an object with the world
       bool collideWithWorld(RigidObject* obj, bool ignoreJoints = true);
