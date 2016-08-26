1e9e65d565e308dc481f5522849df71e5e1db26f
Alexander Neumann <aleneum@gmail.com>
2016-08-26 13:09:27 +0000

    add homebrew test script

---
+++ scripts/icl-homebrew-test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+HOMEBREW_PREFIX=/usr/local
+
+mkdir build
+cd build
+
+cmake -DBUILD_APPS=On \
+      -DBUILD_DEMOS=On \
+      -DBUILD_EXAMPLES=On \
+      -DCMAKE_INSTALL_PREFIX="${HOMEBREW_PREFIX}" \
+	  -DBUILD_WITH_OPENCV=On \
+      -DBUILD_WITH_QT=On \
+      -DQT_ROOT="${HOMEBREW_PREFIX}/opt/qt5" \
+      -DBUILD_WITH_OPENGL=On \
+      -DBUILD_WITH_IMAGEMAGICK=On \
+      -DBUILD_WITH_LIBAV=On \
+      -DBUILD_WITH_LIBDC=On \
+      -DBUILD_WITH_EIGEN3=On  \
+      -DBUILD_WITH_OPENCV=On \
+      -DBUILD_WITH_QT=On \
+      -DQT_ROOT="${HOMEBREW_PREFIX}/opt/qt5" \
+      -DBUILD_WITH_OPENGL=On \
+      -DBUILD_WITH_IMAGEMAGICK=On \
+      -DBUILD_WITH_LIBAV=On \
+      -DBUILD_WITH_LIBDC=On \
+      -DBUILD_WITH_EIGEN3=On \
+      -DBUILD_WITH_RSB=On \
+      -DBUILD_WITH_RSC=On \
+      -DBUILD_WITH_PROTOBUF=On \
+      -DBUILD_WITH_PCL=On \
+      -DBUILD_WITH_BULLET=On \
+      -DBUILD_WITH_LIBFREENECT=On \
+      -DBUILD_WITH_LIBUSB=On \
+      -DBUILD_WITH_ZMQ=On \
+      -DBUILD_WITH_OPENNI=On \
+      ..
+sleep 5
+make -j5
