345bd3763deb20bdf98fde839e0f5c82b1029ef5
SciRes <info@scires.de>
2016-10-18 17:18:52 +0000

    fixed opencl2 incompatibility

--- ICLUtils/src/ICLUtils/CLIncludes.h
+++ ICLUtils/src/ICLUtils/CLIncludes.h
@@ -6,15 +6,20 @@
 #ifdef ICL_HAVE_CL2HPP
 
 #define CL_HPP_ENABLE_EXCEPTIONS //enables openCL error catching
-#define CL_HPP_TARGET_OPENCL_VERSION 200
+#define CL_HPP_TARGET_OPENCL_VERSION 120
 // The following is needed to maintain compatibility to 1.2
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_ENABLE_SIZE_T_COMPATIBILITY
+
 //#define CL_HPP_NO_STD_UNIQUE_PTR
+
 #define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY
+
+//#define CL_HPP_CL_1_2_DEFAULT_BUILD
 
 #warning "including cl2.hpp now"
 #include <CL/cl2.hpp>
+//using namespace  cl::compatibility;
 #include <CL/cl.h>
 
 #else
