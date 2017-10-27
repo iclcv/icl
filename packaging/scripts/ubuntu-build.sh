2ccf249d69e6b58ac391e8e0d6fcb53e205fbc56
Alexander Neumann <aleneum@gmail.com>
2017-10-27 17:56:15 +0200

    integrating optional bullet built
    trusty features an old version (2.81) which does not contain some required features

--- packaging/scripts/ubuntu-build.sh
+++ packaging/scripts/ubuntu-build.sh
@@ -3,6 +3,8 @@
 rm -r build
 mkdir build && cd build
 
+# keep the build with bullet optional since version 2.81
+# at trusty is too old
 cmake -DCMAKE_SKIP_RPATH=ON \
       -DBUILD_WITH_OPENCV=ON  -DBUILD_WITH_QT=ON \
       -DBUILD_WITH_OPENGL=ON \
@@ -13,6 +15,7 @@ cmake -DCMAKE_SKIP_RPATH=ON \
       -DBUILD_WITH_V4L=ON \
       -DBUILD_WITH_LIBUSB=ON -DBUILD_WITH_ZMQ=ON \
       -DBUILD_WITH_OPENCL=ON \
+      -DBUILD_WITH_BULLET=ON -DBUILD_WITH_BULLET_OPTIONAL=ON \
       -DBUILD_WITH_OPENNI=ON -DBUILD_WITH_PCL=ON \
       -DBUILD_EXAMPLES=ON -DBUILD_DEMOS=ON \
       -DBUILD_APPS=ON -DBUILD_DEBS=ON ..
