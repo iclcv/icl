16dd1ef79e8d7f767328b3e8fe5b579bf70828b8
Alexander Neumann <aleneum@gmail.com>
2017-11-17 16:07:22 +0100

    automate package build (source and binary)

--- packaging/scripts/ubuntu-build.sh
+++ packaging/scripts/ubuntu-build.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-rm -r build
-mkdir build && cd build
+script_dir="$(dirname "$0")"
+source "${script_dir}/ubuntu-package-env.sh"
 
-# keep the build with bullet optional since version 2.81
-# at trusty is too old
-cmake -DCMAKE_SKIP_RPATH=ON \
-      -DBUILD_WITH_OPENCV=ON  -DBUILD_WITH_QT=ON \
-      -DBUILD_WITH_OPENGL=ON \
-      -DBUILD_WITH_EIGEN3=ON \
-      -DBUILD_WITH_IMAGEMAGICK=ON \
-      -DBUILD_WITH_LIBAV=ON  -DBUILD_WITH_LIBDC=ON \
-      -DBUILD_WITH_LIBFREENECT=ON \
-      -DBUILD_WITH_V4L=ON \
-      -DBUILD_WITH_LIBUSB=ON -DBUILD_WITH_ZMQ=ON \
-      -DBUILD_WITH_OPENCL=ON \
-      -DBUILD_WITH_BULLET=ON -DBUILD_WITH_BULLET_OPTIONAL=ON \
-      -DBUILD_WITH_OPENNI=ON -DBUILD_WITH_PCL=ON \
-      -DBUILD_EXAMPLES=ON -DBUILD_DEMOS=ON \
-      -DBUILD_APPS=ON -DBUILD_REDIST=DEB ..
+configure_icl
---
+++ packaging/scripts/ubuntu-package-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# keep bullet optional since older distributions contain
+# unsupported versions
+ICL_OPTIONS="\
+  -DCMAKE_SKIP_RPATH=ON \
+  -DBUILD_WITH_OPENCV=ON  -DBUILD_WITH_QT=ON \
+  -DBUILD_WITH_OPENGL=ON \
+  -DBUILD_WITH_EIGEN3=ON \
+  -DBUILD_WITH_IMAGEMAGICK=ON \
+  -DBUILD_WITH_LIBAV=ON  -DBUILD_WITH_LIBDC=ON \
+  -DBUILD_WITH_LIBFREENECT=ON \
+  -DBUILD_WITH_V4L=ON \
+  -DBUILD_WITH_LIBUSB=ON -DBUILD_WITH_ZMQ=ON \
+  -DBUILD_WITH_OPENCL=ON \
+  -DBUILD_WITH_BULLET=ON -DBUILD_WITH_BULLET_OPTIONAL=ON \
+  -DBUILD_WITH_OPENNI=ON -DBUILD_WITH_PCL=ON \
+  -DBUILD_EXAMPLES=ON -DBUILD_DEMOS=ON -DBUILD_APPS=ON \
+  -DBUILD_REDIST=DEB "
+
+configure_icl() {
+  echo "removing old build..."
+  rm -r build
+  rm -r debian
+  echo "configure..."
+  mkdir build && cd build
+  cmake ${ICL_OPTIONS} ..
+  cd ..
+}
