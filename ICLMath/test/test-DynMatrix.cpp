1b49edfc90f3b10c4928d4568c88a6232e0b683b
Alexander Neumann <aleneum@gmail.com>
2017-12-20 14:27:43 +0100

    add exended op and column op

--- ICLMath/test/test-DynMatrix.cpp
+++ ICLMath/test/test-DynMatrix.cpp
@@ -12,6 +12,15 @@ double m3x3r0[9] {1, 2, 3,
 double m3x3r1[9] { 0.3987968 ,  0.11602292,  0.88068036,
                    0.36042386,  0.21228021,  0.52000919,
                    0.71980622,  0.79006622,  0.29349811 };
+
+double m3x3r1_norm = 1.6192413881673249;
+double m3x3r1_sdist = 0.1408216412715566; // 1st vs 2nd row;
+double m3x3r1_trace = 0.90457512000000007;
+double m3x3r1_cond = 193.38165365810715; // frobenius/root-of-sum-squres norm;
+double m3x3r1_cross[3] { -0.12661802711304079305, 
+                          0.11004021383479761775,
+                          0.04283923977645680975 };
+
 double m3x3r2[9] { 0.950307  ,  0.53584179,  0.05760179,
                    0.25855725,  0.74725683,  0.24040829,
                    0.92026487,  0.05839824,  0.41791317 };
@@ -33,6 +42,9 @@ double m4x4r1[16] { 0.36228671,  0.18756258,  0.47559801,  0.23159873,
                     0.16747942,  0.8166452 ,  0.26111905,  0.25612094,
                     0.55120608,  0.39206604,  0.16824783,  0.3067865 ,
                     0.49421738,  0.36987849,  0.63399134,  0.31633955 };
+
+double m4x4r1_cond = 131.68394023560649;
+
 double m4x4r2[16] { 0.54490757,  0.32095175,  0.96347888,  0.30934882,
                     0.02317498,  0.20314625,  0.41469638,  0.73932676,
                     0.48979231,  0.34369873,  0.13328761,  0.88741035,
@@ -64,6 +76,7 @@ TEST(DynMatrixTest, Create3x3) {
   const DynMatrix<double> i = DynMatrix<double>::id(3);
   const DynMatrix<double> dShallowCopy(3, 3, d.data(), false);
   const DynMatrix<double> dDeepCopy(3, 3, d.data());
+  const DynMatrix<double> dConstCopy(3, 3, dDeepCopy.data());
   const DynMatrix<double> dValue(3, 3, 42);
   DynMatrix<double> dCopyTarget(1, 1, NULL, false);
   DynMatrix<double> dCopyTarget2;
@@ -75,6 +88,8 @@ TEST(DynMatrixTest, Create3x3) {
 
   EXPECT_EQ(d.data(), dShallowCopy.data());
   EXPECT_NE(d.data(), dDeepCopy.data());
+  EXPECT_TRUE(d.begin() == dShallowCopy.begin());
+  EXPECT_TRUE(d.end() != dDeepCopy.end());
   EXPECT_EQ(dShallowCopy.at(1, 1), dShallowCopy.at(1, 1));
   for(unsigned int i=0; i<9; ++i) ASSERT_EQ(42, dValue[i]);
 
@@ -91,27 +106,43 @@ TEST(DynMatrixTest, Create3x3) {
 }
 
 TEST(DynMatrixTest, BasicOp3x3) {
-  DynMatrix<double> d1 = DynMatrix<double>(3, 3, m3x3r1);
-  DynMatrix<double> d2 = DynMatrix<double>(3, 3, m3x3r2);
-  DynMatrix<double> d3 = DynMatrix<double>(3, 3, m3x3r3);
-  DynMatrix<double> d4 = DynMatrix<double>(3, 3, m3x3r4);
-  DynMatrix<double> d5 = DynMatrix<double>(3, 3, m3x3r5);
+  DynMatrix<double> d1(3, 3, m3x3r1);
+  DynMatrix<double> d2(3, 3, m3x3r2);
+  DynMatrix<double> d3(3, 3, m3x3r3);
+  DynMatrix<double> d4(3, 3, m3x3r4);
+  DynMatrix<double> d5(3, 3, m3x3r5);
+  const DynMatrix<double> i = DynMatrix<double>::id(3);
+  const DynMatrix<double> o(3, 3);
 
   DynMatrix<double> sum = d1 + d2;
   DynMatrix<double> diff = d3 - d1;
   DynMatrix<double> mult = d1.elementwise_mult(d2);
   DynMatrix<double> dot = d1 * d2;
+  dot *= i;
+  dot += o;
+  dot *= 1;
+  dot /= 1;
+  dot += 0;
+  dot -= 0;
 
   for(unsigned int i=0; i<9; ++i) ASSERT_DOUBLE_EQ(d3[i], sum[i]);
   for(unsigned int i=0; i<9; ++i) ASSERT_NEAR(d2[i], diff[i], DOUBLE_MARGIN);
   for(unsigned int i=0; i<9; ++i) ASSERT_NEAR(d4[i], mult[i], DOUBLE_MARGIN);
   for(unsigned int i=0; i<9; ++i) ASSERT_NEAR(d5[i], dot[i],  DOUBLE_MARGIN);
+
+  const DynMatrix<double> tmp = d1.elementwise_mult(d2).elementwise_div(d2);
+  EXPECT_EQ(d1, tmp);
+
+  d1.setBounds(5, 5, true);
+  EXPECT_EQ(d1.at(1,1), (d1/1).at(1,1));
+  EXPECT_EQ(d1.at(0, 0), m3x3r1[0]);
+  EXPECT_EQ(d1.at(4, 4), 0);
 }
 
 TEST(DynMatrixTest, ErrorCheck) {
   const DynMatrix<double> d0 = DynMatrix<double>(3, 3, m3x3r0);
-  const DynMatrix<double> d1 = DynMatrix<double>(3, 3, m3x3r1);
-  const DynMatrix<double> d2 = DynMatrix<double>(4, 5, m4x4r1);
+  DynMatrix<double> d1 = DynMatrix<double>(3, 3, m3x3r1);
+  const DynMatrix<double> d2 = DynMatrix<double>(4, 4, m4x4r1);
 
   EXPECT_THROW(DynMatrix<double>(0, 0), icl::math::InvalidMatrixDimensionException);
   EXPECT_THROW(d1 + d2, icl::math::IncompatibleMatrixDimensionException);
@@ -123,4 +154,42 @@ TEST(DynMatrixTest, ErrorCheck) {
   EXPECT_THROW(d1.at(3, 3), icl::math::InvalidIndexException);
   EXPECT_THROW(d0.inv(), icl::math::SingularMatrixException);
   EXPECT_NO_THROW(d1.inv());
+  EXPECT_THROW(d1.setBounds(0, 1), icl::math::InvalidMatrixDimensionException);
 }
+
+TEST(DynMatrixTest, ExtendedOp) {
+  DynMatrix<double> d1(3, 3, m3x3r1);
+  DynMatrix<double> d2(4, 4, m4x4r1);
+
+  // check norm
+  ASSERT_NEAR(m3x3r1_norm, d1.norm(), DOUBLE_MARGIN);
+  // check squared distance
+  ASSERT_NEAR(m3x3r1_sdist, d1.row(0).sqrDistanceTo(d1.row(1)), DOUBLE_MARGIN);
+  // check trace
+  ASSERT_NEAR(m3x3r1_trace, d1.trace(), DOUBLE_MARGIN);
+  // check transpose
+  DynMatrix<double> transposed = d1.transp();
+  for (unsigned int x=0; x<3; ++x) {
+      for (unsigned int y=0; y<3; ++y) {
+        ASSERT_DOUBLE_EQ(d1.at(x, y), transposed.at(y, x));
+      }
+  }
+  // check cross; transpose column vector to row vector first
+  DynMatrix<double> cross = DynMatrix<double>::cross(d1.row(0).transp(), d1.row(1).transp());
+  for (unsigned int i=0; i<3; ++i) ASSERT_NEAR(m3x3r1_cross[i], cross.data()[i], DOUBLE_MARGIN);
+  // calc condition number
+  // TODO: Using the SVD norm as well as frobius leads to inaccurate results;
+  // could be an issue of the numpy reference though.
+  EXPECT_NEAR(m3x3r1_cond, d1.cond(), DOUBLE_MARGIN * 1e2);
+  EXPECT_NEAR(m4x4r1_cond, d2.cond(), DOUBLE_MARGIN * 1e4);
+}
+
+TEST(DynMatrixTest, ColumnOp) {
+  DynMatrix<double> d1(3, 3, m3x3r1);
+  DynMatrix<double>::DynMatrixColumn col1(&d1, 1);
+  DynMatrix<double>::DynMatrixColumn col2(&d1, 0);
+  col2 = d1.col(1);
+  EXPECT_EQ(m3x3r1[1], col1.matrix->data()[1]);
+  EXPECT_EQ(d1.col(1).begin(), col1.begin());
+}
+
